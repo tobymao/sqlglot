b2d9b60a3a05c30ebed75400d0999f1e91dcb147
tobymao <toby.mao@gmail.com>
2021-04-04 09:52:49 -0700

    refactor file format

--- tests/test_rewriter.py
+++ tests/test_rewriter.py
@@ -1,36 +1,38 @@
 import unittest
 
-from sqlglot import Generator, parse
+from sqlglot import parse
+from sqlglot.dialects import Hive
 from sqlglot.rewriter import Rewriter
 
 class TestRewriter(unittest.TestCase):
     def test_ctas(self):
         expression = parse("SELECT * FROM y")[0]
+        generator = Hive().generator()
 
         self.assertEqual(
-            Generator().generate(Rewriter(expression).ctas('x').expression),
+            generator.generate(Rewriter(expression).ctas('x').expression),
             'CREATE TABLE x AS SELECT * FROM y'
         )
 
         self.assertEqual(
-            Generator().generate(
+            generator.generate(
                 Rewriter(expression).ctas('x', db='foo', file_format='parquet').expression
             ),
             'CREATE TABLE foo.x STORED AS parquet AS SELECT * FROM y'
         )
 
-        self.assertEqual(Generator().generate(expression), 'SELECT * FROM y')
+        self.assertEqual(generator.generate(expression), 'SELECT * FROM y')
 
         rewriter = Rewriter(expression).ctas('x')
-        self.assertEqual(Generator().generate(rewriter.expression), 'CREATE TABLE x AS SELECT * FROM y')
+        self.assertEqual(generator.generate(rewriter.expression), 'CREATE TABLE x AS SELECT * FROM y')
         self.assertEqual(
-            Generator().generate(rewriter.ctas('y').expression),
+            generator.generate(rewriter.ctas('y').expression),
             'CREATE TABLE y AS SELECT * FROM y'
         )
 
         expression = parse("CREATE TABLE x AS SELECT * FROM y")[0]
         rewriter = Rewriter(expression, copy=False).ctas('x', file_format='ORC')
         self.assertEqual(
-            Generator().generate(expression),
+            generator.generate(expression),
             'CREATE TABLE x STORED AS ORC AS SELECT * FROM y'
         )

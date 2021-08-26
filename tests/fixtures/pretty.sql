8962f22aaefcae3868f49477ae20a8426724d2fc
Toby Mao <toby.mao@gmail.com>
2021-08-26 15:24:51 -0700

    add conditional select
    fixes #11

--- tests/fixtures/pretty.sql
+++ tests/fixtures/pretty.sql
@@ -138,3 +138,44 @@ INSERT OVERWRITE TABLE x VALUES (1, 2.0, '3.0'), (4, 5.0, '6.0');
 INSERT OVERWRITE TABLE x VALUES
   (1, 2.0, '3.0'),
   (4, 5.0, '6.0');
+WITH regional_sales AS (
+    SELECT region, SUM(amount) AS total_sales
+    FROM orders
+    GROUP BY region
+    ), top_regions AS (
+    SELECT region
+    FROM regional_sales
+    WHERE total_sales > (SELECT SUM(total_sales)/10 FROM regional_sales)
+)
+SELECT region,
+product,
+SUM(quantity) AS product_units,
+SUM(amount) AS product_sales
+FROM orders
+WHERE region IN (SELECT region FROM top_regions)
+GROUP BY region, product;
+WITH regional_sales AS (
+    SELECT
+      region,
+      SUM(amount) AS total_sales
+    FROM orders
+    GROUP BY
+      region
+), top_regions AS (
+    SELECT
+      region
+    FROM regional_sales
+    WHERE
+      total_sales > (SELECT SUM(total_sales) / 10 FROM regional_sales)
+)
+SELECT
+  region,
+  product,
+  SUM(quantity) AS product_units,
+  SUM(amount) AS product_sales
+FROM orders
+WHERE
+  region IN (SELECT region FROM top_regions)
+GROUP BY
+  region,
+  product;

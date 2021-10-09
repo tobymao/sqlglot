bdf5bb8c947a01ee8f78068c0fdef3779f9bf523
tobymao <toby.mao@gmail.com>
2021-10-08 23:01:07 -0700

    buff up main

--- sqlglot/__main__.py
+++ sqlglot/__main__.py
@@ -1,7 +1,46 @@
-import sys
+import argparse
 
 import sqlglot
 
 
-for sql in sqlglot.transpile(sys.argv[1], pretty=True):
+parser = argparse.ArgumentParser(description="Transpile SQL")
+parser.add_argument("sql", metavar="sql", type=str, help="SQL string to transpile")
+parser.add_argument(
+    "--read",
+    dest="read",
+    type=str,
+    default=None,
+    help="Dialect to read default is generic",
+)
+parser.add_argument(
+    "--write",
+    dest="write",
+    type=str,
+    default=None,
+    help="Dialect to write default is generic",
+)
+parser.add_argument(
+    "--no-identify",
+    dest="identify",
+    action="store_false",
+    help="Don't auto identify fields",
+)
+parser.add_argument(
+    "--no-pretty",
+    dest="pretty",
+    action="store_false",
+    help="Compress sql",
+)
+
+args = parser.parse_args()
+
+sqls = sqlglot.transpile(
+    args.sql,
+    read=args.read,
+    write=args.write,
+    identify=args.identify,
+    pretty=args.pretty,
+)
+
+for sql in sqls:
     print(sql)

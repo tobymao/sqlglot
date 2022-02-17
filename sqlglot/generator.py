bf3f572b3615cf01132236955a66795d08ae5989
Sam Lindsay <sam.lindsay@digital.justice.gov.uk>
2022-02-17 08:36:00 +0000

    tidy up

--- sqlglot/generator.py
+++ sqlglot/generator.py
@@ -423,12 +423,9 @@ class Generator:
         return f"{expression_sql}{op_sql} {this_sql}{on_sql}"
 
     def lambda_sql(self, expression):
-        a, b = (
-            ["(", ")"] if len(self.expressions(expression, flat=True)) > 1 else ["", ""]
-        )
-        return self.no_identify(
-            lambda: f"{a}{self.expressions(expression, flat=True)}{b} -> {self.sql(expression, 'this')}"
-        )
+        args = self.expressions(expression, flat=True)
+        args = f"({args})" if len(args) > 1 else args
+        return self.no_identify(lambda: f"{args} -> {self.sql(expression, 'this')}")
 
     def lateral_sql(self, expression):
         this = self.sql(expression, "this")

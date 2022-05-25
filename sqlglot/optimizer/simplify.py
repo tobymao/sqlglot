a9344106adc7ed7ae5828311ef95c445e1e6dd73
tobymao <toby.mao@gmail.com>
2022-05-24 23:44:30 -0700

    add simplify optimizer

---
+++ sqlglot/optimizer/simplify.py
@@ -0,0 +1,92 @@
+import sqlglot.expressions as exp
+
+
+TRUE = exp.Boolean(this=True)
+FALSE = exp.Boolean(this=False)
+NULL = exp.Null()
+
+
+def simplify(expression):
+    expression = expression.copy()
+
+    # wrap because you cannot replace a node without a parent
+    expression = exp.Paren(this=expression)
+
+    while True:
+        start = hash(expression)
+        expression = simplify_equality(expression)
+        expression = simplify_not(expression)
+        expression = simplify_conjunctions(expression)
+        expression = simplify_parens(expression)
+
+        if start == hash(expression):
+            break
+
+    return expression.this
+
+
+def simplify_equality(expression):
+    for node in reverse_traverse(expression, exp.EQ):
+        left = node.left
+        right = node.right
+
+        if NULL in (left, right):
+            node.replace(NULL)
+        elif left == right:
+            node.replace(TRUE)
+    return expression
+
+
+def simplify_not(expression):
+    for node in reverse_traverse(expression, exp.Not):
+        if always_true(node.this):
+            node.replace(FALSE)
+        elif node.this == FALSE:
+            node.replace(TRUE)
+    return expression
+
+
+def simplify_conjunctions(expression):
+    for node in reverse_traverse(expression, exp.And, exp.Or):
+        left = node.left
+        right = node.right
+
+        if isinstance(node, exp.And):
+            if NULL in (left, right):
+                node.replace(NULL)
+            elif FALSE in (left, right):
+                node.replace(FALSE)
+            elif always_true(left) and always_true(right):
+                node.replace(TRUE)
+        elif isinstance(node, exp.Or):
+            if always_true(left) or always_true(right):
+                node.replace(TRUE)
+            elif left == FALSE and right == FALSE:
+                node.replace(FALSE)
+            elif (
+                (left == NULL and right == NULL)
+                or (left == NULL and right == FALSE)
+                or (left == FALSE and right == NULL)
+            ):
+                node.replace(NULL)
+            elif left == FALSE:
+                node.replace(right)
+            elif right == FALSE:
+                node.replace(left)
+    return expression
+
+
+def simplify_parens(expression):
+    for node in reverse_traverse(expression, exp.Paren):
+        if not isinstance(node.this, exp.Binary):
+            node.replace(node.this)
+    return expression
+
+
+def always_true(expression):
+    return expression == TRUE or isinstance(expression, exp.Literal)
+
+
+def reverse_traverse(expression, *types):
+    for node in reversed(list(expression.find_all(types or exp.Expression, bfs=False))):
+        yield node

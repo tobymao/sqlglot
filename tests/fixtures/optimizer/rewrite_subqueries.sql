c68791828afd70ba2615dcc53cb55a88285e0caa
tobymao <toby.mao@gmail.com>
2022-05-27 10:50:14 -0700

    add barak's changes to use new scope

--- tests/fixtures/optimizer/rewrite_subqueries.sql
+++ tests/fixtures/optimizer/rewrite_subqueries.sql
@@ -12,3 +12,6 @@ SELECT * FROM x AS x JOIN (SELECT y.a FROM y AS y WHERE TRUE AND y.a > 1 GROUP B
 
 SELECT * FROM x AS x, y AS y WHERE x.a = y.a AND (SELECT y.b FROM y AS y WHERE x.b = y.b) < 0;
 SELECT * FROM x AS x, y AS y JOIN (SELECT y.b FROM y AS y WHERE TRUE GROUP BY y.b) AS "_d_0" ON _d_0.b = x.b AND ("_d_0".b) < 0 WHERE x.a = y.a AND TRUE;
+
+SELECT (SELECT MAX(y.c) AS max_c FROM y AS y WHERE y.b = x.b) AS max_c FROM x;
+SELECT ("_d_0".max_c) AS max_c FROM x JOIN (SELECT MAX(y.c) AS max_c, y.b FROM y AS y WHERE TRUE GROUP BY y.b) AS "_d_0" ON _d_0.b = x.b;

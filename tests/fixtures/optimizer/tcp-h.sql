264d3c4162c2f505f8511d6f9c83ee1cf07cfc33
barak <barak.alon@airbnb.com>
2022-05-28 11:09:51 -0700

    Allow GROUP BY and ORDER BY to reference output columns (#151)
    * Allow GROUP BY and ORDER BY to reference output columns
    
    * fixup

--- tests/fixtures/optimizer/tcp-h.sql
+++ tests/fixtures/optimizer/tcp-h.sql
@@ -138,27 +138,49 @@ LIMIT 100;
 --------------------------------------
 -- TCP-H 3
 --------------------------------------
---select
---        l_orderkey,
---        sum(l_extendedprice * (1 - l_discount)) as revenue,
---        o_orderdate,
---        o_shippriority
---from
---        customer,
---        orders,
---        lineitem
---where
---        c_mktsegment = 'BUILDING'
---        and c_custkey = o_custkey
---        and l_orderkey = o_orderkey
---        and o_orderdate < date '1995-03-15'
---        and l_shipdate > date '1995-03-15'
---group by
---        l_orderkey,
---        o_orderdate,
---        o_shippriority
---order by
---        revenue desc,
---        o_orderdate
---limit
---        10;
+select
+        l_orderkey,
+        sum(l_extendedprice * (1 - l_discount)) as revenue,
+        o_orderdate,
+        o_shippriority
+from
+        customer,
+        orders,
+        lineitem
+where
+        c_mktsegment = 'BUILDING'
+        and c_custkey = o_custkey
+        and l_orderkey = o_orderkey
+        and o_orderdate < date '1995-03-15'
+        and l_shipdate > date '1995-03-15'
+group by
+        l_orderkey,
+        o_orderdate,
+        o_shippriority
+order by
+        revenue desc,
+        o_orderdate
+limit
+        10;
+SELECT
+  "lineitem"."l_orderkey" AS "l_orderkey",
+  SUM("lineitem"."l_extendedprice" * (1 - "lineitem"."l_discount")) AS "revenue",
+  "orders"."o_orderdate" AS "o_orderdate",
+  "orders"."o_shippriority" AS "o_shippriority"
+FROM "customer" AS "customer"
+JOIN "orders" AS "orders"
+  ON "orders"."o_orderdate" < CAST('1995-03-15' AS DATE)
+  AND "lineitem"."l_orderkey" = "orders"."o_orderkey"
+  AND "customer"."c_custkey" = "orders"."o_custkey"
+JOIN "lineitem" AS "lineitem"
+  ON "lineitem"."l_shipdate" > CAST('1995-03-15' AS DATE)
+WHERE
+  "customer"."c_mktsegment" = 'BUILDING'
+GROUP BY
+  "lineitem"."l_orderkey",
+  "orders"."o_orderdate",
+  "orders"."o_shippriority"
+ORDER BY
+  "revenue" DESC,
+  "orders"."o_orderdate"
+LIMIT 10;

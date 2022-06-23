d1ed2cdcbd55d78ac9b0c6862e2ec10e7035620e
tobymao <toby.mao@gmail.com>
2022-06-22 22:33:11 -0700

    get rid of columnar reader since it doesn't add anything

--- sqlglot/executor/table.py
+++ sqlglot/executor/table.py
@@ -1,37 +1,36 @@
 class Table:
-    def __init__(self, *columns, _columnar=True, **table):
-        if table:
-            self.data = table
-        else:
-            self.data = {column: [] for column in columns}
-        self.columns = tuple(self.data)
-        self.column_indicies = dict(enumerate(self.columns))
-        self.width = len(self.columns)
-        self.length = len(tuple(self.data.values())[0])
-        self.reader = (
-            ColumnarReader(self.data) if _columnar else RowReader(self.columns)
-        )
-        self.range_reader = RangeReader(self.data)
+    def __init__(self, *columns, rows=None):
+        self.columns = tuple(columns if isinstance(columns[0], str) else columns[0])
+        self.rows = rows or []
+        if rows:
+            assert len(rows[0]) == len(self.columns)
+        self.reader = RowReader(self.columns)
+        self.range_reader = RangeReader(self)
 
-    def add(self, row):
-        for i in range(self.width):
-            self.data[self.column_indicies[i]].append(row[i])
-        self.length += 1
+    def append(self, row):
+        assert len(row) == len(self.columns)
+        self.rows.append(row)
 
     def pop(self):
-        for column in self.data.values():
-            column.pop()
-        self.length -= 1
+        self.rows.pop()
+
+    @property
+    def width(self):
+        return len(self.columns)
+
+    def __len__(self):
+        return len(self.rows)
 
     def __iter__(self):
         return TableIter(self)
 
-    def __getitem__(self, column):
-        return self.data[column]
+    def __getitem__(self, index):
+        self.reader.row = self.rows[index]
+        return self.reader
 
     def __repr__(self):
-        widths = {column: len(column) for column in self.data}
-        lines = [" ".join(column for column in self.data)]
+        widths = {column: len(column) for column in self.columns}
+        lines = [" ".join(column for column in self.columns)]
 
         for i, row in enumerate(self):
             if i > 10:
@@ -40,15 +39,15 @@ class Table:
             lines.append(
                 " ".join(
                     str(row[column]).rjust(widths[column])[0 : widths[column]]
-                    for column in self.data
+                    for column in self.columns
                 )
             )
         return "\n".join(lines)
 
 
 class TableIter:
-    def __init__(self, data_table):
-        self.data_table = data_table
+    def __init__(self, table):
+        self.table = table
         self.index = -1
 
     def __iter__(self):
@@ -56,9 +55,8 @@ class TableIter:
 
     def __next__(self):
         self.index += 1
-        if self.index < self.data_table.length:
-            self.data_table.reader.row = self.index
-            return self.data_table.reader
+        if self.index < len(self.table):
+            return self.table[self.index]
         raise StopIteration
 
 
@@ -71,28 +69,13 @@ class RangeReader:
         return len(self.range)
 
     def __getitem__(self, column):
-        return (self.table[column][i] for i in self.range)
-
-
-class ColumnarReader:
-    def __init__(self, table):
-        self.table = table
-        self.row = None
-
-    def tuple(self):
-        return tuple(self[column] for column in self.table)
-
-    def __getitem__(self, column):
-        return self.table[column][self.row]
+        return (self.table[i][column] for i in self.range)
 
 
 class RowReader:
     def __init__(self, columns):
         self.columns = {column: i for i, column in enumerate(columns)}
         self.row = None
-
-    def tuple(self):
-        return tuple(self.row)
 
     def __getitem__(self, column):
         return self.row[self.columns[column]]

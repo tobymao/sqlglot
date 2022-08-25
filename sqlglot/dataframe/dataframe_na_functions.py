5e259c2a8a9b630c6a65184461d16fbf14c73d55
reakman <reakman@netflix.com>
2022-08-24 21:21:07 -0700

    add replace support

--- sqlglot/dataframe/dataframe_na_functions.py
+++ sqlglot/dataframe/dataframe_na_functions.py
@@ -16,3 +16,8 @@ class DataFrameNaFunctions:
              value: t.Union[int, bool, float, str, t.Dict[str, t.Any]],
              subset: t.Optional[t.Union[str, t.Tuple[str, ...], t.List[str]]] = None) -> "DataFrame":
         return self.df.fillna(value=value, subset=subset)
+
+    def replace(self, to_replace: t.Union[bool, int, float, str, t.List, t.Dict],
+                value: t.Optional[t.Union[bool, int, float, str, t.List]] = None,
+                subset: t.Optional[t.Union[str, t.List[str]]] = None) -> "DataFrame":
+        return self.df.replace(to_replace=to_replace, value=value, subset=subset)

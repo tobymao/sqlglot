3cacd97f8de67bd203cf35d1b40e67d548ed7a8d
Mike <mpf82@users.noreply.github.com>
2022-08-29 15:04:40 +0200

    improve oracle support
    improve oracle support

--- sqlglot/dialects/oracle.py
+++ sqlglot/dialects/oracle.py
@@ -1,9 +1,13 @@
 from sqlglot import exp
 from sqlglot.dialects.dialect import Dialect, no_ilike_sql
 from sqlglot import transforms
+from sqlglot.helper import csv
 from sqlglot.generator import Generator
 from sqlglot.tokens import Tokenizer, TokenType
 
+
+def _limit_sql(self, expression):
+    return self.fetch_sql(exp.Fetch(direction="FIRST", count=expression.expression))
 
 class Oracle(Dialect):
     class Generator(Generator):
@@ -20,7 +24,30 @@ class Oracle(Dialect):
             **Generator.TRANSFORMS,
             **transforms.UNALIAS_GROUP,
             exp.ILike: no_ilike_sql,
+            exp.Limit: _limit_sql,
         }
+
+        def query_modifiers(self, expression, *sqls):
+            return csv(
+                *sqls,
+                *[self.sql(sql) for sql in expression.args.get("laterals", [])],
+                *[self.sql(sql) for sql in expression.args.get("joins", [])],
+                self.sql(expression, "where"),
+                self.sql(expression, "group"),
+                self.sql(expression, "having"),
+                self.sql(expression, "qualify"),
+                self.sql(expression, "window"),
+                self.sql(expression, "distribute"),
+                self.sql(expression, "sort"),
+                self.sql(expression, "cluster"),
+                self.sql(expression, "order"),
+                self.sql(expression, "offset"), # offset before limit in oracle
+                self.sql(expression, "limit"),
+                sep="",
+            )
+
+        def offset_sql(self, expression):
+            return f"{super().offset_sql(expression)} ROWS"
 
     class Tokenizer(Tokenizer):
         KEYWORDS = {

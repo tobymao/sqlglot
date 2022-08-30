c57ef24fe85acaf2aa4d48003a027c6091b5c7fc
barak <barak.alon@airbnb.com>
2022-08-29 17:37:17 -0700

    MySQL double quotes (#376)

--- tests/dialects/test_mysql.py
+++ tests/dialects/test_mysql.py
@@ -6,3 +6,11 @@ class TestMySQL(Validator):
 
     def test_identity(self):
         self.validate_identity("SELECT CAST(`a`.`b` AS INT) FROM foo")
+
+    def test_string_literals(self):
+        self.validate(
+            'SELECT "2021-01-01" + INTERVAL 1 MONTH',
+            "SELECT '2021-01-01' + INTERVAL 1 MONTH",
+            read="mysql",
+            write="mysql",
+        )

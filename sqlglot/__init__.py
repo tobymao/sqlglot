e5ea0e69e36c55341d7c15d9c631bd27deae5f24
barak_alon <barak.alon@airbnb.com>
2022-09-15 10:26:09 -0700

    v6.0.5

--- sqlglot/__init__.py
+++ sqlglot/__init__.py
@@ -20,7 +20,7 @@ from sqlglot.generator import Generator
 from sqlglot.parser import Parser
 from sqlglot.tokens import Tokenizer, TokenType
 
-__version__ = "6.0.4"
+__version__ = "6.0.5"
 
 pretty = False
 

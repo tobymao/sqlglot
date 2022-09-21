33576b5b834ec4d99ab8981127a4cb9847a677e7
GeorgeSittas <giwrgos.sittas@gmail.com>
2022-09-22 00:48:32 +0300

    Add delimited identifier test

--- tests/dialects/test_transactsql.py
+++ tests/dialects/test_transactsql.py
@@ -4,6 +4,17 @@ from tests.dialects.test_dialect import Validator
 class TestTransactSQL(Validator):
     dialect = "transactsql"
 
+    def test_transactsql(self):
+        self.validate_identity('SELECT "x"."y" FROM foo')
+
+        self.validate_all(
+            "SELECT CAST([a].[b] AS SMALLINT) FROM foo",
+            write={
+                "transactsql": 'SELECT CAST("a"."b" AS SMALLINT) FROM foo',
+                "spark": "SELECT CAST(`a`.`b` AS SHORT) FROM foo",
+            },
+        )
+
     def test_types(self):
         self.validate_identity("CAST(x AS XML)")
         self.validate_identity("CAST(x AS UNIQUEIDENTIFIER)")

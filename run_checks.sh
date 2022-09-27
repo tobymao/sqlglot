8b99e4a9963ad69be8cc92c85ce27c4985c3d236
reakman <reakman@netflix.com>
2022-09-27 15:45:10 -0700

    quiet autoflake and add black return code

--- run_checks.sh
+++ run_checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-python -m autoflake -i -r -c \
+python -m autoflake -i -r -c --quiet \
   --expand-star-imports \
   --remove-all-unused-imports \
   --ignore-init-module-imports \
@@ -8,5 +8,5 @@ python -m autoflake -i -r -c \
   --remove-unused-variables \
   sqlglot/ tests/
 python -m isort --profile black sqlglot/ tests/
-python -m black --line-length 120 sqlglot/ tests/
+python -m black --check --line-length 120 sqlglot/ tests/
 python -m unittest

d72aff2904889a4abec0eef7fe47a7d63d4373e8
Ryan Eakman <eakmanrq@gmail.com>
2022-09-27 09:16:19 -0700

    Improve unalias group by transform (#488)
    * improve unalias to handle alias matching column name
    
    * fix typo
    
    * fix bad variable name
    
    * do a dfs walk instead of going up and down
    
    Co-authored-by: reakman <reakman@netflix.com>

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -12,5 +12,25 @@ class TestTime(unittest.TestCase):
         self.validate(
             unalias_group,
             "SELECT a, b AS b, c AS c, 4 FROM x GROUP BY a, b, x.c, 4",
-            "SELECT a, b AS b, c AS c, 4 FROM x GROUP BY a, 2, x.c, 4",
+            "SELECT a, b AS b, c AS c, 4 FROM x GROUP BY a, b, x.c, 4",
+        )
+        self.validate(
+            unalias_group,
+            "SELECT TO_DATE(the_date) AS the_date, CUSTOM_UDF(other_col) AS other_col, last_col AS aliased_last, COUNT(*) AS the_count FROM x GROUP BY TO_DATE(the_date), CUSTOM_UDF(other_col), aliased_last",
+            "SELECT TO_DATE(the_date) AS the_date, CUSTOM_UDF(other_col) AS other_col, last_col AS aliased_last, COUNT(*) AS the_count FROM x GROUP BY TO_DATE(the_date), CUSTOM_UDF(other_col), 3",
+        )
+        self.validate(
+            unalias_group,
+            "SELECT SOME_UDF(TO_DATE(the_date)) AS the_date, COUNT(*) AS the_count FROM x GROUP BY SOME_UDF(TO_DATE(the_date))",
+            "SELECT SOME_UDF(TO_DATE(the_date)) AS the_date, COUNT(*) AS the_count FROM x GROUP BY SOME_UDF(TO_DATE(the_date))",
+        )
+        self.validate(
+            unalias_group,
+            "SELECT SOME_UDF(TO_DATE(the_date)) AS new_date, COUNT(*) AS the_count FROM x GROUP BY new_date",
+            "SELECT SOME_UDF(TO_DATE(the_date)) AS new_date, COUNT(*) AS the_count FROM x GROUP BY 1",
+        )
+        self.validate(
+            unalias_group,
+            "SELECT the_date AS the_date, COUNT(*) AS the_count FROM x GROUP BY the_date",
+            "SELECT the_date AS the_date, COUNT(*) AS the_count FROM x GROUP BY the_date",
         )

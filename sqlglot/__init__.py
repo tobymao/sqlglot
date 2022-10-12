71049a3833dfb08239cd24c11bfe7dd1e0c71f1e
tobymao <toby.mao@gmail.com>
2022-10-11 19:01:43 -0700

    v6.3.5

--- sqlglot/__init__.py
+++ sqlglot/__init__.py
@@ -23,7 +23,7 @@ from sqlglot.generator import Generator
 from sqlglot.parser import Parser
 from sqlglot.tokens import Tokenizer, TokenType
 
-__version__ = "6.3.4"
+__version__ = "6.3.5"
 
 pretty = False
 

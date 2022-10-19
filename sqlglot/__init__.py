0896cf00037fbf07bf7e47fecf9fd993e2e1d62a
tobymao <toby.mao@gmail.com>
2022-10-19 15:41:40 -0700

    v8.0.3

--- sqlglot/__init__.py
+++ sqlglot/__init__.py
@@ -24,7 +24,7 @@ from sqlglot.parser import Parser
 from sqlglot.schema import MappingSchema
 from sqlglot.tokens import Tokenizer, TokenType
 
-__version__ = "8.0.2"
+__version__ = "8.0.3"
 
 pretty = False
 

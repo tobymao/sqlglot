f7091feaae2af6262670ad15f44f30b43e327811
Sebastiaan Fransen <s.fransen@vanlanschotkempen.com>
2022-10-21 14:31:38 +0200

    Added support for convert with all format conversions

--- sqlglot/dialects/tsql.py
+++ sqlglot/dialects/tsql.py
@@ -1,13 +1,80 @@
 from sqlglot import exp
 from sqlglot.dialects.dialect import Dialect
 from sqlglot.generator import Generator
+from sqlglot.expressions import DataType
 from sqlglot.parser import Parser
+from sqlglot.helper import list_get
 from sqlglot.tokens import Tokenizer, TokenType
 
 
 class TSQL(Dialect):
     null_ordering = "nulls_are_small"
     time_format = "'yyyy-mm-dd hh:mm:ss'"
+
+    time_mapping = {
+        "y": "%Y",
+        "Y": "%Y",
+        "YYYY": "%Y",
+        "yyyy": "%Y",
+        "YY": "%y",
+        "yy": "%y",
+        "MMMM": "%B",
+        "MMM": "%b",
+        "MM": "%m",
+        "M": "%-m",
+        "dd": "%d",
+        "d": "%-d",
+        "HH": "%H",
+        "H": "%-H",
+        "hh": "%I",
+        "h": "%-I",
+        "mm": "%M",
+        "m": "%-M",
+        "ss": "%S",
+        "s": "%-S",
+        "S": "%f",
+    }
+
+    convert_format_mapping = {
+        0: "%b %d %Y %-I:%M%p",
+        1: "%m/%d/%y",
+        2: "%y.%m.%d",
+        3: "%d/%m/%y",
+        4: "%d.%m.%y",
+        5: "%d-%m-%y",
+        6: "%d %b %y",
+        7: "%b %d, %y",
+        8: "%H:%M:%S",
+        9: "%b %d %Y %-I:%M:%S:%f%p",
+        10: "mm-dd-yy",
+        11: "yy/mm/dd",
+        12: "yymmdd",
+        13: "%d %b %Y %H:%M:ss:%f",
+        14: "%H:%M:%S:%f",
+        20: "%Y-%m-%d %H:%M:%S",
+        21: "%Y-%m-%d %H:%M:%S.%f",
+        22: "%m/%d/%y %-I:%M:%S %p",
+        23: "%Y-%m-%d",
+        24: "%H:%M:%S",
+        25: "%Y-%m-%d %H:%M:%S.%f",
+        100: "%b %d %Y %-I:%M%p",
+        101: "%m/%d/%Y",
+        102: "%Y.%m.%d",
+        103: "%d/%m/%Y",
+        104: "%d.%m.%Y",
+        105: "%d-%m-%Y",
+        106: "%d %b %Y",
+        107: "%b %d, %Y",
+        108: "%H:%M:%S",
+        109: "%b %d %Y %-I:%M:%S:%f%p",
+        110: "%m-%d-%Y",
+        111: "%Y/%m/%d",
+        112: "%Y%m%d",
+        113: "%d %b %Y %H:%M:%S:%f",
+        114: "%H:%M:%S:%f",
+        120: "%Y-%m-%d %H:%M:%S",
+        121: "%Y-%m-%d %H:%M:%S.%f",
+    }
 
     class Tokenizer(Tokenizer):
         IDENTIFIERS = ['"', ("[", "]")]
@@ -29,6 +96,10 @@ class TSQL(Dialect):
             "UNIQUEIDENTIFIER": TokenType.UNIQUEIDENTIFIER,
             "XML": TokenType.XML,
             "SQL_VARIANT": TokenType.VARIANT,
+            "NVARCHAR(MAX)": TokenType.TEXT,
+            "VARCHAR(MAX)": TokenType.TEXT,
+            "NVARCHAR": TokenType.VARCHAR,
+            "NCHAR": TokenType.CHAR,
         }
 
     class Parser(Parser):
@@ -37,12 +108,60 @@ class TSQL(Dialect):
             "CHARINDEX": exp.StrPosition.from_arg_list,
         }
 
-
-        def _parse_convert(self):
+        def _parse_convert(self, strict):
             to = self._parse_types()
             self._match(TokenType.COMMA)
             this = self._parse_field()
-            return self.expression(exp.Cast, this=this, to=to)
+
+            var_length_datatype = [
+                DataType.Type.NVARCHAR,
+                DataType.Type.VARCHAR,
+                DataType.Type.CHAR,
+                DataType.Type.NCHAR
+            ]
+
+            # Retrieve length of datatype and override to default if not specified
+            if list_get(to.expressions, 0) is None and to.this in var_length_datatype:
+                to = exp.DataType.build(to.this, expressions=[exp.Literal(this="30", is_string=False)], nested=False)
+
+            # Check whether a conversion with format is applicable
+            if self._match(TokenType.COMMA):
+                format_norm = TSQL.convert_format_mapping[int(self._parse_number().this)]
+
+                # Check whether the convert entails a string to date format
+                if to.this in [DataType.Type.DATE]:
+                    return self.expression(
+                        exp.StrToDate,
+                        this=this,
+                        format=exp.Literal(this=format_norm, is_string=True)
+                    )
+                # Check whether the convert entails a string to datetime format
+                elif to.this in [DataType.Type.DATETIME]:
+                    return self.expression(
+                        exp.StrToTime,
+                        this=this,
+                        format=exp.Literal(this=format_norm, is_string=True)
+                    )
+                # Check whether the convert entails a date to string format
+                elif to.this in var_length_datatype:
+                    return self.expression(
+                        exp.Cast if strict else exp.TryCast,
+                        to = to,
+                        this = self.expression(
+                            exp.TimeToStr,
+                            this=this,
+                            format=exp.Literal(this=format_norm, is_string=True)
+                        )
+                    )
+                elif to.this == DataType.Type.TEXT:
+                    return self.expression(
+                        exp.TimeToStr,
+                        this=this,
+                        format=exp.Literal(this=format_norm, is_string=True)
+                    )
+
+            # Entails a simple cast without any format requirement
+            return self.expression(exp.Cast if strict else exp.TryCast, this=this, to=to)
 
 
     class Generator(Generator):
--- sqlglot/parser.py
+++ sqlglot/parser.py
@@ -432,7 +432,8 @@ class Parser:
     }
 
     FUNCTION_PARSERS = {
-        "CONVERT": lambda self: self._parse_convert(),
+        "CONVERT": lambda self: self._parse_convert(self.STRICT_CAST),
+        "TRY_CONVERT": lambda self: self._parse_convert(False),
         "EXTRACT": lambda self: self._parse_extract(),
         "POSITION": lambda self: self._parse_position(),
         "SUBSTRING": lambda self: self._parse_substring(),
@@ -2133,7 +2134,7 @@ class Parser:
 
         return self.expression(exp.Cast if strict else exp.TryCast, this=this, to=to)
 
-    def _parse_convert(self):
+    def _parse_convert(self, strict):
         this = self._parse_field()
         if self._match(TokenType.USING):
             to = self.expression(exp.CharacterSet, this=self._parse_var())

3d321139499258cc3a2e5504048745ceec4720a8
Tim Zhou <5866950+ttzhou@users.noreply.github.com>
2022-10-27 21:10:20 -0400

    More complete transient test for Snowflake

--- tests/dialects/test_snowflake.py
+++ tests/dialects/test_snowflake.py
@@ -292,13 +292,17 @@ class TestSnowflake(Validator):
         self.validate_identity(
             "CREATE TABLE a (x DATE, y BIGINT) WITH (PARTITION BY (x), integration='q', auto_refresh=TRUE, file_format=(type = parquet))"
         )
-        self.validate_identity("CREATE OR REPLACE TRANSIENT TABLE a (x DATE, y BIGINT)")
         self.validate_identity("CREATE MATERIALIZED VIEW a COMMENT='...' AS SELECT 1 FROM x")
         self.validate_all(
             "CREATE OR REPLACE TRANSIENT TABLE a (id INT)",
+            read={
+                "postgres": "CREATE OR REPLACE TRANSIENT TABLE a (id INT)",
+                "snowflake": "CREATE OR REPLACE TRANSIENT TABLE a (id INT)",
+            },
             write={
                 "postgres": "CREATE OR REPLACE TABLE a (id INT)",
                 "mysql": "CREATE OR REPLACE TABLE a (id INT)",
+                "snowflake": "CREATE OR REPLACE TRANSIENT TABLE a (id INT)",
             },
         )
 

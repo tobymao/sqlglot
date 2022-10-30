819a279f75329e526aab9404fd067e6ca5440ba3
GeorgeSittas <giwrgos.sittas@gmail.com>
2022-10-31 01:33:14 +0200

    refactor tests

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -6,9 +6,13 @@ from sqlglot.tokens import Tokenizer
 class TestTokens(unittest.TestCase):
     def test_comment_attachment(self):
         tokenizer = Tokenizer()
+        sql_comment = [
+            ("/*comment*/ foo", "comment"),
+            ("/*comment*/ foo --test", "comment"),
+            ("--comment\nfoo --test", "comment"),
+            ("foo --comment", "comment"),
+            ("foo", None),
+        ]
 
-        sqls = ["/*comment*/ foo", "/*comment*/ foo --test", "--comment\nfoo --test", "foo --comment", "foo"]
-        comments = ["comment", "comment", "comment", "comment", None]
-
-        for sql, comment in zip(sqls, comments):
+        for sql, comment in sql_comment:
             self.assertEqual(tokenizer.tokenize(sql)[0].comment, comment)

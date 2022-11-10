0db7a089bb5771070f1f534690ce45aec759f430
Chris Ginter <chris_ginter@airbnb.com>
2022-11-10 11:14:03 -0800

    v10.0.1

--- sqlglot/__init__.py
+++ sqlglot/__init__.py
@@ -30,7 +30,7 @@ from sqlglot.parser import Parser
 from sqlglot.schema import MappingSchema
 from sqlglot.tokens import Tokenizer, TokenType
 
-__version__ = "10.0.0"
+__version__ = "10.0.1"
 
 pretty = False
 

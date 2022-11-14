52719ee7fd73a815766038dc79177b1be7af0f03
tobymao <toby.mao@gmail.com>
2022-11-14 12:33:23 -0800

    cleanup

--- tests/fixtures/optimizer/canonicalize.sql
+++ tests/fixtures/optimizer/canonicalize.sql
@@ -1,4 +1,5 @@
 SELECT w.d + w.e AS c FROM w AS w;
 SELECT CONCAT(w.d, w.e) AS c FROM w AS w;
+
 SELECT CAST(w.d AS DATE) > w.e AS a FROM w AS w;
 SELECT CAST(w.d AS DATE) > CAST(w.e AS DATE) AS a FROM w AS w;

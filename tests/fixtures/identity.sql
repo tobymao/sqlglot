bc7bba5bccf644a265fcf5ba71bc67585f7855e8
George Sittas <giwrgos.sittas@gmail.com>
2022-11-25 18:13:40 +0200

    Add a test case for using INTO as an alias

--- tests/fixtures/identity.sql
+++ tests/fixtures/identity.sql
@@ -591,6 +591,7 @@ SELECT x FROM a.b.c /* x */, e.f.g /* x */
 SELECT FOO(x /* c */) /* FOO */, b /* b */
 SELECT FOO(x /* c1 */ + y /* c2 */ + BLA(5 /* c3 */)) FROM VALUES (1 /* c4 */, "test" /* c5 */) /* c6 */
 SELECT a FROM x WHERE a COLLATE 'utf8_general_ci' = 'b'
+SELECT x AS INTO FROM bla
 SELECT * INTO newevent FROM event
 SELECT * INTO TEMPORARY newevent FROM event
 SELECT * INTO UNLOGGED newevent FROM event

c6acfe3cc9cc0d32fc19fa6f5cfafa358b37e09b
barak_alon <barak.alon@airbnb.com>
2022-11-28 17:10:29 -0800

    fixup

--- sqlglot/optimizer/eliminate_subqueries.py
+++ sqlglot/optimizer/eliminate_subqueries.py
@@ -163,12 +163,10 @@ def _eliminate_cte(scope, existing_ctes, taken):
 
 def _new_cte(scope, existing_ctes, taken):
     """
-    Get a new name for a CTE.
-
     Returns:
-        tuple of (name, cte), where:
-        - name: Name for this CTE in the root scope
-        - cte: New CTE instance. If this CTE duplicates an existing CTE, this is None.
+        tuple of (name, cte)
+        where `name` is a new name for this CTE in the root scope and `cte` is a new CTE instance.
+        If this CTE duplicates an existing CTE, `cte` will be None.
     """
     duplicate_cte_alias = existing_ctes.get(scope.expression)
     parent = scope.expression.parent

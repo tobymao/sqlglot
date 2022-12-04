3dbbcd5238d77a955451bd72c6f2f4ebff7bd970
Toby Mao <toby.mao@gmail.com>
2022-12-04 10:10:04 -0800

    support minus alias for except

--- sqlglot/dialects/snowflake.py
+++ sqlglot/dialects/snowflake.py
@@ -198,6 +198,7 @@ class Snowflake(Dialect):
             "TIMESTAMP_NTZ": TokenType.TIMESTAMP,
             "TIMESTAMP_TZ": TokenType.TIMESTAMPTZ,
             "TIMESTAMPNTZ": TokenType.TIMESTAMP,
+            "MINUS": TokenType.EXCEPT,
             "SAMPLE": TokenType.TABLE_SAMPLE,
         }
 

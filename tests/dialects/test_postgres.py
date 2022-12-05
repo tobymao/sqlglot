0f20105947ae1a83483c144a725d14aef5e8e3c2
Yuchao <fanyuchao01@gmail.com>
2022-12-05 18:44:45 +0000

    allow table alias for UPDATE

--- tests/dialects/test_postgres.py
+++ tests/dialects/test_postgres.py
@@ -94,6 +94,7 @@ class TestPostgres(Validator):
         self.validate_identity("COMMENT ON TABLE mytable IS 'this'")
         self.validate_identity("SELECT e'\\xDEADBEEF'")
         self.validate_identity("SELECT CAST(e'\\176' AS BYTEA)")
+        self.validate_identity("UPDATE MYTABLE AS T1 SET T1.COL = 13")
 
         self.validate_all(
             "END WORK AND NO CHAIN",

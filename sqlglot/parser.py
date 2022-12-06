0b521f54aa093ee275d161599df3534094e5a1bd
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2022-12-06 02:22:11 +0200

    Fix extract for bigquery (#806)

--- sqlglot/parser.py
+++ sqlglot/parser.py
@@ -2241,7 +2241,7 @@ class Parser(metaclass=_Parser):
         return self._parse_window(this)
 
     def _parse_extract(self):
-        this = self._parse_var() or self._parse_type()
+        this = self._parse_function() or self._parse_var() or self._parse_type()
 
         if self._match(TokenType.FROM):
             return self.expression(exp.Extract, this=this, expression=self._parse_bitwise())

4472d5c3961206ea248c3a4074b9d20a2ca46717
eakmanrq <eakmanrq@gmail.com>
2022-12-14 12:13:29 -0800

    fmt

--- tests/test_build.py
+++ tests/test_build.py
@@ -474,7 +474,9 @@ class TestBuild(unittest.TestCase):
             (lambda: exp.values([("1", 2)], "alias"), "(VALUES ('1', 2)) AS alias"),
             (lambda: exp.values([("1", 2), ("2", 3)]), "VALUES ('1', 2), ('2', 3)"),
             (
-                lambda: exp.values([("1", 2, None), ("2", 3, None)], "alias", ["col1", "col2", "col3"]),
+                lambda: exp.values(
+                    [("1", 2, None), ("2", 3, None)], "alias", ["col1", "col2", "col3"]
+                ),
                 "(VALUES ('1', 2, NULL), ('2', 3, NULL)) AS alias(col1, col2, col3)",
             ),
             (lambda: exp.delete("y", where="x > 1"), "DELETE FROM y WHERE x > 1"),

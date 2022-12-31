6bcf14543ace359becb046eb0d978094ec9aee14
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2022-12-30 18:12:01 -0800

    cleanup after rebase

--- sqlglot/optimizer/quote_identities.py
+++ sqlglot/optimizer/quote_identities.py
@@ -22,10 +22,11 @@ def quote_identities(expression, unquote_values_columns=False):
 
     Args:
         expression (sqlglot.Expression): expression to quote
-        unquote_values_columns (bool):
+        unquote_values_columns (bool): Indicates whether to unquote values defined in a VALUES expression.
     Returns:
         sqlglot.Expression: quoted expression
     """
+
     def qualify(node):
         if isinstance(node, exp.Identifier):
             node.set("quoted", True)
@@ -37,17 +38,23 @@ def quote_identities(expression, unquote_values_columns=False):
 
     for scope in traverse_scope(quoted_expression):
         if isinstance(scope.expression, exp.Values):
-            alias = scope.expression.args.get('alias')
+            alias = scope.expression.args.get("alias")
             if not alias:
                 continue
             for column in alias.args.get("columns", []):
                 column.set("quoted", False)
         if isinstance(scope.expression, exp.Select):
-            values_scopes = [scope for scope in scope.sources.values() if isinstance(scope.expression, exp.Values)]
+            values_scopes = [
+                scope
+                for scope in scope.sources.values()
+                if isinstance(scope.expression, exp.Values)
+            ]
             values_columns = []
             for value_scope in values_scopes:
                 values_columns.extend(value_scope.outer_column_list)
-            selected_value_columns = [sel_column for sel_column in scope.columns if sel_column.name in values_columns]
+            selected_value_columns = [
+                sel_column for sel_column in scope.columns if sel_column.name in values_columns
+            ]
             for selected_value_column in selected_value_columns:
                 selected_value_column.this.set("quoted", False)
     return quoted_expression

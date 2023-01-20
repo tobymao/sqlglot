78a09ee55c4f4f83457c9600693b79167f3cb690
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2023-01-20 08:53:44 -0800

    v10.5.6

--- sqlglot/__init__.py
+++ sqlglot/__init__.py
@@ -32,7 +32,7 @@ from sqlglot.parser import Parser
 from sqlglot.schema import MappingSchema
 from sqlglot.tokens import Tokenizer, TokenType
 
-__version__ = "10.5.5"
+__version__ = "10.5.6"
 
 pretty = False
 

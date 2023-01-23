73a40759177b63b6f6518632c0afc0fa91af4cd7
George Sittas <giwrgos.sittas@gmail.com>
2023-01-23 18:41:10 +0200

    Add support for the ADD CONSTRAINT clause in ALTER TABLE

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -2292,10 +2292,17 @@ class Rollback(Expression):
 
 
 class AlterTable(Expression):
+    arg_types = {"this": True, "actions": True, "exists": False}
+
+
+class AddConstraint(Expression):
     arg_types = {
-        "this": True,
-        "actions": True,
-        "exists": False,
+        "this": False,
+        "expression": False,  # Only used in the CHECK constraint
+        "primary": False,  # None: CHECK, False: FOREIGN KEY, True: PRIMARY KEY
+        "columns": False,
+        "references": False,
+        "options": False,
     }
 
 

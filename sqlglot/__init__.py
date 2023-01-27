242c6734e1fe7c3aa0cc1b1f8779bd1b46b3fc4a
George Sittas <giwrgos.sittas@gmail.com>
2023-01-27 22:08:01 +0200

    v10.5.8

--- sqlglot/__init__.py
+++ sqlglot/__init__.py
@@ -33,7 +33,7 @@ from sqlglot.parser import Parser
 from sqlglot.schema import MappingSchema, Schema
 from sqlglot.tokens import Tokenizer, TokenType
 
-__version__ = "10.5.7"
+__version__ = "10.5.8"
 
 pretty = False
 """Whether to format generated SQL by default."""

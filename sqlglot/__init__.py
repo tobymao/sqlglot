6a5a24aee7c843e6519749537792470fbcb333ca
tobymao <toby.mao@gmail.com>
2023-02-08 11:53:54 -0800

    v10.6.4

--- sqlglot/__init__.py
+++ sqlglot/__init__.py
@@ -40,7 +40,7 @@ if t.TYPE_CHECKING:
     T = t.TypeVar("T", bound=Expression)
 
 
-__version__ = "10.6.3"
+__version__ = "10.6.4"
 
 pretty = False
 """Whether to format generated SQL by default."""

f3f8e879c2c91b5625096a5ab46a34f3ebb47191
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2023-02-07 22:35:09 -0800

    Add more type hints, make some small fixes (#1117)
    * Add more type hints, make some small fixes
    
    * PR feedback
    
    * Fixup

--- sqlglot/errors.py
+++ sqlglot/errors.py
@@ -7,10 +7,17 @@ from sqlglot.helper import AutoName
 
 
 class ErrorLevel(AutoName):
-    IGNORE = auto()  # Ignore any parser errors
-    WARN = auto()  # Log any parser errors with ERROR level
-    RAISE = auto()  # Collect all parser errors and raise a single exception
-    IMMEDIATE = auto()  # Immediately raise an exception on the first parser error
+    IGNORE = auto()
+    """Ignore all errors."""
+
+    WARN = auto()
+    """Log all errors."""
+
+    RAISE = auto()
+    """Collect all errors and raise a single exception."""
+
+    IMMEDIATE = auto()
+    """Immediately raise an exception on the first error found."""
 
 
 class SqlglotError(Exception):

3042386f30f23a2e652498c215850faaac044ecb
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2023-02-24 01:55:45 +0200

    Fix transpilation of TsOrDsAdd for Bigquery (#1221)
    * Fix transpilation of TsOrDsAdd for Bigquery
    
    * Fixup
    
    * Remove quote from DAY in bigquery date_add sqlgen
    
    * exp.Var -> exp.var

--- sqlglot/dialects/bigquery.py
+++ sqlglot/dialects/bigquery.py
@@ -38,7 +38,10 @@ def _date_add_sql(
 ) -> t.Callable[[generator.Generator, exp.Expression], str]:
     def func(self, expression):
         this = self.sql(expression, "this")
-        return f"{data_type}_{kind}({this}, {self.sql(exp.Interval(this=expression.expression, unit=expression.args.get('unit') or exp.Literal.string('day')))})"
+        unit = expression.args.get("unit")
+        unit = exp.var(unit.name.upper() if unit else "DAY")
+        interval = exp.Interval(this=expression.expression, unit=unit)
+        return f"{data_type}_{kind}({this}, {self.sql(interval)})"
 
     return func
 
@@ -235,6 +238,7 @@ class BigQuery(Dialect):
             exp.TimestampSub: _date_add_sql("TIMESTAMP", "SUB"),
             exp.TimeStrToTime: timestrtotime_sql,
             exp.TsOrDsToDate: ts_or_ds_to_date_sql("bigquery"),
+            exp.TsOrDsAdd: _date_add_sql("DATE", "ADD"),
             exp.PartitionedByProperty: lambda self, e: f"PARTITION BY {self.sql(e, 'this')}",
             exp.VariancePop: rename_func("VAR_POP"),
             exp.Values: _derived_table_values_to_unnest,

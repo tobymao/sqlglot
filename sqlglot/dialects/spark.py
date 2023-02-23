711dc8b0bd9532c59ac0b3f350b73481fe316686
Trey Spiller <treyspiller@gmail.com>
2023-02-23 10:34:20 -0600

    Fix spark _create_sql round 2

--- sqlglot/dialects/spark.py
+++ sqlglot/dialects/spark.py
@@ -8,10 +8,11 @@ from sqlglot.helper import seq_get
 
 def _create_sql(self, e):
     kind = e.args.get("kind")
+    properties = e.args.get("properties")
 
     if kind.upper() == "TABLE" and any(
         isinstance(prop, exp.TemporaryProperty)
-        for prop in e.args.get("properties", exp.Properties(expressions=[])).expressions
+        for prop in (properties.expressions if properties else [])
     ):
         return f"CREATE TEMPORARY VIEW {self.sql(e, 'this')} AS {self.sql(e, 'expression')}"
     return create_with_partitions_sql(self, e)

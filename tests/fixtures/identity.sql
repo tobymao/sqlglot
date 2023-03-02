46da588b7c6ed8e3c1544befecc375f8aec67d62
tobymao <toby.mao@gmail.com>
2023-03-02 08:47:41 -0800

    comment on command fixes #1239

--- tests/fixtures/identity.sql
+++ tests/fixtures/identity.sql
@@ -674,6 +674,7 @@ UPDATE db.tbl_name SET foo = 123, foo_1 = 234 WHERE tbl_name.bar = 234
 TRUNCATE TABLE x
 OPTIMIZE TABLE y
 VACUUM FREEZE my_table
+COMMENT ON PROCEDURE my_proc (integer, integer) IS 'Runs a report'
 WITH a AS (SELECT 1) INSERT INTO b SELECT * FROM a
 WITH a AS (SELECT * FROM b) UPDATE a SET col = 1
 WITH a AS (SELECT * FROM b) CREATE TABLE b AS SELECT * FROM a

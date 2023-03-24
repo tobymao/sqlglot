b80e634a69c08a86feaf919d90f67dd4f14550cc
George Sittas <giwrgos.sittas@gmail.com>
2023-03-24 16:04:41 +0200

    Fix tokenizing of T-SQL parameters

--- sqlglot/dialects/tsql.py
+++ sqlglot/dialects/tsql.py
@@ -269,7 +269,6 @@ class TSQL(Dialect):
 
         # TSQL allows @, # to appear as a variable/identifier prefix
         SINGLE_TOKENS = tokens.Tokenizer.SINGLE_TOKENS.copy()
-        SINGLE_TOKENS.pop("@")
         SINGLE_TOKENS.pop("#")
 
     class Parser(parser.Parser):

c86b0e708803ed644d898d542ba7d70ac908f40f
Vincent Chan <vchan@users.noreply.github.com>
2023-04-07 11:52:49 -0700

    Update sqlglot/lineage.py
    Co-authored-by: Jo <46752250+GeorgeSittas@users.noreply.github.com>

--- sqlglot/lineage.py
+++ sqlglot/lineage.py
@@ -71,7 +71,7 @@ def lineage(
     optimized = optimize(expression, schema=schema, rules=rules)
     scope = build_scope(optimized)
     tables: t.Dict[str, Node] = {}
-    aliases = {dt.alias: dt.comments[0][len("source: ") :] for dt in scope.derived_tables}
+    aliases = {dt.alias: dt.comments[0].split()[1] for dt in scope.derived_tables}
 
     def to_node(
         column_name: str,

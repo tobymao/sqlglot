3e31916aada8eb7cbff301a7d8c217591d3d0588
Vincent Chan <vchan@users.noreply.github.com>
2023-04-14 15:01:12 -0700

    Fix: Expand as we traverse lineage (#1429)
    * Expand as we traverse lineage
    
    * Expand sources if possible
    
    * Expand sources in lineage
    
    * Set copy to False for source subquery transforms

--- tests/test_lineage.py
+++ tests/test_lineage.py
@@ -9,18 +9,82 @@ class TestLineage(unittest.TestCase):
     def test_lineage(self) -> None:
         node = lineage(
             "a",
-            "SELECT a FROM y",
+            "SELECT a FROM z",
+            schema={"x": {"a": "int"}},
+            sources={"y": "SELECT * FROM x", "z": "SELECT a FROM y"},
+        )
+        self.assertEqual(
+            node.source.sql(),
+            "SELECT z.a AS a FROM (SELECT y.a AS a FROM (SELECT x.a AS a FROM x AS x) AS y /* source: y */) AS z /* source: z */",
+        )
+        self.assertEqual(node.alias, "")
+
+        downstream = node.downstream[0]
+        self.assertEqual(
+            downstream.source.sql(),
+            "SELECT y.a AS a FROM (SELECT x.a AS a FROM x AS x) AS y /* source: y */",
+        )
+        self.assertEqual(downstream.alias, "z")
+
+        downstream = downstream.downstream[0]
+        self.assertEqual(
+            downstream.source.sql(),
+            "SELECT x.a AS a FROM x AS x",
+        )
+        self.assertEqual(downstream.alias, "y")
+        self.assertGreater(len(node.to_html()._repr_html_()), 1000)
+
+    def test_lineage_sql_with_cte(self) -> None:
+        node = lineage(
+            "a",
+            "WITH z AS (SELECT a FROM y) SELECT a FROM z",
             schema={"x": {"a": "int"}},
             sources={"y": "SELECT * FROM x"},
         )
         self.assertEqual(
             node.source.sql(),
-            "SELECT y.a AS a FROM (SELECT x.a AS a FROM x AS x) AS y /* source: y */",
+            "WITH z AS (SELECT y.a AS a FROM (SELECT x.a AS a FROM x AS x) AS y /* source: y */) SELECT z.a AS a FROM z",
         )
         self.assertEqual(node.alias, "")
+
+        # Node containing expanded CTE expression
+        downstream = node.downstream[0]
         self.assertEqual(
-            node.downstream[0].source.sql(),
+            downstream.source.sql(),
+            "SELECT y.a AS a FROM (SELECT x.a AS a FROM x AS x) AS y /* source: y */",
+        )
+        self.assertEqual(downstream.alias, "")
+
+        downstream = downstream.downstream[0]
+        self.assertEqual(
+            downstream.source.sql(),
             "SELECT x.a AS a FROM x AS x",
         )
-        self.assertEqual(node.downstream[0].alias, "y")
-        self.assertGreater(len(node.to_html()._repr_html_()), 1000)
+        self.assertEqual(downstream.alias, "y")
+
+    def test_lineage_source_with_cte(self) -> None:
+        node = lineage(
+            "a",
+            "SELECT a FROM z",
+            schema={"x": {"a": "int"}},
+            sources={"z": "WITH y AS (SELECT * FROM x) SELECT a FROM y"},
+        )
+        self.assertEqual(
+            node.source.sql(),
+            "SELECT z.a AS a FROM (WITH y AS (SELECT x.a AS a FROM x AS x) SELECT y.a AS a FROM y) AS z /* source: z */",
+        )
+        self.assertEqual(node.alias, "")
+
+        downstream = node.downstream[0]
+        self.assertEqual(
+            downstream.source.sql(),
+            "WITH y AS (SELECT x.a AS a FROM x AS x) SELECT y.a AS a FROM y",
+        )
+        self.assertEqual(downstream.alias, "z")
+
+        downstream = downstream.downstream[0]
+        self.assertEqual(
+            downstream.source.sql(),
+            "SELECT x.a AS a FROM x AS x",
+        )
+        self.assertEqual(downstream.alias, "")

3d62bcc1daad4944728766c3ee1a30d9c73f05f5
George Sittas <giwrgos.sittas@gmail.com>
2023-04-25 22:00:33 +0300

    Feat(databricks): add support for the : (colon sign) operator

--- sqlglot/dialects/databricks.py
+++ sqlglot/dialects/databricks.py
@@ -4,11 +4,14 @@ from sqlglot import exp
 from sqlglot.dialects.dialect import parse_date_delta
 from sqlglot.dialects.spark import Spark
 from sqlglot.dialects.tsql import generate_date_delta_with_unit_sql
+from sqlglot.parser import binary_range_parser
 from sqlglot.tokens import TokenType
 
 
 class Databricks(Spark):
     class Parser(Spark.Parser):
+        LOG_DEFAULTS_TO_LN = True
+
         FUNCTIONS = {
             **Spark.Parser.FUNCTIONS,
             "DATEADD": parse_date_delta(exp.DateAdd),
@@ -16,13 +19,17 @@ class Databricks(Spark):
             "DATEDIFF": parse_date_delta(exp.DateDiff),
         }
 
-        LOG_DEFAULTS_TO_LN = True
+        RANGE_PARSERS = {
+            **Spark.Parser.RANGE_PARSERS,
+            TokenType.COLON: binary_range_parser(exp.JSONExtract),
+        }
 
     class Generator(Spark.Generator):
         TRANSFORMS = {
             **Spark.Generator.TRANSFORMS,  # type: ignore
             exp.DateAdd: generate_date_delta_with_unit_sql,
             exp.DateDiff: generate_date_delta_with_unit_sql,
+            exp.JSONExtract: lambda self, e: self.binary(e, ":"),
             exp.ToChar: lambda self, e: self.function_fallback_sql(e),
         }
         TRANSFORMS.pop(exp.Select)  # Remove the ELIMINATE_QUALIFY transformation

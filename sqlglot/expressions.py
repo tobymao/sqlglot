80287dd290e9076ce5981ea440dc193df27e0d46
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2023-04-30 08:51:57 +0300

    Feat(presto): transpile explode/posexplode into (cross join) unnest (#1501)
    * Feat(presto): transpile explode/posexplode into (cross join) unnest
    
    * Use more descriptive auto-generated names
    
    * Make exp.alias_ pure, change base names to pos, col

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -4741,6 +4741,8 @@ def alias_(
 
     if table:
         table_alias = TableAlias(this=alias)
+
+        exp = exp.copy()
         exp.set("alias", table_alias)
 
         if not isinstance(table, bool):

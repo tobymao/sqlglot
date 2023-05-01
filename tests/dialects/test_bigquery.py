f137815db8264739d16339a8b8543a6fb6fc3c12
Philippe Laflamme <484152+plaflamme@users.noreply.github.com>
2023-05-01 00:38:44 -0400

    fix: BigQuery `TIMESTAMP` and `TIMESTAMPTZ` types (#1511)
    BigQuery does not have a `TIMESTAMPTZ` type. A timestamp with
    time zone maps to the [`TIMESTAMP`](https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types#timestamp_type) type and one without a
    timezone maps to [`DATETIME`](https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types#datetime_type).

--- tests/dialects/test_bigquery.py
+++ tests/dialects/test_bigquery.py
@@ -22,6 +22,8 @@ class TestBigQuery(Validator):
         self.validate_all("CAST(x AS CHAR)", write={"bigquery": "CAST(x AS STRING)"})
         self.validate_all("CAST(x AS NCHAR)", write={"bigquery": "CAST(x AS STRING)"})
         self.validate_all("CAST(x AS NVARCHAR)", write={"bigquery": "CAST(x AS STRING)"})
+        self.validate_all("CAST(x AS TIMESTAMP)", write={"bigquery": "CAST(x AS DATETIME)"})
+        self.validate_all("CAST(x AS TIMESTAMPTZ)", write={"bigquery": "CAST(x AS TIMESTAMP)"})
         self.validate_all(
             "SELECT ARRAY(SELECT AS STRUCT 1 a, 2 b)",
             write={

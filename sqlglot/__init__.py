2b3d4e93d90f8b5be0a7feeb64d62873e9960689
George Sittas <giwrgos.sittas@gmail.com>
2023-05-16 20:01:30 +0300

    v13.0.1

--- sqlglot/__init__.py
+++ sqlglot/__init__.py
@@ -50,7 +50,7 @@ if t.TYPE_CHECKING:
     T = t.TypeVar("T", bound=Expression)
 
 
-__version__ = "13.0.0"
+__version__ = "13.0.1"
 
 pretty = False
 """Whether to format generated SQL by default."""

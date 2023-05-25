4d97e353fc5bdc2d9835493b9c9fcadd3bf68875
George Sittas <giwrgos.sittas@gmail.com>
2023-05-25 22:03:47 +0300

    Fixup

--- sqlglot/dialects/mysql.py
+++ sqlglot/dialects/mysql.py
@@ -24,8 +24,8 @@ from sqlglot.helper import seq_get
 from sqlglot.tokens import TokenType
 
 
-def _show_parser(*args: t.Any, **kwargs: t.Any) -> t.Callable[[parser.Parser], exp.Show]:
-    def _parse(self) -> exp.Show:
+def _show_parser(*args: t.Any, **kwargs: t.Any) -> t.Callable[[MySQL.Parser], exp.Show]:
+    def _parse(self: MySQL.Parser) -> exp.Show:
         return self._parse_show_mysql(*args, **kwargs)
 
     return _parse

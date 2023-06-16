6c2626d4d1a635ca9d613c6c6e4e850e9fba16a1
George Sittas <giwrgos.sittas@gmail.com>
2023-06-16 19:31:29 +0300

    Cleanup

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -3221,11 +3221,11 @@ class Star(Expression):
         return self.name
 
 
-class Parameter(Expression):
+class Parameter(Condition):
     arg_types = {"this": True, "wrapped": False}
 
 
-class SessionParameter(Expression):
+class SessionParameter(Condition):
     arg_types = {"this": True, "kind": False}
 
 

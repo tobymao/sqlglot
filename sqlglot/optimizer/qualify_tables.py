808ceef9d2fe590a60a4a4500b05420f5ecc14f8
George Sittas <giwrgos.sittas@gmail.com>
2023-07-09 01:40:15 +0300

    Add docstring example for join construct expansion

--- sqlglot/optimizer/qualify_tables.py
+++ sqlglot/optimizer/qualify_tables.py
@@ -15,13 +15,18 @@ def qualify_tables(
     schema: t.Optional[Schema] = None,
 ) -> E:
     """
-    Rewrite sqlglot AST to have fully qualified tables.
+    Rewrite sqlglot AST to have fully qualified tables. Join constructs such as
+    (t1 JOIN t2) AS t will be expanded into (SELECT * FROM t1 AS t1, t2 AS t2) AS t.
 
     Examples:
         >>> import sqlglot
         >>> expression = sqlglot.parse_one("SELECT 1 FROM tbl")
         >>> qualify_tables(expression, db="db").sql()
         'SELECT 1 FROM db.tbl AS tbl'
+        >>>
+        >>> expression = sqlglot.parse_one("SELECT 1 FROM (t1 JOIN t2) AS t")
+        >>> qualify_tables(expression).sql()
+        >>> 'SELECT 1 FROM (SELECT * FROM t1 AS t1, t2 AS t2) AS t'
 
     Args:
         expression: Expression to qualify

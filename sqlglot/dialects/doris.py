ea1aba2c48dbba4fb1a597a1f724e682bd6b0d21
liujiwen-up <uptual@163.com>
2023-08-08 17:35:42 +0800

    Add test cases and api documentation for doris

--- sqlglot/dialects/doris.py
+++ sqlglot/dialects/doris.py
@@ -11,62 +11,11 @@ from sqlglot.dialects.dialect import (
 from sqlglot.dialects.mysql import MySQL
 from sqlglot.helper import seq_get
 
-# (FuncType, Multiplier)
-DATE_DELTA_INTERVAL = {
-    "YEAR": ("ADD_MONTHS", 12),
-    "MONTH": ("ADD_MONTHS", 1),
-    "QUARTER": ("ADD_MONTHS", 3),
-    "WEEK": ("DATE_ADD", 7),
-    "DAY": ("DATE_ADD", 1),
-}
-
-
-# TRANSFORMS.pop(exp.DateTrunc)
-def _add_date_sql(self: generator.Generator, expression: exp.DateAdd | exp.DateSub) -> str:
-    unit = expression.text("unit").upper()
-    func, multiplier = DATE_DELTA_INTERVAL.get(unit, ("DATE_ADD", 1))
-
-    if isinstance(expression, exp.DateSub):
-        multiplier *= -1
-
-    if expression.expression.is_number:
-        modified_increment = exp.Literal.number(int(expression.text("expression")) * multiplier)
-    else:
-        modified_increment = expression.expression
-        if multiplier != 1:
-            modified_increment = exp.Mul(  # type: ignore
-                this=modified_increment, expression=exp.Literal.number(multiplier)
-            )
-
-    return self.func(func, expression.this, modified_increment)
-
 
 def _to_date_sql(self: generator.Generator, expression: exp.TsOrDsToDate) -> str:
     this = self.sql(expression, "this")
     self.format_time(expression)
-    # if time_format and time_format not in (Doris.TIME_FORMAT, Doris.DATE_FORMAT):
-    #     return f"TO_DATE({this}, {time_format})"
     return f"TO_DATE({this})"
-
-
-def _str_to_date_sql(self: generator.Generator, expression: exp.StrToDate) -> str:
-    this = self.sql(expression, "this")
-    time_format = self.format_time(expression)
-    if time_format not in (Doris.TIME_FORMAT, Doris.DATE_FORMAT):
-        this = f"FROM_UNIXTIME(UNIX_TIMESTAMP({this}, {time_format}))"
-    return f"CAST({this} AS DATE)"
-
-
-def _str_to_time_sql(self: generator.Generator, expression: exp.StrToTime) -> str:
-    this = self.sql(expression, "this")
-    time_format = self.format_time(expression)
-    if time_format not in (Doris.TIME_FORMAT, Doris.DATE_FORMAT):
-        this = f"FROM_UNIXTIME(UNIX_TIMESTAMP({this}, {time_format}))"
-    return f"CAST({this} AS TIMESTAMP)"
-
-
-def _str_to_unix_sql(self: generator.Generator, expression: exp.StrToUnix) -> str:
-    return self.func("UNIX_TIMESTAMP", expression.this, _time_format(self, expression))
 
 
 def _time_format(
@@ -78,35 +27,17 @@ def _time_format(
     return time_format
 
 
-def var_map_sql(self, expression: exp.Map | exp.VarMap, map_func_name: str = "ARRAY_MAP") -> str:
-    keys = expression.args["keys"]
-    values = expression.args["values"]
-
-    if not isinstance(keys, exp.Array) or not isinstance(values, exp.Array):
-        self.unsupported("Cannot convert array columns into map.")
-        return self.func(map_func_name, keys, values)
-
-    args = []
-    for key, value in zip(keys.expressions, values.expressions):
-        args.append(self.sql(key))
-        args.append(self.sql(value))
-
-    return self.func(*args, map_func_name)
-
-
 class Doris(MySQL):
     DATE_FORMAT = "'yyyy-MM-dd'"
     DATEINT_FORMAT = "'yyyyMMdd'"
     TIME_FORMAT = "'yyyy-MM-dd HH:mm:ss'"
 
-    # https://prestodb.io/docs/current/functions/datetime.html#mysql-date-functions
     TIME_MAPPING = {
         "%M": "%B",
         "%m": "%%-M",
         "%c": "%-m",
         "%e": "%-d",
         "%h": "%I",
-        "%i": "%M",
         "%S": "%S",
         "%u": "%W",
         "%k": "%-H",
@@ -116,6 +47,9 @@ class Doris(MySQL):
         "%d": "%%-d",
         "%H": "%%-H",
         "%s": "%%-S",
+        "%D": "%%-j",
+        "%a": "%%p",
+        "%y": "%%Y",
         "%": "%%",
     }
 
@@ -146,15 +80,13 @@ class Doris(MySQL):
             exp.RegexpLike: rename_func("REGEXP"),
             exp.Coalesce: rename_func("NVL"),
             exp.CurrentTimestamp: lambda self, e: "NOW()",
-            # exp.CurrentTime: lambda self, e: "NOW()",
             exp.TimeToStr: lambda self, e: f"DATE_FORMAT({self.sql(e, 'this')}, {self.format_time(e)})",
-            # exp.StrToUnix: rename_func("UNIX_TIMESTAMP"),
+            exp.ToChar: lambda self, e: f"DATE_FORMAT({self.sql(e, 'this')}, {self.format_time(e)})",
             exp.StrToUnix: lambda self, e: f"UNIX_TIMESTAMP({self.sql(e, 'this')}, {self.format_time(e)})",
             exp.TimestampTrunc: lambda self, e: self.func(
                 "DATE_TRUNC", exp.Literal.string(e.text("unit")), e.this
             ),
-            exp.TimeStrToDate: rename_func("STR_TO_DATE"),
-            # exp.UnixToStr: lambda self, e: f"FROM_UNIXTIME({self.sql(e, 'this')}, {self.format_time(e)})",
+            exp.TimeStrToDate: rename_func("TO_DATE"),
             exp.UnixToStr: lambda self, e: self.func(
                 "FROM_UNIXTIME", e.this, _time_format(self, e)
             ),
@@ -163,14 +95,17 @@ class Doris(MySQL):
             exp.SetAgg: rename_func("COLLECT_SET"),
             exp.TsOrDsAdd: lambda self, e: f"DATE_ADD({self.sql(e, 'this')}, {self.sql(e, 'expression')})",  # Only for day level
             exp.TsOrDsToDate: _to_date_sql,
-            exp.StrToDate: _str_to_date_sql,
-            exp.StrToTime: _str_to_time_sql,
             exp.Map: rename_func("ARRAY_MAP"),
-            exp.VarMap: var_map_sql,
             exp.RegexpSplit: rename_func("SPLIT_BY_STRING"),
             exp.Split: rename_func("SPLIT_BY_STRING"),
             exp.Quantile: rename_func("PERCENTILE"),
             exp.ApproxQuantile: rename_func("PERCENTILE_APPROX"),
-            # exp.StrToUnix: _str_to_unix_sql,
+            exp.TimeStrToUnix: rename_func("UNIX_TIMESTAMP"),
+            exp.TimeToUnix: rename_func("UNIX_TIMESTAMP"),
+            exp.DateTrunc: lambda self, e: self.func(
+                "DATE_TRUNC", e.this, "'" + e.text("unit") + "'"
+            ),
+            exp.TimestampTrunc: lambda self, e: self.func(
+                "DATE_TRUNC", e.this, "'" + e.text("unit") + "'"
+            ),
         }
-        # TRANSFORMS.pop(exp.Map)

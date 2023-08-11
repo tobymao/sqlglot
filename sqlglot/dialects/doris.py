8d90a605c2ffa0416cbc6d4e3e57f05e8898e693
liujiwen-up <uptual@163.com>
2023-08-11 17:49:51 +0800

    fix to_date/to_char/nvl/nvl2/concat/date_format expression

--- sqlglot/dialects/doris.py
+++ sqlglot/dialects/doris.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from sqlglot import exp
+from typing import Union
+
+from sqlglot import exp, generator
 from sqlglot.dialects.dialect import (
     approx_count_distinct_sql,
     arrow_json_extract_sql,
@@ -9,6 +11,54 @@ from sqlglot.dialects.dialect import (
     time_format,
 )
 from sqlglot.dialects.mysql import MySQL
+
+
+def _to_date_sql(
+    self: generator.Generator, expression: Union[exp.Anonymous, exp.TsOrDsToDate]
+) -> str:
+    if isinstance(expression, exp.Anonymous):
+        expressions = expression.args.get("expressions")
+        if expressions is None or len(expressions) == 0:
+            raise ValueError("Missing expressions for TO_DATE")
+        expr = expressions[0]
+        time_format = expressions[1] if len(expressions) > 1 else None
+        expr_TIMESTAMP = self.sql(expression, "this")
+        if expr_TIMESTAMP == "TIMESTAMP":
+            return f"TIMESTAMP({expr})"
+    elif isinstance(expression, exp.TsOrDsToDate):
+        expr = expression.args.get("this")
+        time_format = expression.args.get("format")
+    else:
+        raise ValueError("Invalid expression type")
+
+    if time_format is not None:
+        return f"STR_TO_DATE({expr}, {time_format})"
+    else:
+        return f"TO_DATE({expr})"
+
+
+def handle_nvl2(self, expression: exp.Nvl2) -> str:
+    this = self.sql(expression, "this")
+    expr1 = self.sql(expression, "true")
+    expr2 = self.sql(expression, "false")
+    return f"CASE WHEN {this} IS NOT NULL THEN {expr2} ELSE {expr1} END"
+
+
+def handle_to_char(self, expression: exp.ToChar) -> str:
+    self.sql(expression, "this")
+    decimal_places, has_decimal = parse_format_string(self.sql(expression, "format"))
+    if has_decimal:
+        return f"Round({self.sql(expression, 'this')},{decimal_places})"
+    else:
+        return f"DATE_FORMAT({self.sql(expression, 'this')}, {self.format_time(expression)})"
+
+
+def parse_format_string(format_string):
+    decimal_places = None
+    if "." in format_string:
+        decimal_places = len(format_string) - format_string.index(".") - 2
+    has_decimal = decimal_places is not None
+    return decimal_places, has_decimal
 
 
 class Doris(MySQL):
@@ -35,9 +85,9 @@ class Doris(MySQL):
 
         TRANSFORMS = {
             **MySQL.Generator.TRANSFORMS,
+            exp.Anonymous: _to_date_sql,
             exp.ApproxDistinct: approx_count_distinct_sql,
             exp.ArrayAgg: rename_func("COLLECT_LIST"),
-            exp.Coalesce: rename_func("NVL"),
             exp.CurrentTimestamp: lambda *_: "NOW()",
             exp.DateTrunc: lambda self, e: self.func(
                 "DATE_TRUNC", e.this, "'" + e.text("unit") + "'"
@@ -49,8 +99,9 @@ class Doris(MySQL):
             exp.SetAgg: rename_func("COLLECT_SET"),
             exp.StrToUnix: lambda self, e: f"UNIX_TIMESTAMP({self.sql(e, 'this')}, {self.format_time(e)})",
             exp.Split: rename_func("SPLIT_BY_STRING"),
+            exp.SafeDPipe: rename_func("CONCAT"),
             exp.TimeStrToDate: rename_func("TO_DATE"),
-            exp.ToChar: lambda self, e: f"DATE_FORMAT({self.sql(e, 'this')}, {self.format_time(e)})",
+            exp.ToChar: handle_to_char,
             exp.TsOrDsAdd: lambda self, e: f"DATE_ADD({self.sql(e, 'this')}, {self.sql(e, 'expression')})",  # Only for day level
             exp.TsOrDsToDate: lambda self, e: self.func("TO_DATE", e.this),
             exp.TimeToUnix: rename_func("UNIX_TIMESTAMP"),
@@ -62,4 +113,5 @@ class Doris(MySQL):
             ),
             exp.UnixToTime: rename_func("FROM_UNIXTIME"),
             exp.Map: rename_func("ARRAY_MAP"),
+            exp.Nvl2: handle_nvl2,
         }
--- tests/dialects/test_doris.py
+++ tests/dialects/test_doris.py
@@ -18,3 +18,56 @@ class TestDoris(Validator):
                 "doris": "SELECT REGEXP(abc, '%foo%')",
             },
         )
+
+    def test_concat(self):
+        self.validate_all(
+            "SELECT a || b",
+            write={
+                "doris": "SELECT CONCAT(a, b)",
+            },
+        )
+
+    def test_to_date(self):
+        self.validate_all(
+            "SELECT TO_DATE('2019-12-01')",
+            read={
+                "oracle": "SELECT TO_DATE('2019-12-01')",
+                "hive": "SELECT TO_DATE('2019-12-01')",
+            },
+            write={
+                "doris": "SELECT TO_DATE('2019-12-01')",
+            },
+        ),
+        self.validate_all(
+            "SELECT STR_TO_DATE('2019-12-01', 'yyyy-MM-dd')",
+            read={
+                "oracle": "SELECT TO_DATE('2019-12-01', 'yyyy-MM-dd')",
+            },
+            write={
+                "doris": "SELECT STR_TO_DATE('2019-12-01', 'yyyy-MM-dd')",
+            },
+        ),
+        self.validate_all(
+            "SELECT STR_TO_DATE('2019-12-01', '%Y-%m-%d')",
+            write={
+                "doris": "SELECT STR_TO_DATE('2019-12-01', '%Y-%m-%d')",
+            },
+        ),
+        self.validate_all(
+            "NVL2(x, y, z)",
+            write={
+                "doris": "CASE WHEN x IS NOT NULL THEN z ELSE y END",
+            },
+        ),
+        self.validate_all(
+            "to_char('2009-10-04 22:23:00', 'yyyymm')",
+            write={
+                "doris": "DATE_FORMAT('2009-10-04 22:23:00', 'yyyymm')",
+            },
+        ),
+        self.validate_all(
+            "to_char(t, 'FM99999999990.00')",
+            write={
+                "doris": "Round(t,2)",
+            },
+        ),

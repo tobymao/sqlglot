acfa0ef5d6a3338a72362f0be8435f978b2c648b
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2023-08-14 19:39:34 +0300

    Fix(tsql): improve handling of table hints in MERGE statement (#2049)

--- tests/dialects/test_tsql.py
+++ tests/dialects/test_tsql.py
@@ -17,6 +17,11 @@ class TestTSQL(Validator):
                 "spark": "DROP TABLE IF EXISTS TempTableName",
             },
         )
+
+        self.validate_identity(
+            "MERGE INTO mytable WITH (HOLDLOCK) AS T USING mytable_merge AS S "
+            "ON (T.user_id = S.user_id) WHEN NOT MATCHED THEN INSERT (c1, c2) VALUES (S.c1, S.c2)"
+        )
         self.validate_identity("UPDATE x SET y = 1 OUTPUT x.a, x.b INTO @y FROM y")
         self.validate_identity("UPDATE x SET y = 1 OUTPUT x.a, x.b FROM y")
         self.validate_identity("INSERT INTO x (y) OUTPUT x.a, x.b INTO l SELECT * FROM z")

33220b9cca8bb9f77aab2ff1fb47329cb8c9318f
tobymao <toby.mao@gmail.com>
2023-08-15 15:53:17 -0700

    Feat: add more funcs to scope module

--- sqlglot/optimizer/__init__.py
+++ sqlglot/optimizer/__init__.py
@@ -1,2 +1,9 @@
 from sqlglot.optimizer.optimizer import RULES, optimize
-from sqlglot.optimizer.scope import Scope, build_scope, traverse_scope
+from sqlglot.optimizer.scope import (
+    Scope,
+    build_scope,
+    find_all_in_scope,
+    find_in_scope,
+    traverse_scope,
+    walk_in_scope,
+)

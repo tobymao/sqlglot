bd96d0c1a1928f506516caf11a093c0dd2180ce2
Trey Spiller <treyspiller@gmail.com>
2023-08-31 15:38:09 -0500

    Fix(tsql): support adding multiple columns with ALTER TABLE (#2140)
    * Support adding multiple columns with ALTER TABLE in tsql
    
    * Correctly parse tsql ALTER TABLE with multiple added columns
    
    * Fix styling

--- tests/dialects/test_tsql.py
+++ tests/dialects/test_tsql.py
@@ -488,9 +488,14 @@ class TestTSQL(Validator):
         )
 
         self.validate_all(
-            "ALTER TABLE a ADD b INTEGER",
-            read={"": "ALTER TABLE a ADD COLUMN b INT"},
-            write={"tsql": "ALTER TABLE a ADD b INTEGER"},
+            "ALTER TABLE a ADD b INTEGER, c INTEGER",
+            read={
+                "": "ALTER TABLE a ADD COLUMN b INT, ADD COLUMN c INT",
+            },
+            write={
+                "": "ALTER TABLE a ADD COLUMN b INT, ADD COLUMN c INT",
+                "tsql": "ALTER TABLE a ADD b INTEGER, c INTEGER",
+            },
         )
 
         self.validate_all(

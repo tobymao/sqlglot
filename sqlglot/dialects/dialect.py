7a424a564f86bc096b113a2e9a0841a4b7fc47db
Trey Spiller <treyspiller@gmail.com>
2023-09-20 11:24:50 -0500

    Move cte before insert (#2270)

--- sqlglot/dialects/dialect.py
+++ sqlglot/dialects/dialect.py
@@ -787,3 +787,10 @@ def is_parse_json(expression: exp.Expression) -> bool:
 
 def isnull_to_is_null(args: t.List) -> exp.Expression:
     return exp.Paren(this=exp.Is(this=seq_get(args, 0), expression=exp.null()))
+
+
+def move_insert_cte_sql(self: Generator, expression: exp.Insert) -> str:
+    if expression.expression.args.get("with"):
+        expression = expression.copy()
+        expression.set("with", expression.expression.args.pop("with"))
+    return self.insert_sql(expression)

e90312a81f929dfd6af1fb6799cd77e0bf58ea47
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2023-09-21 18:48:42 +0300

    Feat!: improve transpilation of T-SQL's SET assignment command (#2275)
    * Feat: improve transpilation of T-SQL's SET assignment command
    
    * Always output ${..} for params in Spark, Databricks
    
    * Bring back parameters in Hive

--- sqlglot/dialects/hive.py
+++ sqlglot/dialects/hive.py
@@ -480,6 +480,16 @@ class Hive(Dialect):
             exp.VolatileProperty: exp.Properties.Location.UNSUPPORTED,
         }
 
+        def parameter_sql(self, expression: exp.Parameter) -> str:
+            this = self.sql(expression, "this")
+            parent = expression.parent
+
+            if isinstance(parent, exp.EQ) and isinstance(parent.parent, exp.SetItem):
+                # We need to produce SET key = value instead of SET ${key} = value
+                return this
+
+            return f"${{{this}}}"
+
         def schema_sql(self, expression: exp.Schema) -> str:
             expression = expression.copy()
 

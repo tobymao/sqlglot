9219926cf92708b6627e3763c7015de841e7e80b
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2023-09-28 02:01:05 +0300

    Fix!: revert escape sequence changes introduced in #2230 (#2336)

--- sqlglot/generator.py
+++ sqlglot/generator.py
@@ -333,8 +333,6 @@ class Generator:
         exp.Paren,
     )
 
-    UNESCAPED_SEQUENCE_TABLE = None  # type: ignore
-
     SENTINEL_LINE_BREAK = "__SQLGLOT__LB__"
 
     # Autofilled
@@ -347,6 +345,7 @@ class Generator:
     STRICT_STRING_CONCAT = False
     NORMALIZE_FUNCTIONS: bool | str = "upper"
     NULL_ORDERING = "nulls_are_small"
+    ESCAPE_LINE_BREAK = False
 
     can_identify: t.Callable[[str, str | bool], bool]
 
@@ -1659,8 +1658,8 @@ class Generator:
 
     def escape_str(self, text: str) -> str:
         text = text.replace(self.QUOTE_END, self._escaped_quote_end)
-        if self.UNESCAPED_SEQUENCE_TABLE:
-            text = text.translate(self.UNESCAPED_SEQUENCE_TABLE)
+        if self.ESCAPE_LINE_BREAK:
+            text = text.replace("\n", "\\n")
         elif self.pretty:
             text = text.replace("\n", self.SENTINEL_LINE_BREAK)
         return text

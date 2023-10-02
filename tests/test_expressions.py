51779b1078e14a97a9054e5ad763570d75af3623
George Sittas <giwrgos.sittas@gmail.com>
2023-10-02 12:54:16 +0300

    Fix: take Literals into account in Table.parts, improve table_name

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -192,6 +192,8 @@ class TestExpressions(unittest.TestCase):
         )
         self.assertEqual(exp.table_name(exp.to_table("a-1.b.c", dialect="bigquery")), '"a-1".b.c')
         self.assertEqual(exp.table_name(exp.to_table("a.b.c.d.e", dialect="bigquery")), "a.b.c.d.e")
+        self.assertEqual(exp.table_name(exp.to_table("'@foo'", dialect="snowflake")), "'@foo'")
+        self.assertEqual(exp.table_name(exp.to_table("@foo", dialect="snowflake")), "@foo")
 
     def test_table(self):
         self.assertEqual(exp.table_("a", alias="b"), parse_one("select * from a b").find(exp.Table))

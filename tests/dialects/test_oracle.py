958382588f2c0d8246637cfef5fabd2c8c78840f
George Sittas <giwrgos.sittas@gmail.com>
2023-11-07 17:56:46 +0000

    Fix(oracle): parse TO_CHAR using format_time_lambda

--- tests/dialects/test_oracle.py
+++ tests/dialects/test_oracle.py
@@ -63,6 +63,13 @@ class TestOracle(Validator):
         )
 
         self.validate_all(
+            "SELECT TO_CHAR(TIMESTAMP '1999-12-01 10:00:00')",
+            write={
+                "oracle": "SELECT TO_CHAR(CAST('1999-12-01 10:00:00' AS TIMESTAMP), 'YYYY-MM-DD HH24:MI:SS')",
+                "postgres": "SELECT TO_CHAR(CAST('1999-12-01 10:00:00' AS TIMESTAMP), 'YYYY-MM-DD HH24:MI:SS')",
+            },
+        )
+        self.validate_all(
             "SELECT CAST(NULL AS VARCHAR2(2328 CHAR)) AS COL1",
             write={
                 "oracle": "SELECT CAST(NULL AS VARCHAR2(2328 CHAR)) AS COL1",

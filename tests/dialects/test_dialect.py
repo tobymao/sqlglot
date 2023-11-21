ef3cb46c9d44ab855a8fe139a5b92c9b18130a40
tobymao <toby.mao@gmail.com>
2023-11-20 16:09:31 -0800

    fix!: use more canonical cast instead of to_date
    prior, presto and hive had custom datestrtodate conversions. this is
    problematic for select * from values (... ) as to_date is not allowed in
    literal sql expressions. after testing, i decided to use the more
    canonical form of cast('2022-01-01' as date).

--- tests/dialects/test_dialect.py
+++ tests/dialects/test_dialect.py
@@ -867,9 +867,9 @@ class TestDialect(Validator):
             write={
                 "drill": "CAST(x AS DATE)",
                 "duckdb": "CAST(x AS DATE)",
-                "hive": "TO_DATE(x)",
-                "presto": "CAST(DATE_PARSE(x, '%Y-%m-%d') AS DATE)",
-                "spark": "TO_DATE(x)",
+                "hive": "CAST(x AS DATE)",
+                "presto": "CAST(x AS DATE)",
+                "spark": "CAST(x AS DATE)",
                 "sqlite": "x",
             },
         )

4d68e39a3aadc9b07d3f96c51fc46fd407486d86
tobymao <toby.mao@gmail.com>
2023-12-06 14:40:23 -0800

    fix!: remove redundant todate closes #2636

--- sqlglot/dialects/hive.py
+++ sqlglot/dialects/hive.py
@@ -187,6 +187,8 @@ def _to_date_sql(self: Hive.Generator, expression: exp.TsOrDsToDate) -> str:
     time_format = self.format_time(expression)
     if time_format and time_format not in (Hive.TIME_FORMAT, Hive.DATE_FORMAT):
         return f"TO_DATE({this}, {time_format})"
+    if isinstance(expression.this, exp.TsOrDsToDate):
+        return this
     return f"TO_DATE({this})"
 
 

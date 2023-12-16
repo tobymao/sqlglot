77766eff20f3b9ae04924c4a6fba1b64208e97ef
barak <barak.alon@airbnb.com>
2023-12-15 20:03:10 -0800

    fix(optimizer): CTE aliases overridden in FROM (#2692)
    * fix(optimizer): CTE aliases overridden in FROM
    
    * fixup

--- tests/fixtures/optimizer/eliminate_ctes.sql
+++ tests/fixtures/optimizer/eliminate_ctes.sql
@@ -46,3 +46,71 @@ FROM x;
 SELECT
   a
 FROM x;
+
+# title: CTE reference in subquery where alias matches outer table name
+WITH q AS (
+  SELECT
+    a
+  FROM y
+)
+SELECT
+  a
+FROM x AS q
+WHERE
+  a IN (
+    SELECT
+      a
+    FROM q
+  );
+WITH q AS (
+  SELECT
+    a
+  FROM y
+)
+SELECT
+  a
+FROM x AS q
+WHERE
+  a IN (
+    SELECT
+      a
+    FROM q
+  );
+
+# title: CTE reference in subquery where alias matches outer table name and outer alias is also CTE
+WITH q AS (
+  SELECT
+    a
+  FROM y
+), q2 AS (
+  SELECT
+    a
+  FROM y
+)
+SELECT
+  a
+FROM q2 AS q
+WHERE
+  a IN (
+    SELECT
+      a
+    FROM q
+  );
+WITH q AS (
+  SELECT
+    a
+  FROM y
+), q2 AS (
+  SELECT
+    a
+  FROM y
+)
+SELECT
+  a
+FROM q2 AS q
+WHERE
+  a IN (
+    SELECT
+      a
+    FROM q
+  );

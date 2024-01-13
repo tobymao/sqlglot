3dba6c194e2fff90f93f0370255a94a1b2a2365a
Jiahong Long <13944989+j1ah0ng@users.noreply.github.com>
2024-01-12 21:44:58 -0800

    fix: add parentheses to no_safe_divide args (#2826)
    * add parentheses to no_safe_divide args
    
    * fix test

--- sqlglot/dialects/dialect.py
+++ sqlglot/dialects/dialect.py
@@ -533,7 +533,7 @@ def no_recursive_cte_sql(self: Generator, expression: exp.With) -> str:
 def no_safe_divide_sql(self: Generator, expression: exp.SafeDivide) -> str:
     n = self.sql(expression, "this")
     d = self.sql(expression, "expression")
-    return f"IF({d} <> 0, {n} / {d}, NULL)"
+    return f"IF(({d}) <> 0, ({n}) / ({d}), NULL)"
 
 
 def no_tablesample_sql(self: Generator, expression: exp.TableSample) -> str:

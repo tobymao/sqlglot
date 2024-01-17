f9d1732a469e1b05353b5f8584f23592eb10fee0
liugddx <liugddx@gmail.com>
2024-01-17 16:35:15 +0800

    1

--- sqlglot/dialects/dameng.py
+++ sqlglot/dialects/dameng.py
@@ -4,12 +4,4 @@ from sqlglot.dialects.oracle import Oracle
 
 
 class Dameng(Oracle):
-    class Parser(Oracle.Parser):
-        FUNCTIONS = {**Oracle.Parser.FUNCTIONS}
-
-    class Generator(Oracle.Generator):
-        TYPE_MAPPING = {**Oracle.Generator.TYPE_MAPPING}
-
-        LAST_DAY_SUPPORTS_DATE_PART = False
-
-        TRANSFORMS = {**Oracle.Generator.TRANSFORMS}
+    pass

66edd09e91aaa92cc055c26ad0224efc816e457a
Jo <46752250+georgesittas@users.noreply.github.com>
2024-01-18 16:52:39 -0500

    Refactor: simplify merge without target transformation (#2854)

--- sqlglot/dialects/trino.py
+++ sqlglot/dialects/trino.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from sqlglot import exp
-from sqlglot.dialects.dialect import remove_target_from_merge
+from sqlglot.dialects.dialect import merge_without_target_sql
 from sqlglot.dialects.presto import Presto
 
 
@@ -12,7 +12,7 @@ class Trino(Presto):
         TRANSFORMS = {
             **Presto.Generator.TRANSFORMS,
             exp.ArraySum: lambda self, e: f"REDUCE({self.sql(e, 'this')}, 0, (acc, x) -> acc + x, acc -> acc)",
-            exp.Merge: remove_target_from_merge,
+            exp.Merge: merge_without_target_sql,
         }
 
     class Tokenizer(Presto.Tokenizer):

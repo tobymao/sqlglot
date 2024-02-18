22ed4d0a976dbba15962670873422e86874680b0
Toby Mao <toby.mao@gmail.com>
2024-02-18 08:06:31 -0800

    chore: cleanup kv defs from brackets (#2987)

--- sqlglot/generator.py
+++ sqlglot/generator.py
@@ -462,7 +462,7 @@ class Generator(metaclass=_Generator):
     # Expressions that need to have all CTEs under them bubbled up to them
     EXPRESSIONS_WITHOUT_NESTED_CTES: t.Set[t.Type[exp.Expression]] = set()
 
-    KEY_VALUE_DEFINITIONS = (exp.Bracket, exp.EQ, exp.PropertyEQ, exp.Slice)
+    KEY_VALUE_DEFINITIONS = (exp.EQ, exp.PropertyEQ, exp.Slice)
 
     SENTINEL_LINE_BREAK = "__SQLGLOT__LB__"
 

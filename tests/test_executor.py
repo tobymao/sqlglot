05998e41b037d55186f94a9dffa8e782f60466fb
Jo <46752250+georgesittas@users.noreply.github.com>
2024-03-08 22:00:32 +0200

    Refactor(executor): simplify column type inference (#3104)
    * Refactor(executor): simplify column type inference
    
    * Add test for list value

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -811,7 +811,7 @@ class TestExecutor(unittest.TestCase):
                 self.assertEqual(result.columns, columns)
                 self.assertEqual(result.rows, expected)
 
-    def test_dict_values(self):
+    def test_nested_values(self):
         tables = {"foo": [{"raw": {"name": "Hello, World", "a": [{"b": 1}]}}]}
 
         result = execute("SELECT raw:name AS name FROM foo", read="snowflake", tables=tables)
@@ -841,3 +841,9 @@ class TestExecutor(unittest.TestCase):
 
         self.assertEqual(result.columns, ("flavor",))
         self.assertEqual(result.rows, [("cherry",), ("lime",), ("apple",)])
+
+        tables = {"t": [{"x": [1, 2, 3]}]}
+
+        result = execute("SELECT x FROM t", dialect="duckdb", tables=tables)
+        self.assertEqual(result.columns, ("x",))
+        self.assertEqual(result.rows, [([1, 2, 3],)])

1bdba5492de8775ab3fad243c1396582a5d59bb2
vaggelisd <vagosgan13@gmail.com>
2024-03-18 16:47:37 +0200

    Removing whitespace

--- tests/dialects/test_clickhouse.py
+++ tests/dialects/test_clickhouse.py
@@ -396,7 +396,6 @@ class TestClickhouse(Validator):
         self.validate_identity(
             "INSERT INTO FUNCTION remote('localhost', default.simple_table) VALUES (100, 'inserted via remote()')"
         )
-
         self.validate_identity(
             """INSERT INTO TABLE FUNCTION hdfs('hdfs://hdfs1:9000/test', 'TSV', 'name String, column2 UInt32, column3 UInt32') VALUES ('test', 1, 2)""",
             """INSERT INTO FUNCTION hdfs('hdfs://hdfs1:9000/test', 'TSV', 'name String, column2 UInt32, column3 UInt32') VALUES ('test', 1, 2)""",

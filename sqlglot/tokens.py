48865a72942c7d2a6de40bf7587e0ca57cb543b8
Jake Lucas <jakelucasnyc@gmail.com>
2024-03-19 10:40:21 -0400

    adding DECLARE as a tokentype

--- sqlglot/tokens.py
+++ sqlglot/tokens.py
@@ -231,6 +231,7 @@ class TokenType(AutoName):
     CURRENT_TIME = auto()
     CURRENT_TIMESTAMP = auto()
     CURRENT_USER = auto()
+    DECLARE = auto()
     DEFAULT = auto()
     DELETE = auto()
     DESC = auto()

6bba0308b590aed73e454c2c40d600c670e0ad7f
Jo <46752250+georgesittas@users.noreply.github.com>
2024-04-05 14:37:09 +0300

    Feat!: transpile map retrieval to duckdb, transpile TRY_ELEMENT_AT (#3277)

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -4399,7 +4399,13 @@ class Between(Predicate):
 
 class Bracket(Condition):
     # https://cloud.google.com/bigquery/docs/reference/standard-sql/operators#array_subscript_operator
-    arg_types = {"this": True, "expressions": True, "offset": False, "safe": False}
+    arg_types = {
+        "this": True,
+        "expressions": True,
+        "offset": False,
+        "safe": False,
+        "returns_list_for_maps": False,
+    }
 
     @property
     def output_name(self) -> str:

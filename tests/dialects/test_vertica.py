fcbb620c447a5e67a01f14192634ddf4cfc62197
Rajasekhar-Vuppala <vuppala.venkata@microfocus.com>
2024-04-17 07:11:28 -0400

    fixed lint issues

--- tests/dialects/test_vertica.py
+++ tests/dialects/test_vertica.py
@@ -2,8 +2,8 @@ from tests.dialects.test_dialect import Validator
 
 
 class TestVertica(Validator):
-    dialect = "vertica"
-    maxDiff = None
+    dialect = "vertica" # type:ignore
+    maxDiff = None # type:ignore
 
     def test_vertica(self):
         self.validate_all(

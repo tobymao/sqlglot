8f87a8d82bee197a1dcffbf038b72e17a096f302
Rajasekhar-Vuppala <vuppala.venkata@microfocus.com>
2024-04-25 06:20:04 -0400

    fixed issue

--- tests/dialects/test_vertica.py
+++ tests/dialects/test_vertica.py
@@ -6,6 +6,6 @@ class TestVertica(Validator):
     maxDiff = None
 
     def test_vertica(self):
-        self.validate_identity("CAST(x AS MONEY)")
-        self.validate_identity("CAST(x AS LONGVARBINARY)")
-        self.validate_identity("CAST(x AS INTERVAL DAY TO SECOND)")
+        self.validate_identity("CAST(public.x AS MONEY)")
+        self.validate_identity("CAST(public.x AS LONGVARBINARY)")
+        self.validate_identity("CAST(public.x AS INTERVAL DAY TO SECOND)")

3c8bb5239359df425bb3afd56c073d172a13d4d0
George Sittas <giwrgos.sittas@gmail.com>
2024-05-01 00:56:15 +0300

    Fix(trino): bring back TRIM parsing

--- sqlglot/dialects/trino.py
+++ sqlglot/dialects/trino.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from sqlglot import exp
-from sqlglot.dialects.dialect import merge_without_target_sql
+from sqlglot.dialects.dialect import merge_without_target_sql, trim_sql
 from sqlglot.dialects.presto import Presto
 
 
@@ -9,12 +9,19 @@ class Trino(Presto):
     SUPPORTS_USER_DEFINED_TYPES = False
     LOG_BASE_FIRST = True
 
+    class Parser(Presto.Parser):
+        FUNCTION_PARSERS = {
+            **Presto.Parser.FUNCTION_PARSERS,
+            "TRIM": lambda self: self._parse_trim(),
+        }
+
     class Generator(Presto.Generator):
         TRANSFORMS = {
             **Presto.Generator.TRANSFORMS,
             exp.ArraySum: lambda self,
             e: f"REDUCE({self.sql(e, 'this')}, 0, (acc, x) -> acc + x, acc -> acc)",
             exp.Merge: merge_without_target_sql,
+            exp.Trim: trim_sql,
         }
 
         SUPPORTED_JSON_PATH_PARTS = {

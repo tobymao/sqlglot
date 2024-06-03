06ecba9edfe98187b0c56445bc9ce779902e75aa
Toby Mao <toby.mao@gmail.com>
2024-06-03 16:23:19 -0700

    fix: div aliases closes #3583 (#3586)

--- tests/dialects/test_redshift.py
+++ tests/dialects/test_redshift.py
@@ -6,6 +6,7 @@ class TestRedshift(Validator):
     dialect = "redshift"
 
     def test_redshift(self):
+        self.validate_identity("1 div", "1 AS div")
         self.validate_all(
             "SELECT SPLIT_TO_ARRAY('12,345,6789')",
             write={

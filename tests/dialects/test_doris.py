75ed5a695524e3a29e10fe3ecae91fd859126e6d
George Sittas <giwrgos.sittas@gmail.com>
2024-06-18 13:37:10 +0300

    Fix(doris): ensure LAG/LEAD are generated with three arguments

--- tests/dialects/test_doris.py
+++ tests/dialects/test_doris.py
@@ -56,6 +56,34 @@ class TestDoris(Validator):
                 "postgres": "SELECT STRING_AGG('aa', ',')",
             },
         )
+        self.validate_all(
+            "SELECT LAG(1, 1, NULL) OVER (ORDER BY 1)",
+            read={
+                "doris": "SELECT LAG(1, 1, NULL) OVER (ORDER BY 1)",
+                "postgres": "SELECT LAG(1) OVER (ORDER BY 1)",
+            },
+        )
+        self.validate_all(
+            "SELECT LAG(1, 2, NULL) OVER (ORDER BY 1)",
+            read={
+                "doris": "SELECT LAG(1, 2, NULL) OVER (ORDER BY 1)",
+                "postgres": "SELECT LAG(1, 2) OVER (ORDER BY 1)",
+            },
+        )
+        self.validate_all(
+            "SELECT LEAD(1, 1, NULL) OVER (ORDER BY 1)",
+            read={
+                "doris": "SELECT LEAD(1, 1, NULL) OVER (ORDER BY 1)",
+                "postgres": "SELECT LEAD(1) OVER (ORDER BY 1)",
+            },
+        )
+        self.validate_all(
+            "SELECT LEAD(1, 2, NULL) OVER (ORDER BY 1)",
+            read={
+                "doris": "SELECT LEAD(1, 2, NULL) OVER (ORDER BY 1)",
+                "postgres": "SELECT LEAD(1, 2) OVER (ORDER BY 1)",
+            },
+        )
 
     def test_identity(self):
         self.validate_identity("COALECSE(a, b, c, d)")

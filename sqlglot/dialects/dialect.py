e9236e36c94464af21c7e2f35a083eef316feab1
Jo <46752250+georgesittas@users.noreply.github.com>
2024-06-26 11:41:25 +0300

    Refactor: add EXPAND_ALIAS_REFS_ONLY_IN_GROUP_BY dialect constant (#3702)
    * Refactor: add EXPAND_ALIAS_REFS_ONLY_IN_GROUP_BY dialect constant
    
    * Rename

--- sqlglot/dialects/dialect.py
+++ sqlglot/dialects/dialect.py
@@ -351,6 +351,9 @@ class Dialect(metaclass=_Dialect):
         - Clickhouse, which will forward the alias across the query i.e it resolves to "WHERE id = 1 GROUP BY id HAVING id = 1"
     """
 
+    EXPAND_ALIAS_REFS_EARLY_ONLY_IN_GROUP_BY = False
+    """Whether alias reference expansion before qualification should only happen for the GROUP BY clause."""
+
     # --- Autofilled ---
 
     tokenizer_class = Tokenizer

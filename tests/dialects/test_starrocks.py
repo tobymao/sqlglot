8c623c96b467eb1c1cd975b0335693a39366e765
vaggelisd <daniasevangelos@gmail.com>
2024-08-30 20:13:20 +0300

    feat: Add UnsupportedError to transforms

--- tests/dialects/test_starrocks.py
+++ tests/dialects/test_starrocks.py
@@ -1,3 +1,4 @@
+from sqlglot.errors import UnsupportedError
 from tests.dialects.test_dialect import Validator
 
 
@@ -60,6 +61,7 @@ class TestStarrocks(Validator):
                 "spark": r"""SELECT id, t.type, t.scores FROM example_table LATERAL VIEW INLINE(ARRAYS_ZIP(SPLIT(type, CONCAT('\\Q', ';')), scores)) t AS type, scores""",
                 "databricks": r"""SELECT id, t.type, t.scores FROM example_table LATERAL VIEW INLINE(ARRAYS_ZIP(SPLIT(type, CONCAT('\\Q', ';')), scores)) t AS type, scores""",
                 "starrocks": r"""SELECT id, t.type, t.scores FROM example_table, UNNEST(SPLIT(type, ';'), scores) AS t(type, scores)""",
+                "hive": UnsupportedError,
             },
         )
 
@@ -68,6 +70,7 @@ class TestStarrocks(Validator):
             write={
                 "spark": r"""SELECT id, t.type, t.scores FROM example_table_2 LATERAL VIEW INLINE(ARRAYS_ZIP(SPLIT(type, CONCAT('\\Q', ';')), scores)) t AS type, scores""",
                 "starrocks": r"""SELECT id, t.type, t.scores FROM example_table_2 CROSS JOIN LATERAL UNNEST(SPLIT(type, ';'), scores) AS t(type, scores)""",
+                "hive": UnsupportedError,
             },
         )
 

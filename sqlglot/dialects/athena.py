f27e24ccf77bb447b9c38f5005ce3c5ed53fb4b1
Erin Drummond <erin.dru@gmail.com>
2024-09-12 15:43:28 +1200

    Feat(athena): Improve DDL query support (#4099)
    * Feat(athena): Improve DDL query support

--- sqlglot/dialects/athena.py
+++ sqlglot/dialects/athena.py
@@ -1,31 +1,88 @@
 from __future__ import annotations
 
+import typing as t
+
 from sqlglot import exp
 from sqlglot.dialects.trino import Trino
+from sqlglot.dialects.hive import Hive
 from sqlglot.tokens import TokenType
 
 
+def _generate_as_hive(expression: exp.Expression) -> bool:
+    if isinstance(expression, exp.Create):
+        if expression.kind == "TABLE":
+            properties: t.Optional[exp.Properties] = expression.args.get("properties")
+            if properties and properties.find(exp.ExternalProperty):
+                return True  # CREATE EXTERNAL TABLE is Hive
+
+            if not isinstance(expression.expression, exp.Select):
+                return True  # any CREATE TABLE other than CREATE TABLE AS SELECT is Hive
+        else:
+            return expression.kind != "VIEW"  # CREATE VIEW is never Hive but CREATE SCHEMA etc is
+
+    elif isinstance(expression, exp.Alter) or isinstance(expression, exp.Drop):
+        return True  # all ALTER and DROP statements are Hive
+
+    return False
+
+
 class Athena(Trino):
+    """
+    Over the years, it looks like AWS has taken various execution engines, bolted on AWS-specific modifications and then
+    built the Athena service around them.
+
+    Thus, Athena is not simply hosted Trino, it's more like a router that routes SQL queries to an execution engine depending
+    on the query type.
+
+    As at 2024-09-10, assuming your Athena workgroup is configured to use "Athena engine version 3", the following engines exist:
+
+    Hive:
+     - Accepts mostly the same syntax as Hadoop / Hive
+     - Uses backticks to quote identifiers
+     - Has a distinctive DDL syntax (around things like setting table properties, storage locations etc) that is different from Trino
+     - Used for *most* DDL, with some exceptions that get routed to the Trino engine instead:
+        - CREATE [EXTERNAL] TABLE (without AS SELECT)
+        - ALTER
+        - DROP
+
+    Trino:
+      - Uses double quotes to quote identifiers
+      - Used for DDL operations that involve SELECT queries, eg:
+        - CREATE VIEW
+        - CREATE TABLE... AS SELECT
+      - Used for DML operations
+        - SELECT, INSERT, UPDATE, DELETE, MERGE
+
+    The SQLGlot Athena dialect tries to identify which engine a query would be routed to and then uses the parser / generator for that engine
+    rather than trying to create a universal syntax that can handle both types.
+    """
+
     class Tokenizer(Trino.Tokenizer):
+        """
+        The Tokenizer is flexible enough to tokenize queries across both the Hive and Trino engines
+        """
+
         IDENTIFIERS = ['"', "`"]
         KEYWORDS = {
+            **Hive.Tokenizer.KEYWORDS,
             **Trino.Tokenizer.KEYWORDS,
             "UNLOAD": TokenType.COMMAND,
         }
 
     class Parser(Trino.Parser):
+        """
+        Parse queries for the Athena Trino execution engine
+        """
+
         STATEMENT_PARSERS = {
             **Trino.Parser.STATEMENT_PARSERS,
             TokenType.USING: lambda self: self._parse_as_command(self._prev),
         }
 
     class Generator(Trino.Generator):
-        WITH_PROPERTIES_PREFIX = "TBLPROPERTIES"
-
-        PROPERTIES_LOCATION = {
-            **Trino.Generator.PROPERTIES_LOCATION,
-            exp.LocationProperty: exp.Properties.Location.POST_SCHEMA,
-        }
+        """
+        Generate queries for the Athena Trino execution engine
+        """
 
         TYPE_MAPPING = {
             **Trino.Generator.TYPE_MAPPING,
@@ -37,23 +94,15 @@ class Athena(Trino):
             exp.FileFormatProperty: lambda self, e: f"'FORMAT'={self.sql(e, 'this')}",
         }
 
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            hive_kwargs = {**kwargs, "dialect": "hive"}
+
+            self._hive_generator = Hive.Generator(*args, **hive_kwargs)
+
         def generate(self, expression: exp.Expression, copy: bool = True) -> str:
-            if isinstance(expression, exp.DDL) or isinstance(expression, exp.Drop):
-                # Athena DDL uses backticks for quoting, unlike Athena DML which uses double quotes
-                # ...unless the DDL is CREATE VIEW, then it uses DML quoting, I guess because the view is based on a SELECT query
-                # ref: https://docs.aws.amazon.com/athena/latest/ug/reserved-words.html
-                # ref: https://docs.aws.amazon.com/athena/latest/ug/tables-databases-columns-names.html#table-names-that-include-numbers
-                if not (isinstance(expression, exp.Create) and expression.kind == "VIEW"):
-                    self._identifier_start = "`"
-                    self._identifier_end = "`"
+            if _generate_as_hive(expression):
+                return self._hive_generator.generate(expression, copy)
 
-            try:
-                return super().generate(expression, copy)
-            finally:
-                self._identifier_start = self.dialect.IDENTIFIER_START
-                self._identifier_end = self.dialect.IDENTIFIER_END
-
-        def property_sql(self, expression: exp.Property) -> str:
-            return (
-                f"{self.property_name(expression, string_key=True)}={self.sql(expression, 'value')}"
-            )
+            return super().generate(expression, copy)

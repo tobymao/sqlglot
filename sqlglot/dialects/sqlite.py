1d16ee2f3e6242bbc8dea7270c427ae8c8f66816
George Sittas <giwrgos.sittas@gmail.com>
2024-10-02 18:28:58 +0300

    Fix style

--- sqlglot/dialects/sqlite.py
+++ sqlglot/dialects/sqlite.py
@@ -187,7 +187,9 @@ class SQLite(Dialect):
                     transforms.eliminate_semi_and_anti_joins,
                 ]
             ),
-            exp.StrPosition: lambda self, e: str_position_sql(self, e, str_position_func_name="INSTR"),
+            exp.StrPosition: lambda self, e: str_position_sql(
+                self, e, str_position_func_name="INSTR"
+            ),
             exp.TableSample: no_tablesample_sql,
             exp.TimeStrToTime: lambda self, e: self.sql(e, "this"),
             exp.TimeToStr: lambda self, e: self.func("STRFTIME", e.args.get("format"), e.this),

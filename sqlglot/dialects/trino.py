08b2b3ae4697ee17a6f15bedf2ec45e5567e9aba
Vaggelis Danias <daniasevangelos@gmail.com>
2024-10-03 18:11:50 +0300

    feat(trino): Support JSON_QUERY (#4206)
    * feat(trino): Support JSON_QUERY
    
    * PR Feedback 1

--- sqlglot/dialects/trino.py
+++ sqlglot/dialects/trino.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from sqlglot import exp
+from sqlglot import exp, parser
 from sqlglot.dialects.dialect import merge_without_target_sql, trim_sql, timestrtotime_sql
 from sqlglot.dialects.presto import Presto
+from sqlglot.tokens import TokenType
 
 
 class Trino(Presto):
@@ -13,7 +14,29 @@ class Trino(Presto):
         FUNCTION_PARSERS = {
             **Presto.Parser.FUNCTION_PARSERS,
             "TRIM": lambda self: self._parse_trim(),
+            "JSON_QUERY": lambda self: self._parse_json_query(),
         }
+
+        JSON_QUERY_OPTIONS: parser.OPTIONS_TYPE = {
+            **dict.fromkeys(
+                ("WITH", "WITHOUT"),
+                (
+                    ("CONDITIONAL", "WRAPPER"),
+                    ("CONDITIONAL", "ARRAY", "WRAPPED"),
+                    ("UNCONDITIONAL", "WRAPPER"),
+                    ("UNCONDITIONAL", "ARRAY", "WRAPPER"),
+                ),
+            ),
+        }
+
+        def _parse_json_query(self):
+            return self.expression(
+                exp.JSONExtract,
+                this=self._parse_bitwise(),
+                expression=self._match(TokenType.COMMA) and self._parse_bitwise(),
+                option=self._parse_var_from_options(self.JSON_QUERY_OPTIONS, raise_unmatched=False),
+                json_query=True,
+            )
 
     class Generator(Presto.Generator):
         TRANSFORMS = {
@@ -23,6 +46,7 @@ class Trino(Presto):
             exp.Merge: merge_without_target_sql,
             exp.TimeStrToTime: lambda self, e: timestrtotime_sql(self, e, include_precision=True),
             exp.Trim: trim_sql,
+            exp.JSONExtract: lambda self, e: self.jsonextract_sql(e),
         }
 
         SUPPORTED_JSON_PATH_PARTS = {
@@ -30,6 +54,16 @@ class Trino(Presto):
             exp.JSONPathRoot,
             exp.JSONPathSubscript,
         }
+
+        def jsonextract_sql(self, expression: exp.JSONExtract) -> str:
+            if not expression.args.get("json_query"):
+                return super().jsonextract_sql(expression)
+
+            json_path = self.sql(expression, "expression")
+            option = self.sql(expression, "option")
+            option = f" {option}" if option else ""
+
+            return self.func("JSON_QUERY", expression.this, json_path + option)
 
     class Tokenizer(Presto.Tokenizer):
         HEX_STRINGS = [("X'", "'")]
--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -5738,6 +5738,8 @@ class JSONExtract(Binary, Func):
         "only_json_types": False,
         "expressions": False,
         "variant_extract": False,
+        "json_query": False,
+        "option": False,
     }
     _sql_names = ["JSON_EXTRACT"]
     is_var_len_args = True

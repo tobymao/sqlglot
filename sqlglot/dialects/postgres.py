14b4e47808841c316d5f218dc825c55890d6d01e
Vaggelis Danias <daniasevangelos@gmail.com>
2024-11-08 18:18:30 +0200

    Update comment 2

--- sqlglot/dialects/postgres.py
+++ sqlglot/dialects/postgres.py
@@ -240,8 +240,8 @@ def _unix_to_time_sql(self: Postgres.Generator, expression: exp.UnixToTime) -> s
 def _build_levenshtein_less_equal(args: t.List) -> exp.Levenshtein:
     # Postgres has two signatures for levenshtein_less_equal function, but in both cases 
     # max_dist is the last argument
-    # levenshtein_less_equal(source text, target text, ins_cost int, del_cost int, sub_cost int, max_d int) returns int
-    # levenshtein_less_equal(source text, target text, max_d int) returns int
+    # levenshtein_less_equal(source, target, ins_cost, del_cost, sub_cost, max_d)
+    # levenshtein_less_equal(source, target, max_d)
     max_dist = args.pop()
 
     return exp.Levenshtein(

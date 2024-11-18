c27a8aff38f8290277eb9eb5dd5019a1cfa9e0ba
vaggelisd <daniasevangelos@gmail.com>
2024-11-18 19:51:41 +0200

    PR Feedback 1

--- sqlglot/dialects/duckdb.py
+++ sqlglot/dialects/duckdb.py
@@ -1021,23 +1021,3 @@ class DuckDB(Dialect):
             return self.func(
                 "REGEXP_EXTRACT", expression.this, expression.expression, group, params
             )
-
-        def attach_sql(self, expression: exp.Attach) -> str:
-            this = self.sql(expression, "this")
-            exists_sql = " IF NOT EXISTS" if expression.args.get("exists") else ""
-            expressions = self.expressions(expression)
-            expressions = f" ({expressions})" if expressions else ""
-
-            return f"ATTACH{exists_sql} {this}{expressions}"
-
-        def detach_sql(self, expression: exp.Detach) -> str:
-            this = self.sql(expression, "this")
-            exists_sql = " IF EXISTS" if expression.args.get("exists") else ""
-
-            return f"DETACH{exists_sql} {this}"
-
-        def attachoption_sql(self, expression: exp.AttachOption) -> str:
-            this = self.sql(expression, "this")
-            value = self.sql(expression, "expression")
-            value = f" {value}" if value else ""
-            return f"{this}{value}"

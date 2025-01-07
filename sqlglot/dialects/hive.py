83595b67f0aa4cafdfcf4bace7b92c17f9e9f5f3
George Sittas <giwrgos.sittas@gmail.com>
2025-01-07 19:41:47 +0200

    Feat(hive)!: parse ASCII into Unicode to facilitate transpilation

--- sqlglot/dialects/hive.py
+++ sqlglot/dialects/hive.py
@@ -277,6 +277,7 @@ class Hive(Dialect):
 
         FUNCTIONS = {
             **parser.Parser.FUNCTIONS,
+            "ASCII": exp.Unicode.from_arg_list,
             "BASE64": exp.ToBase64.from_arg_list,
             "COLLECT_LIST": lambda args: exp.ArrayAgg(this=seq_get(args, 0), nulls_excluded=True),
             "COLLECT_SET": exp.ArrayUniqueAgg.from_arg_list,
@@ -582,6 +583,7 @@ class Hive(Dialect):
             exp.TsOrDsDiff: _date_diff_sql,
             exp.TsOrDsToDate: _to_date_sql,
             exp.TryCast: no_trycast_sql,
+            exp.Unicode: rename_func("ASCII"),
             exp.UnixToStr: lambda self, e: self.func(
                 "FROM_UNIXTIME", e.this, time_format("hive")(self, e)
             ),

5a363801f5ac23622c40805189b3e84603989c4b
Jakub Pruzinec <66100884+pruzko@users.noreply.github.com>
2025-01-08 14:52:27 +0100

    Feat: add support for Chr in tsql and sqlite (#4566)
    * char vs chr and char(100 using utf8mb4) by default for mysql
    
    * removed default encoding for mysql
    
    * Revert MySQL
    
    * Revert tests
    
    ---------
    
    Co-authored-by: Jo <46752250+georgesittas@users.noreply.github.com>

--- tests/dialects/test_mysql.py
+++ tests/dialects/test_mysql.py
@@ -332,6 +332,8 @@ class TestMySQL(Validator):
             write={
                 "mysql": "CHAR(10)",
                 "presto": "CHR(10)",
+                "sqlite": "CHAR(10)",
+                "tsql": "CHAR(10)",
             },
         )
 

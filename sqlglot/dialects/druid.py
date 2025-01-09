c3950f779cd89ea7ac2f02e15300a3e98ba8daa1
Beto Dealmeida <roberto@dealmeida.net>
2025-01-09 11:21:02 -0500

    Fix mapping

--- sqlglot/dialects/druid.py
+++ sqlglot/dialects/druid.py
@@ -7,16 +7,8 @@ class Druid(Dialect):
         # https://druid.apache.org/docs/latest/querying/sql-data-types/
         TYPE_MAPPING = {
             **generator.Generator.TYPE_MAPPING,
-            exp.DataType.Type.CHAR: "STRING",
             exp.DataType.Type.NCHAR: "STRING",
             exp.DataType.Type.NVARCHAR: "STRING",
             exp.DataType.Type.TEXT: "STRING",
-            exp.DataType.Type.VARCHAR: "STRING",
             exp.DataType.Type.UUID: "STRING",
-            exp.DataType.Type.DECIMAL: "DOUBLE",
-            exp.DataType.Type.BOOLEAN: "LONG",
-            exp.DataType.Type.TINYINT: "LONG",
-            exp.DataType.Type.SMALLINT: "LONG",
-            exp.DataType.Type.INT: "LONG",
-            exp.DataType.Type.BIGINT: "LONG",
         }

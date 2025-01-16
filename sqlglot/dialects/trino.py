585983388cca6ffbfb693f2013566cee07b6f1ba
George Sittas <giwrgos.sittas@gmail.com>
2025-01-16 17:43:03 +0200

    Fix(trino): generate ArrayUniqueAgg as ARRAY_AGG(DISTINCT ...)

--- sqlglot/dialects/trino.py
+++ sqlglot/dialects/trino.py
@@ -47,6 +47,7 @@ class Trino(Presto):
             **Presto.Generator.TRANSFORMS,
             exp.ArraySum: lambda self,
             e: f"REDUCE({self.sql(e, 'this')}, 0, (acc, x) -> acc + x, acc -> acc)",
+            exp.ArrayUniqueAgg: lambda self, e: f"ARRAY_AGG(DISTINCT {self.sql(e, 'this')})",
             exp.Merge: merge_without_target_sql,
             exp.TimeStrToTime: lambda self, e: timestrtotime_sql(self, e, include_precision=True),
             exp.Trim: trim_sql,

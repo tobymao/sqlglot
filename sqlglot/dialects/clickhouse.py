d8ade830bbca4d2893a7e406868a0bd3a654057e
Constantine Peresypkin <pkit@users.noreply.github.com>
2025-01-16 00:48:41 -0800

    feat(clickhouse): Dynamic data type (#4624)
    * feat(clickhouse): Dynamic data type
    
    see https://clickhouse.com/docs/en/sql-reference/data-types/dynamic
    
    * Styling fixup
    
    ---------
    
    Co-authored-by: Jo <46752250+georgesittas@users.noreply.github.com>

--- sqlglot/dialects/clickhouse.py
+++ sqlglot/dialects/clickhouse.py
@@ -447,6 +447,14 @@ class ClickHouse(Dialect):
 
         FUNCTION_PARSERS.pop("MATCH")
 
+        PROPERTY_PARSERS = parser.Parser.PROPERTY_PARSERS.copy()
+        PROPERTY_PARSERS.pop("DYNAMIC")
+
+        ENUM_TYPE_TOKENS = {
+            *parser.Parser.ENUM_TYPE_TOKENS,
+            TokenType.DYNAMIC,
+        }
+
         NO_PAREN_FUNCTION_PARSERS = parser.Parser.NO_PAREN_FUNCTION_PARSERS.copy()
         NO_PAREN_FUNCTION_PARSERS.pop("ANY")
 
@@ -953,6 +961,7 @@ class ClickHouse(Dialect):
             exp.DataType.Type.MULTIPOLYGON: "MultiPolygon",
             exp.DataType.Type.AGGREGATEFUNCTION: "AggregateFunction",
             exp.DataType.Type.SIMPLEAGGREGATEFUNCTION: "SimpleAggregateFunction",
+            exp.DataType.Type.DYNAMIC: "Dynamic",
         }
 
         TRANSFORMS = {

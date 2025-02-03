af47346461ea05d86fb26d356df9b7613fd10ca1
George Sittas <giwrgos.sittas@gmail.com>
2025-02-03 20:44:00 +0200

    Fix(redshift): generate proper syntax for column type alteration

--- sqlglot/dialects/redshift.py
+++ sqlglot/dialects/redshift.py
@@ -158,6 +158,7 @@ class Redshift(Postgres):
         SUPPORTS_CONVERT_TIMEZONE = True
         EXCEPT_INTERSECT_SUPPORT_ALL_CLAUSE = False
         SUPPORTS_MEDIAN = True
+        ALTER_SET_TYPE = "TYPE"
 
         # Redshift doesn't have `WITH` as part of their with_properties so we remove it
         WITH_PROPERTIES_PREFIX = " "

e05983a62ee9b3689e8a277924f2b1e36744a2a9
Jo <46752250+georgesittas@users.noreply.github.com>
2025-02-26 00:35:42 +0200

    Fix(tokenizer-rs)!: make hex literal tokenization more robust (#4802)

--- tests/dialects/test_dune.py
+++ tests/dialects/test_dune.py
@@ -1,3 +1,4 @@
+from sqlglot import exp
 from tests.dialects.test_dialect import Validator
 
 
@@ -8,16 +9,28 @@ class TestDune(Validator):
         self.validate_identity("CAST(x AS INT256)")
         self.validate_identity("CAST(x AS UINT256)")
 
-        self.validate_all(
-            "SELECT 0xdeadbeef",
-            read={
-                "dune": "SELECT X'deadbeef'",
-                "postgres": "SELECT x'deadbeef'",
-                "trino": "SELECT X'deadbeef'",
-            },
-            write={
-                "dune": "SELECT 0xdeadbeef",
-                "postgres": "SELECT x'deadbeef'",
-                "trino": "SELECT x'deadbeef'",
-            },
-        )
+        for hex_literal in (
+            "deadbeef",
+            "deadbeefdead",
+            "deadbeefdeadbeef",
+            "deadbeefdeadbeefde",
+            "deadbeefdeadbeefdead",
+            "deadbeefdeadbeefdeadbeef",
+            "deadbeefdeadbeefdeadbeefdeadbeef",
+        ):
+            with self.subTest(f"Transpiling hex literal {hex_literal}"):
+                self.parse_one(f"0x{hex_literal}").assert_is(exp.HexString)
+
+                self.validate_all(
+                    f"SELECT 0x{hex_literal}",
+                    read={
+                        "dune": f"SELECT X'{hex_literal}'",
+                        "postgres": f"SELECT x'{hex_literal}'",
+                        "trino": f"SELECT X'{hex_literal}'",
+                    },
+                    write={
+                        "dune": f"SELECT 0x{hex_literal}",
+                        "postgres": f"SELECT x'{hex_literal}'",
+                        "trino": f"SELECT x'{hex_literal}'",
+                    },
+                )

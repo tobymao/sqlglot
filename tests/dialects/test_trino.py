912a4c23762918efa16dfd61a93bb29598cc9ec9
George Sittas <giwrgos.sittas@gmail.com>
2025-02-26 15:16:37 +0200

    Feat(trino): add support for ON ... ERROR/NULL syntax for JSON_QUERY

--- tests/dialects/test_trino.py
+++ tests/dialects/test_trino.py
@@ -5,6 +5,7 @@ class TestTrino(Validator):
     dialect = "trino"
 
     def test_trino(self):
+        self.validate_identity("JSON_QUERY(m.properties, 'lax $.area' OMIT QUOTES NULL ON ERROR)")
         self.validate_identity("JSON_EXTRACT(content, json_path)")
         self.validate_identity("JSON_QUERY(content, 'lax $.HY.*')")
         self.validate_identity("JSON_QUERY(content, 'strict $.HY.*' WITH WRAPPER)")

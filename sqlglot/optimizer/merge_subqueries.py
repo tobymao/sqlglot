0b1bba15266f9dd49de8e910c4df1a222a10ce18
Jo <46752250+georgesittas@users.noreply.github.com>
2025-03-26 18:05:31 +0200

    Fix(optimizer): avoid merging window function nested under a projection (#4919)

--- sqlglot/optimizer/merge_subqueries.py
+++ sqlglot/optimizer/merge_subqueries.py
@@ -129,8 +129,7 @@ def _mergeable(
     inner_select = inner_scope.expression.unnest()
 
     def _is_a_window_expression_in_unmergable_operation():
-        window_expressions = inner_select.find_all(exp.Window)
-        window_alias_names = {window.parent.alias_or_name for window in window_expressions}
+        window_aliases = {s.alias_or_name for s in inner_select.selects if s.find(exp.Window)}
         inner_select_name = from_or_join.alias_or_name
         unmergable_window_columns = [
             column
@@ -142,7 +141,7 @@ def _mergeable(
         window_expressions_in_unmergable = [
             column
             for column in unmergable_window_columns
-            if column.table == inner_select_name and column.name in window_alias_names
+            if column.table == inner_select_name and column.name in window_aliases
         ]
         return any(window_expressions_in_unmergable)
 

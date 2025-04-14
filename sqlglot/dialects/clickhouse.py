06714f195f365bb061764c69799bc7d3557d696e
Ricky Nguyen <lepfhty@users.noreply.github.com>
2025-04-14 04:57:07 -0700

    fix(clickhouse): allow string literal for clickhouse ON CLUSTER clause (#4971)

--- sqlglot/dialects/clickhouse.py
+++ sqlglot/dialects/clickhouse.py
@@ -837,7 +837,7 @@ class ClickHouse(Dialect):
         def _parse_on_property(self) -> t.Optional[exp.Expression]:
             index = self._index
             if self._match_text_seq("CLUSTER"):
-                this = self._parse_id_var()
+                this = self._parse_string() or self._parse_id_var()
                 if this:
                     return self.expression(exp.OnCluster, this=this)
                 else:

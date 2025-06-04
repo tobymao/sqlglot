29ff3dee4292e5fa2788ffc4c69a5bb8b00c6e4c
geooo109 <geomichas96@gmail.com>
2025-06-05 01:36:18 +0300

    feat(parser): AGGREGATE with GROUP AND ORDER BY

--- sqlglot/parser.py
+++ sqlglot/parser.py
@@ -936,6 +936,7 @@ class Parser(metaclass=_Parser):
         "ORDER BY": lambda self, query: query.order_by(self._parse_order(), copy=False),
         "LIMIT": lambda self, query: self._parse_pipe_syntax_limit(query),
         "OFFSET": lambda self, query: query.offset(self._parse_offset(), copy=False),
+        "AGGREGATE": lambda self, query: self._parse_pipe_syntax_aggregate(query),
     }
 
     PROPERTY_PARSERS: t.Dict[str, t.Callable] = {
@@ -1141,6 +1142,61 @@ class Parser(metaclass=_Parser):
             query.limit(limit, copy=False)
         if offset:
             query.offset(offset, copy=False)
+        return query
+
+    def _parse_pipe_syntax_order(self) -> t.Optional[exp.Expression]:
+        this = self._parse_assignment()
+        if self._match_text_seq("GROUP", advance=False):
+            return this
+
+        this = self._parse_alias(this)
+
+        if self._match_set((TokenType.ASC, TokenType.DESC), advance=False):
+            return self._parse_ordered(lambda: this)
+
+        return this
+
+    def _parse_pipe_syntax_aggregate_group_order_by(
+        self, query: exp.Select, group_by_exists: bool = True
+    ) -> exp.Select:
+        expr = self._parse_csv(self._parse_pipe_syntax_order)
+        aggregates_or_groups, orders = [], []
+        for element in expr:
+            if isinstance(element, exp.Ordered):
+                this = element.this
+                orders.append(
+                    exp.Ordered(
+                        this=this.alias_or_name,
+                        desc=element.args.get("desc"),
+                        nulls_first=element.args.get("nulls_first"),
+                        with_fill=element.args.get("with_fill"),
+                    )
+                )
+                aggregates_or_groups.append(this)
+            else:
+                aggregates_or_groups.append(element)
+
+        if group_by_exists:
+            query = query.select(*aggregates_or_groups, copy=False).group_by(
+                *[element.alias_or_name for element in aggregates_or_groups], copy=False
+            )
+        else:
+            query = exp.select(*aggregates_or_groups, copy=False).from_(
+                query.subquery(copy=False), copy=False
+            )
+
+        if orders:
+            return query.order_by(*orders, copy=False)
+
+        return query
+
+    def _parse_pipe_syntax_aggregate(self, query: exp.Select) -> exp.Query:
+        self._match_text_seq("AGGREGATE")
+        query = self._parse_pipe_syntax_aggregate_group_order_by(query, group_by_exists=False)
+
+        if self._match(TokenType.GROUP_BY) or self._match_text_seq("GROUP", "AND", "ORDER BY"):
+            return self._parse_pipe_syntax_aggregate_group_order_by(query)
+
         return query
 
     def _parse_partitioned_by_bucket_or_truncate(self) -> exp.Expression:

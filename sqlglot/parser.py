f7c53031439102b7db4fef46e62790e800aed617
geooo109 <geomichas96@gmail.com>
2025-06-05 11:00:21 +0300

    let GROUP be an alias for AGGREGATE

--- sqlglot/parser.py
+++ sqlglot/parser.py
@@ -1146,7 +1146,9 @@ class Parser(metaclass=_Parser):
 
     def _parse_pipe_syntax_aggregate_fields(self) -> t.Optional[exp.Expression]:
         this = self._parse_assignment()
-        if self._match_text_seq("GROUP", advance=False):
+        if self._match(TokenType.GROUP_BY, advance=False) or self._match_text_seq(
+            "GROUP", "AND", advance=False
+        ):
             return this
 
         this = self._parse_alias(this)

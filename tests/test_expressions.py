29e2f1d89c095c9fab0944a6962c99bd745c2c91
Harish Ravi <100912891+HarishRavi96@users.noreply.github.com>
2025-06-09 14:24:51 +0530

    feat!: Array_intersection transpilation support (#5186)
    * Array_intersection support added
    
    * updated it with priority dialect

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -669,6 +669,8 @@ class TestExpressions(unittest.TestCase):
         self.assertIsInstance(parse_one("ARRAY_AGG(a)"), exp.ArrayAgg)
         self.assertIsInstance(parse_one("ARRAY_CONTAINS(a, 'a')"), exp.ArrayContains)
         self.assertIsInstance(parse_one("ARRAY_SIZE(a)"), exp.ArraySize)
+        self.assertIsInstance(parse_one("ARRAY_INTERSECTION([1, 2], [2, 3])"), exp.ArrayIntersect)
+        self.assertIsInstance(parse_one("ARRAY_INTERSECT([1, 2], [2, 3])"), exp.ArrayIntersect)
         self.assertIsInstance(parse_one("AVG(a)"), exp.Avg)
         self.assertIsInstance(parse_one("BEGIN DEFERRED TRANSACTION"), exp.Transaction)
         self.assertIsInstance(parse_one("CEIL(a)"), exp.Ceil)
@@ -1226,3 +1228,20 @@ FROM foo""",
 
     def test_parse_identifier(self):
         self.assertEqual(exp.parse_identifier("a ' b"), exp.to_identifier("a ' b"))
+
+    def test_array_intersection(self):
+        expr = parse_one("ARRAY_INTERSECTION([1, 2], [2, 3])")
+        self.assertIsInstance(expr, exp.ArrayIntersect)
+        self.assertEqual(expr.sql(dialect="snowflake"), "ARRAY_INTERSECTION([1, 2], [2, 3])")
+
+        expr2 = parse_one("ARRAY_INTERSECT([1, 2], [2, 3])")
+        self.assertIsInstance(expr2, exp.ArrayIntersect)
+        self.assertEqual(expr2.sql(dialect="snowflake"), "ARRAY_INTERSECTION([1, 2], [2, 3])")
+
+        expr = parse_one("ARRAY_INTERSECT([1, 2], [2, 3])")
+        self.assertIsInstance(expr, exp.ArrayIntersect)
+        self.assertEqual(expr.sql(dialect="starrocks"), "ARRAY_INTERSECT([1, 2], [2, 3])")
+
+        expr2 = parse_one("ARRAY_INTERSECTION([1, 2], [2, 3])")
+        self.assertIsInstance(expr2, exp.ArrayIntersect)
+        self.assertEqual(expr2.sql(dialect="starrocks"), "ARRAY_INTERSECT([1, 2], [2, 3])")

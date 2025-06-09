589a6fe2d306b76454db78228c1c49718619c5a8
Swathiraj23 <49307137+Swathiraj23@users.noreply.github.com>
2025-06-09 14:46:53 +0530

    fix(starrocks): array_agg_transpilation_fix (#5190)
    Co-authored-by: Durairajan, Swathi <Swathi_Durairajan@comcast.com>

--- tests/dialects/test_starrocks.py
+++ tests/dialects/test_starrocks.py
@@ -9,6 +9,7 @@ class TestStarrocks(Validator):
         self.validate_identity("SELECT ARRAY_JOIN([1, 3, 5, NULL], '_', 'NULL')")
         self.validate_identity("SELECT ARRAY_JOIN([1, 3, 5, NULL], '_')")
         self.validate_identity("ALTER TABLE a SWAP WITH b")
+        self.validate_identity("SELECT ARRAY_AGG(a) FROM x")
 
     def test_ddl(self):
         ddl_sqls = [

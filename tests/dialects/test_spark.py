c20f85e3e171e502fc51f74894d3313f0ad61535
Jo <46752250+georgesittas@users.noreply.github.com>
2025-06-11 17:51:11 +0300

    Feat(spark): support ALTER ADD PARTITION (#5208)
    * Feat(spark): support ALTER ADD PARTITION
    
    * PR feedback

--- tests/dialects/test_spark.py
+++ tests/dialects/test_spark.py
@@ -247,6 +247,8 @@ TBLPROPERTIES (
             "REFRESH TABLE t",
         )
 
+        self.validate_identity("ALTER TABLE foo ADD PARTITION(event = 'click')")
+        self.validate_identity("ALTER TABLE foo ADD IF NOT EXISTS PARTITION(event = 'click')")
         self.validate_identity("IF(cond, foo AS bar, bla AS baz)")
         self.validate_identity("any_value(col, true)", "ANY_VALUE(col) IGNORE NULLS")
         self.validate_identity("first(col, true)", "FIRST(col) IGNORE NULLS")

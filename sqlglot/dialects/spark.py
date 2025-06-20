1d049bfc6c5b89fcbe8a45a3f238ab623f9f507b
Giorgos Michas <geomichas96@gmail.com>
2025-06-20 19:33:02 +0300

    feat(spark, databricks): parse brackets as placeholder (#5256)
    * feat(spark, databricks): parse brackets as placeholder
    
    * fix format
    
    * PR feedback 1 (george)
    
    * refactor placeholder expr

--- sqlglot/dialects/spark.py
+++ sqlglot/dialects/spark.py
@@ -7,6 +7,7 @@ from sqlglot.dialects.dialect import rename_func, unit_to_var, timestampdiff_sql
 from sqlglot.dialects.hive import _build_with_ignore_nulls
 from sqlglot.dialects.spark2 import Spark2, temporary_storage_provider, _build_as_cast
 from sqlglot.helper import ensure_list, seq_get
+from sqlglot.tokens import TokenType
 from sqlglot.transforms import (
     ctas_with_tmp_tables_to_create_tmp_view,
     remove_unique_constraints,
@@ -121,6 +122,16 @@ class Spark(Spark2):
             ),
         }
 
+        PLACEHOLDER_PARSERS = {
+            **Spark2.Parser.PLACEHOLDER_PARSERS,
+            TokenType.L_BRACE: lambda self: self._parse_query_parameter(),
+        }
+
+        def _parse_query_parameter(self) -> t.Optional[exp.Expression]:
+            this = self._parse_id_var()
+            self._match(TokenType.R_BRACE)
+            return self.expression(exp.Placeholder, this=this, widget=True)
+
         def _parse_generated_as_identity(
             self,
         ) -> (
@@ -200,3 +211,9 @@ class Spark(Spark2):
                 return self.func("DATEDIFF", unit_to_var(expression), start, end)
 
             return self.func("DATEDIFF", end, start)
+
+        def placeholder_sql(self, expression: exp.Placeholder) -> str:
+            if not expression.args.get("widget"):
+                return super().placeholder_sql(expression)
+
+            return f"{{{expression.name}}}"

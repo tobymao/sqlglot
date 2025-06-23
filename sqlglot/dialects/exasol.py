b90fd722f453696ee8b2f68abaa03049db62e5cb
nnamdi16 <nwabuokeinnamdi@gmail.com>
2025-06-23 17:41:03 +0200

    chore: removed redundant code and properties from the implementation of Exasol dialect.

--- sqlglot/dialects/exasol.py
+++ sqlglot/dialects/exasol.py
@@ -1,39 +1,12 @@
 from __future__ import annotations
-from sqlglot import exp, generator, parser
+from sqlglot import exp, generator
 from sqlglot.dialects.dialect import (
     Dialect,
 )
 
 
 class Exasol(Dialect):
-    class Parser(parser.Parser):
-        FUNCTIONS = {
-            **parser.Parser.FUNCTIONS,
-        }
-
-        CONSTRAINT_PARSERS = {
-            **parser.Parser.CONSTRAINT_PARSERS,
-        }
-
-        STRING_ALIASES = True
-        MODIFIERS_ATTACHED_TO_SET_OP = False
-        OPTIONAL_ALIAS_TOKEN_CTE = False
-
     class Generator(generator.Generator):
-        QUERY_HINTS = False
-        STRUCT_DELIMITER = ("(", ")")
-        NVL2_SUPPORTED = False
-        TABLESAMPLE_REQUIRES_PARENS = False
-        TABLESAMPLE_SIZE_IS_ROWS = False
-        LAST_DAY_SUPPORTS_DATE_PART = False
-        SUPPORTS_TO_NUMBER = True
-        JOIN_HINTS = False
-        TABLE_HINTS = False
-        GROUPINGS_SEP = ""
-        SET_OP_MODIFIERS = False
-        VALUES_AS_TABLE = False
-        ARRAY_SIZE_NAME = "LENGTH"
-
         # https://docs.exasol.com/db/latest/sql_references/data_types/datatypedetails.htm#StringDataType
         STRING_TYPE_MAPPING = {
             exp.DataType.Type.BLOB: "VARCHAR",

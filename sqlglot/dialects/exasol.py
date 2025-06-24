e4d56180cbed08abe392fd0661a3f741e3a4d748
nnamdi16 <nwabuokeinnamdi@gmail.com>
2025-06-24 17:33:21 +0200

    fixes: refactored the test, mappings and method overriding

--- sqlglot/dialects/exasol.py
+++ sqlglot/dialects/exasol.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
-from sqlglot import exp, generator
+from sqlglot import exp, generator, tokens
 from sqlglot.dialects.dialect import Dialect
+from sqlglot.tokens import TokenType
 
 
 class Exasol(Dialect):
+    class Tokenizer(tokens.Tokenizer):
+        KEYWORDS = {
+            **tokens.Tokenizer.KEYWORDS,
+            "DATETIME2": TokenType.DATETIME2,
+            "SMALLDATETIME": TokenType.SMALLDATETIME,
+        }
+
     class Generator(generator.Generator):
         # https://docs.exasol.com/db/latest/sql_references/data_types/datatypedetails.htm#StringDataType
         STRING_TYPE_MAPPING = {
@@ -23,26 +31,11 @@ class Exasol(Dialect):
             **STRING_TYPE_MAPPING,
             # https://docs.exasol.com/db/latest/sql_references/data_types/datatypealiases.htm
             exp.DataType.Type.TINYINT: "SMALLINT",
-            exp.DataType.Type.INT: "INTEGER",
-            exp.DataType.Type.MEDIUMINT: "INTEGER",
-            exp.DataType.Type.DOUBLE: "DOUBLE PRECISION",
+            exp.DataType.Type.MEDIUMINT: "INT",
             exp.DataType.Type.DECIMAL32: "DECIMAL",
             exp.DataType.Type.DECIMAL64: "DECIMAL",
             exp.DataType.Type.DECIMAL128: "DECIMAL",
             exp.DataType.Type.DECIMAL256: "DECIMAL",
             exp.DataType.Type.DATETIME: "TIMESTAMP",
-            exp.DataType.Type.DATETIME2: "TIMESTAMP",
-            exp.DataType.Type.SMALLDATETIME: "TIMESTAMP",
             exp.DataType.Type.TIMESTAMPLTZ: "TIMESTAMP WITH LOCAL TIME ZONE",
         }
-
-        def datatype_sql(self, expression: exp.DataType) -> str:
-            """
-            Exasol  does not support `DATETIME2` and `SMALLDATETIME` data type but converts it to `TIMESTAMP`
-            https://docs.exasol.com/db/latest/sql_references/data_types/datatypedetails.htm#DateTime.
-            """
-            kind = expression.args.get("kind")
-            if kind and kind.upper() in {"DATETIME2", "SMALLDATETIME"}:
-                expression.set("this", exp.DataType.Type.TIMESTAMP)
-
-            return super().datatype_sql(expression)

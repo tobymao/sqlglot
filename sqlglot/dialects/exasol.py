96509ae018e7fea8a0fa3ba73ef34a55e6f5acea
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2025-06-25 14:43:29 +0200

    feat(exasol): added datatype mappings and test for exasol dialect. (#5270)
    * feat(exasol): added datatype mappings and test for exasol dialect.
    
    * fixes: refactored the test, mappings and method overriding
    
    * fix: refactored the tokenizer mapping and override the datatype generation for Exasol dialect to handle temporal types correctly.
    
    * fix: added test cases for  DATETIME2, SMALLDATETIME
    
    * fix:  fixed linting
    
    * fix: refactored the datatype_sql override.

--- sqlglot/dialects/exasol.py
+++ sqlglot/dialects/exasol.py
@@ -21,4 +21,25 @@ class Exasol(Dialect):
         TYPE_MAPPING = {
             **generator.Generator.TYPE_MAPPING,
             **STRING_TYPE_MAPPING,
+            # https://docs.exasol.com/db/latest/sql_references/data_types/datatypealiases.htm
+            exp.DataType.Type.TINYINT: "SMALLINT",
+            exp.DataType.Type.MEDIUMINT: "INT",
+            exp.DataType.Type.DECIMAL32: "DECIMAL",
+            exp.DataType.Type.DECIMAL64: "DECIMAL",
+            exp.DataType.Type.DECIMAL128: "DECIMAL",
+            exp.DataType.Type.DECIMAL256: "DECIMAL",
+            exp.DataType.Type.DATETIME: "TIMESTAMP",
+            exp.DataType.Type.TIMESTAMPLTZ: "TIMESTAMP WITH LOCAL TIME ZONE",
         }
+
+        def datatype_sql(self, expression: exp.DataType) -> str:
+            """
+            Override datatype generation to align with Exasol's support for temporal types.
+
+            Exasol supports a fixed default precision for TIMESTAMP WITH LOCAL TIME ZONE and does not allow specifying a custom precision.
+            This method strips any precision argument from TIMESTAMPLTZ types to ensure compatibility with Exasol's syntax.
+            """
+            if expression.is_type(exp.DataType.Type.TIMESTAMPLTZ):
+                return "TIMESTAMP WITH LOCAL TIME ZONE"
+
+            return super().datatype_sql(expression)
--- tests/dialects/test_exasol.py
+++ tests/dialects/test_exasol.py
@@ -17,3 +17,43 @@ class TestExasol(Validator):
         self.validate_identity("CAST(x AS VARBINARY)", "CAST(x AS VARCHAR)")
         self.validate_identity("CAST(x AS VARCHAR)", "CAST(x AS VARCHAR)")
         self.validate_identity("CAST(x AS CHAR)", "CAST(x AS CHAR)")
+        self.validate_identity("CAST(x AS TINYINT)", "CAST(x AS SMALLINT)")
+        self.validate_identity("CAST(x AS SMALLINT)")
+        self.validate_identity("CAST(x AS INT)")
+        self.validate_identity("CAST(x AS MEDIUMINT)", "CAST(x AS INT)")
+        self.validate_identity("CAST(x AS BIGINT)")
+        self.validate_identity("CAST(x AS FLOAT)")
+        self.validate_identity("CAST(x AS DOUBLE)")
+        self.validate_identity("CAST(x AS DECIMAL32)", "CAST(x AS DECIMAL)")
+        self.validate_identity("CAST(x AS DECIMAL64)", "CAST(x AS DECIMAL)")
+        self.validate_identity("CAST(x AS DECIMAL128)", "CAST(x AS DECIMAL)")
+        self.validate_identity("CAST(x AS DECIMAL256)", "CAST(x AS DECIMAL)")
+        self.validate_identity("CAST(x AS DATE)")
+        self.validate_identity("CAST(x AS DATETIME)", "CAST(x AS TIMESTAMP)")
+        self.validate_identity("CAST(x AS TIMESTAMP)")
+        self.validate_all(
+            "CAST(x AS TIMESTAMP)",
+            read={
+                "tsql": "CAST(x AS DATETIME2)",
+            },
+            write={
+                "exasol": "CAST(x AS TIMESTAMP)",
+            },
+        )
+        self.validate_all(
+            "CAST(x AS TIMESTAMP)",
+            read={
+                "tsql": "CAST(x AS SMALLDATETIME)",
+            },
+            write={
+                "exasol": "CAST(x AS TIMESTAMP)",
+            },
+        )
+        self.validate_identity("CAST(x AS BOOLEAN)")
+        self.validate_identity(
+            "CAST(x AS TIMESTAMPLTZ)", "CAST(x AS TIMESTAMP WITH LOCAL TIME ZONE)"
+        )
+        self.validate_identity(
+            "CAST(x AS TIMESTAMP(3) WITH LOCAL TIME ZONE)",
+            "CAST(x AS TIMESTAMP WITH LOCAL TIME ZONE)",
+        )

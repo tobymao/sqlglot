4b07c868d0ce358222afc80135c9e5383a9c7446
George Sittas <giwrgos.sittas@gmail.com>
2025-06-25 15:45:34 +0300

    Cleanup

--- sqlglot/dialects/exasol.py
+++ sqlglot/dialects/exasol.py
@@ -18,10 +18,10 @@ class Exasol(Dialect):
             exp.DataType.Type.VARBINARY: "VARCHAR",
         }
 
+        # https://docs.exasol.com/db/latest/sql_references/data_types/datatypealiases.htm
         TYPE_MAPPING = {
             **generator.Generator.TYPE_MAPPING,
             **STRING_TYPE_MAPPING,
-            # https://docs.exasol.com/db/latest/sql_references/data_types/datatypealiases.htm
             exp.DataType.Type.TINYINT: "SMALLINT",
             exp.DataType.Type.MEDIUMINT: "INT",
             exp.DataType.Type.DECIMAL32: "DECIMAL",
@@ -29,16 +29,11 @@ class Exasol(Dialect):
             exp.DataType.Type.DECIMAL128: "DECIMAL",
             exp.DataType.Type.DECIMAL256: "DECIMAL",
             exp.DataType.Type.DATETIME: "TIMESTAMP",
-            exp.DataType.Type.TIMESTAMPLTZ: "TIMESTAMP WITH LOCAL TIME ZONE",
         }
 
         def datatype_sql(self, expression: exp.DataType) -> str:
-            """
-            Override datatype generation to align with Exasol's support for temporal types.
-
-            Exasol supports a fixed default precision for TIMESTAMP WITH LOCAL TIME ZONE and does not allow specifying a custom precision.
-            This method strips any precision argument from TIMESTAMPLTZ types to ensure compatibility with Exasol's syntax.
-            """
+            # Exasol supports a fixed default precision of 3 for TIMESTAMP WITH LOCAL TIME ZONE
+            # and does not allow specifying a different custom precision
             if expression.is_type(exp.DataType.Type.TIMESTAMPLTZ):
                 return "TIMESTAMP WITH LOCAL TIME ZONE"
 

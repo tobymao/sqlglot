0e43035a601c0ff1c984d533155e8f79a5fdabac
nnamdi16 <nwabuokeinnamdi@gmail.com>
2025-06-27 13:01:29 +0200

    feat(exasol): added BIT_LSHIFT built in function to exasol dialect

--- tests/dialects/test_exasol.py
+++ tests/dialects/test_exasol.py
@@ -137,3 +137,19 @@ class TestExasol(Validator):
                 "spark": "SELECT ~x",
             },
         )
+        self.validate_all(
+            "SELECT BIT_LSHIFT(x, 1)",
+            read={
+                "exasol": "SELECT BIT_LSHIFT(x, 1)",
+                "spark": "SELECT SHIFTLEFT(x, 1)",
+                "duckdb": "SELECT x << 1",
+                "hive": "SELECT x << 1",
+            },
+            write={
+                "exasol": "SELECT BIT_LSHIFT(x, 1)",
+                "duckdb": "SELECT x << 1",
+                "presto": "SELECT BITWISE_ARITHMETIC_SHIFT_LEFT(x, 1)",
+                "hive": "SELECT x << 1",
+                "spark": "SELECT SHIFTLEFT(x, 1)",
+            },
+        )

229722a50d36dcfd57dcb5d46ac791b8d4acf0f7
Giorgos Michas <geomichas96@gmail.com>
2025-07-02 13:37:12 +0300

    fix(optimizer): avoid qualifying CTE (#5327)
    * fix(optimizer): avoid qualifying CTE
    
    * PR feedback1 (george)

--- sqlglot/optimizer/qualify_tables.py
+++ sqlglot/optimizer/qualify_tables.py
@@ -60,8 +60,11 @@ def qualify_tables(
                 table.set("catalog", catalog.copy())
 
     if (db or catalog) and not isinstance(expression, exp.Query):
+        with_ = expression.args.get("with") or exp.With()
+        cte_names = {cte.alias_or_name for cte in with_.expressions}
+
         for node in expression.walk(prune=lambda n: isinstance(n, exp.Query)):
-            if isinstance(node, exp.Table):
+            if isinstance(node, exp.Table) and node.name not in cte_names:
                 _qualify(node)
 
     for scope in traverse_scope(expression):

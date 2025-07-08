1cdc524aebd61762dd3a3db296381f526f7caa9e
Mateusz Poleski <62909233+mateuszpoleski@users.noreply.github.com>
2025-07-09 01:59:10 +0200

    feat(dremio): Add TIME_MAPPING for Dremio dialect (#5378)
    * feat(dremio): Add TIME_MAPPING for Dremio dialect
    
    * Fix linter checks
    
    * Address comments
    
    ---------
    
    Co-authored-by: Mateusz Poleski <Mateusz.Poleski@imc.com>

--- tests/dialects/test_dremio.py
+++ tests/dialects/test_dremio.py
@@ -94,3 +94,38 @@ class TestDremio(Validator):
             "SELECT COUNT(DISTINCT a, b) FROM t",
             "SELECT COUNT(DISTINCT CASE WHEN a IS NULL THEN NULL WHEN b IS NULL THEN NULL ELSE (a, b) END) FROM t",
         )
+
+    def test_time_mapping(self):
+        ts = "CAST('2025-06-24 12:34:56' AS TIMESTAMP)"
+
+        self.validate_all(
+            f"SELECT TO_CHAR({ts}, 'YYYY-MM-DD HH24:MI:SS')",
+            read={
+                "dremio": f"SELECT TO_CHAR({ts}, 'YYYY-MM-DD HH24:MI:SS')",
+                "postgres": f"SELECT TO_CHAR({ts}, 'YYYY-MM-DD HH24:MI:SS')",
+                "oracle": f"SELECT TO_CHAR({ts}, 'YYYY-MM-DD HH24:MI:SS')",
+                "duckdb": f"SELECT STRFTIME({ts}, '%Y-%m-%d %H:%M:%S')",
+            },
+            write={
+                "dremio": f"SELECT TO_CHAR({ts}, 'YYYY-MM-DD HH24:MI:SS')",
+                "postgres": f"SELECT TO_CHAR({ts}, 'YYYY-MM-DD HH24:MI:SS')",
+                "oracle": f"SELECT TO_CHAR({ts}, 'YYYY-MM-DD HH24:MI:SS')",
+                "duckdb": f"SELECT STRFTIME({ts}, '%Y-%m-%d %H:%M:%S')",
+            },
+        )
+
+        self.validate_all(
+            f"SELECT TO_CHAR({ts}, 'YY-DDD HH24:MI:SS.FFF TZD')",
+            read={
+                "dremio": f"SELECT TO_CHAR({ts}, 'YY-DDD HH24:MI:SS.FFF TZD')",
+                "postgres": f"SELECT TO_CHAR({ts}, 'YY-DDD HH24:MI:SS.US TZ')",
+                "oracle": f"SELECT TO_CHAR({ts}, 'YY-DDD HH24:MI:SS.FF6 %Z')",
+                "duckdb": f"SELECT STRFTIME({ts}, '%y-%j %H:%M:%S.%f %Z')",
+            },
+            write={
+                "dremio": f"SELECT TO_CHAR({ts}, 'YY-DDD HH24:MI:SS.FFF TZD')",
+                "postgres": f"SELECT TO_CHAR({ts}, 'YY-DDD HH24:MI:SS.US TZ')",
+                "oracle": f"SELECT TO_CHAR({ts}, 'YY-DDD HH24:MI:SS.FF6 %Z')",
+                "duckdb": f"SELECT STRFTIME({ts}, '%y-%j %H:%M:%S.%f %Z')",
+            },
+        )

11ed70ffb36929c82eeb015f9731590fe77fd794
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2025-07-24 16:33:28 +0300

    Deleted unnecessary test

--- tests/dialects/test_singlestore.py
+++ tests/dialects/test_singlestore.py
@@ -7,6 +7,3 @@ class TestSingleStore(Validator):
     def test_basic(self):
         self.validate_identity("SELECT 1")
         self.validate_identity("SELECT * FROM users ORDER BY ALL")
-        self.validate_identity(
-            "WITH data AS (SELECT 1 AS id, 2 AS my_id) SELECT id AS my_id FROM data WHERE my_id = 1 GROUP BY my_id HAVING my_id = 1"
-        )

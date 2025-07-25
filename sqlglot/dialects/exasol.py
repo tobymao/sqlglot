3881e418faeca4f9b613b4bd790e76a271cc14b3
nnamdi16 <nwabuokeinnamdi@gmail.com>
2025-07-25 14:07:56 +0200

    fix(exasol): added reference to HASHTYPE_MD5

--- sqlglot/dialects/exasol.py
+++ sqlglot/dialects/exasol.py
@@ -180,6 +180,7 @@ class Exasol(Dialect):
             # https://docs.exasol.com/db/latest/sql_references/functions/alphabeticallistfunctions/hash_sha%5B1%5D.htm#HASH_SHA%5B1%5D
             exp.SHA: rename_func("HASH_SHA"),
             exp.MD5: rename_func("HASH_MD5"),
+            # https://docs.exasol.com/db/latest/sql_references/functions/alphabeticallistfunctions/hashtype_md5.htm
             exp.MD5Digest: rename_func("HASHTYPE_MD5"),
             # https://docs.exasol.com/db/latest/sql/create_view.htm
             exp.CommentColumnConstraint: lambda self, e: f"COMMENT IS {self.sql(e, 'this')}",

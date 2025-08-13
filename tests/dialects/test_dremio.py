e675382d7380cf686e5887eeb1c386e6637da455
Jason Thomas <jasonthomassql@gmail.com>
2025-08-13 11:46:40 -0400

    remove unneeded comments

--- tests/dialects/test_dremio.py
+++ tests/dialects/test_dremio.py
@@ -98,7 +98,6 @@ class TestDremio(Validator):
     def test_time_mapping(self):
         ts = "CAST('2025-06-24 12:34:56' AS TIMESTAMP)"
 
-        # Use lowercase in test input to match Dremio behavior
         self.validate_all(
             f"SELECT TO_CHAR({ts}, 'yyyy-mm-dd hh24:mi:ss')",
             read={
@@ -116,7 +115,7 @@ class TestDremio(Validator):
         )
 
         self.validate_all(
-            f"SELECT TO_CHAR({ts}, 'yy-ddd hh24:mi:ss.fff tzd')",  # lowercase to match Dremio
+            f"SELECT TO_CHAR({ts}, 'yy-ddd hh24:mi:ss.fff tzd')",
             read={
                 "dremio": f"SELECT TO_CHAR({ts}, 'yy-ddd hh24:mi:ss.fff tzd')",
                 "postgres": f"SELECT TO_CHAR({ts}, 'YY-DDD HH24:MI:SS.US TZ')",

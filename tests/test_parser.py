1c7a11e595d96be5ee03e7cac804d93fd9760b0a
Jo <46752250+georgesittas@users.noreply.github.com>
2025-08-20 15:18:36 +0300

    Feat(parser)!: raise if query modifier is specified multiple times (#5608)
    * Feat(parser)!: raise if query modifier is specified multiple times
    
    * PR feedback

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1016,3 +1016,16 @@ class TestParser(unittest.TestCase):
             parse_one("select * from tbl pivot(col1 for col2 in (val1, val1))")
 
         self.assertIn("Expecting an aggregation function in PIVOT", str(ctx.exception))
+
+    def test_multiple_query_modifiers(self):
+        sql = "SELECT * FROM a WHERE b = 'true' AND c > 50 WHERE c = 'false'"
+
+        with self.assertRaises(ParseError) as ctx:
+            parse_one(sql)
+
+        self.assertIn("Found multiple 'WHERE' clauses. Line 1, Col: 49.", str(ctx.exception))
+
+        self.assertEqual(
+            parse_one(sql, error_level=ErrorLevel.IGNORE).sql(),
+            "SELECT * FROM a WHERE c = 'false'",
+        )

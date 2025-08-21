de70439958c821845c8f51b7e7f74dd6b7709d3a
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2025-08-21 12:17:19 +0300

    feat(singlestore): Added parsing of WEEKDAY function

--- sqlglot/dialects/singlestore.py
+++ sqlglot/dialects/singlestore.py
@@ -89,6 +89,14 @@ class SingleStore(MySQL):
                 ),
                 DataType.Type.INT,
             ),
+            "WEEKDAY": lambda args: exp.Mod(
+                this=exp.Paren(
+                    this=exp.Add(
+                        this=exp.DayOfWeek(this=seq_get(args, 0)), expression=exp.Literal.number(5)
+                    )
+                ),
+                expression=exp.Literal.number(7),
+            ),
             "UNIX_TIMESTAMP": exp.StrToUnix.from_arg_list,
             "FROM_UNIXTIME": build_formatted_time(exp.UnixToTime, "mysql"),
             "BSON_EXTRACT_BSON": build_json_extract_path(exp.JSONBExtract),

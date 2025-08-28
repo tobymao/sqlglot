5bb1d7658d05dfdb5aaf61d1d2f1f177d8a4a53f
Giorgos Michas <geomichas96@gmail.com>
2025-08-28 14:05:46 +0300

    feat(optimizer)!: annotate type for bq CONTAINS_SUBSTR (#5705)
    * feat(optimizer)!: annotate type for bq CONTAINS_SUBSTR
    
    * fix annotation

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -5942,8 +5942,9 @@ class ConcatWs(Concat):
     _sql_names = ["CONCAT_WS"]
 
 
+# https://cloud.google.com/bigquery/docs/reference/standard-sql/string_functions#contains_substr
 class Contains(Func):
-    arg_types = {"this": True, "expression": True}
+    arg_types = {"this": True, "expression": True, "json_scope": False}
 
 
 # https://docs.oracle.com/cd/B13789_01/server.101/b10759/operators004.htm#i1035022

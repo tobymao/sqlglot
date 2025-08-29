35dc720e45e1e519a26753531030a1c62959f8a5
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2025-08-29 13:52:40 +0300

    feat(singlestore): Implemented generation of exp.DiToDate (#5721)

--- sqlglot/dialects/singlestore.py
+++ sqlglot/dialects/singlestore.py
@@ -277,6 +277,8 @@ class SingleStore(MySQL):
             exp.DateBin: unsupported_args("unit", "zone")(
                 lambda self, e: self.func("TIME_BUCKET", e.this, e.expression, e.args.get("origin"))
             ),
+            exp.DiToDate: lambda self,
+            e: f"STR_TO_DATE({self.sql(e, 'this')}, {SingleStore.DATEINT_FORMAT})",
             exp.DateToDi: lambda self,
             e: f"(DATE_FORMAT({self.sql(e, 'this')}, {SingleStore.DATEINT_FORMAT}) :> INT)",
             exp.TsOrDiToDi: lambda self,

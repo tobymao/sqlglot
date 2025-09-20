f07d35d29104c6203efaab738118d1903614b83c
Brad <104863076+fivetran-BradfordPaskewitz@users.noreply.github.com>
2025-09-20 02:37:33 -0700

    feat(optimizer)!: annotate type for Snowflake CHR function (#5929)
    Co-authored-by: Jo <46752250+georgesittas@users.noreply.github.com>

--- sqlglot/dialects/snowflake.py
+++ sqlglot/dialects/snowflake.py
@@ -540,6 +540,7 @@ class Snowflake(Dialect):
             exp.AIAgg,
             exp.AIClassify,
             exp.AISummarizeAgg,
+            exp.Chr,
             exp.Collate,
             exp.RegexpExtract,
             exp.RegexpReplace,
--- tests/dialects/test_snowflake.py
+++ tests/dialects/test_snowflake.py
@@ -34,6 +34,7 @@ class TestSnowflake(Validator):
             "SELECT BASE64_DECODE_BINARY('SGVsbG8=', 'ABCDEFGHwxyz0123456789+/')"
         )
 
+        self.validate_identity("SELECT CHR(8364)")
         self.validate_identity("SELECT {*} FROM my_table")
         self.validate_identity("SELECT {my_table.*} FROM my_table")
         self.validate_identity("SELECT {* ILIKE 'col1%'} FROM my_table")

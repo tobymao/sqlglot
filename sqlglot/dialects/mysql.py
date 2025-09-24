835b51869cb072de2a9f3b2356c21c97976a7d92
Giorgos Michas <geomichas96@gmail.com>
2025-09-24 15:37:48 +0300

    feat(mysql): add valid INTERVAL units (#5951)
    * feat(mysql): add valid INTERVAL units
    
    * fix test typo
    
    * add extender
    
    * add SECOND_MICROSECOND

--- sqlglot/dialects/mysql.py
+++ sqlglot/dialects/mysql.py
@@ -179,6 +179,21 @@ class MySQL(Dialect):
         "%W": "%A",
     }
 
+    VALID_INTERVAL_UNITS = {
+        *Dialect.VALID_INTERVAL_UNITS,
+        "SECOND_MICROSECOND",
+        "MINUTE_MICROSECOND",
+        "MINUTE_SECOND",
+        "HOUR_MICROSECOND",
+        "HOUR_SECOND",
+        "HOUR_MINUTE",
+        "DAY_MICROSECOND",
+        "DAY_SECOND",
+        "DAY_MINUTE",
+        "DAY_HOUR",
+        "YEAR_MONTH",
+    }
+
     class Tokenizer(tokens.Tokenizer):
         QUOTES = ["'", '"']
         COMMENTS = ["--", "#", ("/*", "*/")]

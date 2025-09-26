3d974ac66edddadc90d3d3c8b1a03d60c10b1eb1
Brad Paskewitz <bradford.paskewitz@fivetran.com>
2025-09-25 18:29:38 -0700

    Revert "feat(optimizer)!: annotate type for Snowflake REGEXP_INSTR function"
    This reverts commit 7a523f5d0a205fd1a45fa2e1a35f1168d4d60f6d.

--- sqlglot/dialects/snowflake.py
+++ sqlglot/dialects/snowflake.py
@@ -576,7 +576,6 @@ class Snowflake(Dialect):
             *Dialect.TYPE_TO_EXPRESSIONS[exp.DataType.Type.BIGINT],
             exp.MD5NumberLower64,
             exp.MD5NumberUpper64,
-            exp.RegexpInstr,
         },
         exp.DataType.Type.ARRAY: {
             exp.Split,

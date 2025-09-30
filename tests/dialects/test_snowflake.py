f992cda35625d7f43ded319bc2aafc9e8d53e841
fivetran-amrutaayachit <156145676+fivetran-amrutabhimsenayachit@users.noreply.github.com>
2025-09-30 13:08:21 -0400

    feat(optimizer): Annotate type for snowflake TRANSLATE function (#5992)

--- tests/dialects/test_snowflake.py
+++ tests/dialects/test_snowflake.py
@@ -47,6 +47,7 @@ class TestSnowflake(Validator):
         self.validate_identity("SELECT SOUNDEX(column_name)")
         self.validate_identity("SELECT SOUNDEX_P123(column_name)")
         self.validate_identity("SELECT JAROWINKLER_SIMILARITY('hello', 'world')")
+        self.validate_identity("SELECT TRANSLATE(column_name, 'abc', '123')")
         self.validate_identity("SELECT SPLIT_PART('11.22.33', '.', 1)")
         self.validate_identity("PARSE_URL('https://example.com/path')")
         self.validate_identity("PARSE_URL('https://example.com/path', 1)")

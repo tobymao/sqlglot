f99503650255364bbbed9d1a49f962f2b4314a8a
fivetran-amrutaayachit <156145676+fivetran-amrutabhimsenayachit@users.noreply.github.com>
2025-10-08 05:34:21 -0400

    feat(optimizer): Annotate type for snowflake MOD function (#6031)
    * feat(optimizer): annotate type for snowflake MOD function
    
    * fix: Removed tests from previous PR
    
    ---------
    
    Co-authored-by: Jo <46752250+georgesittas@users.noreply.github.com>

--- tests/fixtures/optimizer/annotate_functions.sql
+++ tests/fixtures/optimizer/annotate_functions.sql
@@ -324,18 +324,6 @@ CEIL(tbl.bignum_col);
 BIGDECIMAL;
 
 # dialect: bigquery
-FLOOR(1);
-DOUBLE;
-
-# dialect: bigquery
-FLOOR(5.5);
-DOUBLE;
-
-# dialect: bigquery
-FLOOR(tbl.bignum_col);
-BIGDECIMAL;
-
-# dialect: bigquery
 SQRT(1);
 DOUBLE;
 
@@ -1906,6 +1894,18 @@ VARCHAR;
 # dialect: snowflake
 LTRIM(NULL);
 VARCHAR;
+
+# dialect: snowflake
+MOD(tbl.bigint_col, 3);
+BIGINT;
+
+# dialect: snowflake
+MOD(tbl.double_col, 2.5);
+DOUBLE;
+
+# dialect: snowflake
+MOD(42, 7);
+INT;
 
 # dialect: snowflake
 'foo' REGEXP 'bar';

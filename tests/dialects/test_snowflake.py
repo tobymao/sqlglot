5d7a338a69d602ebb7463aa52428feae7b42b911
Jo <46752250+georgesittas@users.noreply.github.com>
2025-11-04 23:03:23 +0200

    Update tests/dialects/test_snowflake.py

--- tests/dialects/test_snowflake.py
+++ tests/dialects/test_snowflake.py
@@ -99,8 +99,7 @@ class TestSnowflake(Validator):
         self.validate_identity("SELECT RANDOM()")
         self.validate_identity("SELECT RANDOM(123)")
         self.validate_identity(
-            "SELECT GROUPING_ID(a, b) AS g_id FROM x GROUP BY ROLLUP(a, b)",
-            "SELECT GROUPING_ID(a, b) AS g_id FROM x GROUP BY ROLLUP (a, b)",
+            "SELECT GROUPING_ID(a, b) AS g_id FROM x GROUP BY ROLLUP (a, b)"
         )
         self.validate_identity("PARSE_URL('https://example.com/path')")
         self.validate_identity("PARSE_URL('https://example.com/path', 1)")

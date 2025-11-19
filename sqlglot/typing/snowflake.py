3aafca74546b932cea93ed830c021f347ae03ded
michaellee <133718882+fivetran-MichaelLee@users.noreply.github.com>
2025-11-19 11:46:31 -0500

    feat(optimizer)!: annotate type for MINHASH_COMBINE (#6362)
    Co-authored-by: Michael Lee <michael.lee@michael.lee-FMF6J19R7N>

--- sqlglot/typing/snowflake.py
+++ sqlglot/typing/snowflake.py
@@ -289,6 +289,7 @@ EXPRESSION_METADATA = {
         expr_type: {"returns": exp.DataType.Type.VARIANT}
         for expr_type in {
             exp.Minhash,
+            exp.MinhashCombine,
         }
     },
     exp.ArgMax: {"annotator": _annotate_arg_max_min},

485a921052458a00019c09475d2e317416830fea
fivetran-amrutaayachit <156145676+fivetran-amrutabhimsenayachit@users.noreply.github.com>
2025-12-02 13:19:34 -0500

    feat(duckdb)!: Add support for format elements used in date/time functions like FORMAT_DATETIME (#6428)
    * feat(duckdb): Add support for format elements used in date/time functions like FORMAT_DATETIME
    
    * feat(duckdb): Add inverse time mapping logic to preserve roundtrip
    
    * fix(duckdb): Added comments, refactored code

--- tests/dialects/test_presto.py
+++ tests/dialects/test_presto.py
@@ -293,7 +293,7 @@ class TestPresto(Validator):
         self.validate_all(
             "DATE_FORMAT(x, '%Y-%m-%d %H:%i:%S')",
             write={
-                "bigquery": "FORMAT_DATE('%Y-%m-%d %H:%M:%S', x)",
+                "bigquery": "FORMAT_DATE('%F %T', x)",
                 "duckdb": "STRFTIME(x, '%Y-%m-%d %H:%M:%S')",
                 "presto": "DATE_FORMAT(x, '%Y-%m-%d %T')",
                 "hive": "DATE_FORMAT(x, 'yyyy-MM-dd HH:mm:ss')",

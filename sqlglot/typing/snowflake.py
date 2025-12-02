360e022e13ebc7a89371bf269bd9f0b49bb7bf3a
fivetran-kwoodbeck <kris.woodbeck@fivetran.com>
2025-12-02 01:18:50 -0500

    merged

--- sqlglot/typing/snowflake.py
+++ sqlglot/typing/snowflake.py
@@ -181,7 +181,9 @@ EXPRESSION_METADATA = {
         expr_type: {"returns": exp.DataType.Type.ARRAY}
         for expr_type in (
             exp.ApproxTopK,
+            exp.ApproxTopKEstimate,
             exp.ArrayAgg,
+            exp.ArrayUnionAgg,
             exp.RegexpExtractAll,
             exp.Split,
             exp.StringToArray,
@@ -259,8 +261,10 @@ EXPRESSION_METADATA = {
             exp.Cbrt,
             exp.Cosh,
             exp.MonthsBetween,
+            exp.Normal,
             exp.RegrAvgx,
             exp.RegrAvgy,
+            exp.RegrSlope,
             exp.RegrValx,
             exp.RegrValy,
             exp.Sinh,
@@ -346,6 +350,8 @@ EXPRESSION_METADATA = {
             exp.AISummarizeAgg,
             exp.Base64DecodeString,
             exp.Base64Encode,
+            exp.CheckJson,
+            exp.CheckXml,
             exp.Chr,
             exp.Collate,
             exp.Collation,
@@ -394,6 +400,11 @@ EXPRESSION_METADATA = {
     exp.DecodeCase: {"annotator": _annotate_decode_case},
     exp.GreatestIgnoreNulls: {
         "annotator": lambda self, e: self._annotate_by_args(e, "expressions")
+    },
+    exp.HashAgg: {
+        "annotator": lambda self, e: self._set_type(
+            e, exp.DataType.build("NUMBER(19, 0)", dialect="snowflake")
+        )
     },
     exp.LeastIgnoreNulls: {"annotator": lambda self, e: self._annotate_by_args(e, "expressions")},
     exp.Median: {"annotator": _annotate_median},

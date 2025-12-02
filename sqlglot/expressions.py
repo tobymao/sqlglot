e6b8ea37552ce69d3f475df28d69493ee1b07d93
Asha Shankar <asha.shankar@fivetran.com>
2025-12-02 10:46:27 -0800

    feat(snowflake)!: annotation support for APPROX_PERCENTILE_ACCUMULATE . Return type OBJECT

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -7480,6 +7480,11 @@ class ApproxQuantile(Quantile):
     }
 
 
+# https://docs.snowflake.com/en/sql-reference/functions/approx_percentile_accumulate
+class ApproxPercentileAccumulate(AggFunc):
+    arg_types = {"this": True}
+
+
 class Quarter(Func):
     pass
 

151dac367d102c8cb5359088f16229dd35ef7059
fivetran-kwoodbeck <kris.woodbeck@fivetran.com>
2025-12-03 03:14:18 -0500

    feat(optimizer)!: Annotate type for ZIPF (#6453)

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -8116,6 +8116,10 @@ class Year(Func):
     pass
 
 
+class Zipf(Func):
+    arg_types = {"this": True, "elementcount": True, "gen": True}
+
+
 class Use(Expression):
     arg_types = {"this": False, "expressions": False, "kind": False}
 

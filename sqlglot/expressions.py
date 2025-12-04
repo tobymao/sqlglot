e89139714aefd8a6481a90d9753c81910c9f88e9
Vincent Chan <vchan@users.noreply.github.com>
2025-12-04 12:37:50 -0800

    Feat(BigQuery): Add support for the NET.HOST function (#6480)
    * Feat(BigQuery)!: Add support for the NET.HOST function
    
    * PR feedback

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -7439,6 +7439,11 @@ class Normal(Func):
     arg_types = {"this": True, "stddev": True, "gen": True}
 
 
+# https://cloud.google.com/bigquery/docs/reference/standard-sql/net_functions#nethost
+class NetHost(Func):
+    _sql_names = ["NET.HOST"]
+
+
 class Overlay(Func):
     arg_types = {"this": True, "expression": True, "from_": True, "for_": False}
 

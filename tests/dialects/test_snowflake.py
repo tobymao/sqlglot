15ef3483bb2a87e5a3b78552859668336cc1a26a
fivetran-ashashankar <asha.shankar@fivetran.com>
2025-12-10 02:57:44 -0800

    feat(snowflake)!: annotation support for CURRENT_AVAILABLE_ROLES (#6514)

--- tests/dialects/test_snowflake.py
+++ tests/dialects/test_snowflake.py
@@ -259,6 +259,7 @@ class TestSnowflake(Validator):
         self.validate_identity("SELECT OBJECT_CONSTRUCT()")
         self.validate_identity("SELECT CURRENT_ACCOUNT()")
         self.validate_identity("SELECT CURRENT_ACCOUNT_NAME()")
+        self.validate_identity("SELECT CURRENT_AVAILABLE_ROLES()")
         self.validate_identity("SELECT CURRENT_ORGANIZATION_USER()")
         self.validate_identity("SELECT CURRENT_REGION()")
         self.validate_identity("SELECT CURRENT_ROLE()")

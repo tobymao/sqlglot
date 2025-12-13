36b36ebefa44125e8ffd8f2687c1d9f1c78e6f1f
Daniel Hawkins <dhawkins1234@gmail.com>
2025-12-12 17:37:38 -0800

    fix(postgres): corrected handling of ToChar for Postgres. Unlike other dialects, it requires 2 arguments, and so falling back to the original function will produce invalid SQL.

--- tests/dialects/test_exasol.py
+++ tests/dialects/test_exasol.py
@@ -336,10 +336,8 @@ class TestExasol(Validator):
                 "SELECT TO_CHAR(CAST('1999-12-31' AS DATE)) AS TO_CHAR",
                 write={
                     "exasol": "SELECT TO_CHAR(CAST('1999-12-31' AS DATE)) AS TO_CHAR",
-                    "redshift": "SELECT TO_CHAR(CAST('1999-12-31' AS DATE)) AS TO_CHAR",
                     "presto": "SELECT DATE_FORMAT(CAST('1999-12-31' AS DATE)) AS TO_CHAR",
                     "oracle": "SELECT TO_CHAR(CAST('1999-12-31' AS DATE)) AS TO_CHAR",
-                    "postgres": "SELECT TO_CHAR(CAST('1999-12-31' AS DATE)) AS TO_CHAR",
                 },
                 read={
                     "exasol": "SELECT TO_CHAR(DATE '1999-12-31') AS TO_CHAR",

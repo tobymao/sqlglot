2bc2506e0e0b26e82661a08217855d693f30dc25
Brad <104863076+fivetran-BradfordPaskewitz@users.noreply.github.com>
2025-12-16 02:40:05 -0800

    feat(bigquery)!: support SAFE.TIMESTAMP annotation (#6550)
    * feat(bigquery)!: support SAFE.TIMESTAMP annotation
    
    * Couple nits
    
    * Change tests to 2 arg
    
    ---------
    
    Co-authored-by: vaggelisd <daniasevangelos@gmail.com>

--- sqlglot/expressions.py
+++ sqlglot/expressions.py
@@ -6628,7 +6628,7 @@ class Exists(Func, SubqueryPredicate):
 
 
 class Timestamp(Func):
-    arg_types = {"this": False, "zone": False, "with_tz": False}
+    arg_types = {"this": False, "zone": False, "with_tz": False, "safe": False}
 
 
 class TimestampAdd(Func, TimeUnit):

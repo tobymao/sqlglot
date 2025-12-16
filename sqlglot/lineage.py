f2311806d14c106adde33ab236c4df992ea104f7
Vaggelis Danias <daniasevangelos@gmail.com>
2025-12-16 14:24:14 +0200

    fix(lineage): Fix GraphHTML edge 'from' key (#6571)

--- sqlglot/lineage.py
+++ sqlglot/lineage.py
@@ -61,7 +61,7 @@ class Node:
             }
 
             for d in node.downstream:
-                edges.append({"from_": node_id, "to": id(d)})
+                edges.append({"from": node_id, "to": id(d)})
         return GraphHTML(nodes, edges, **opts)
 
 

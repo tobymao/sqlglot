7e551608a3fddb94fb8f3ad067ef00d89127c6d0
vaggelisd <daniasevangelos@gmail.com>
2025-12-18 12:31:56 +0200

    Chore: Follow up of 6551

--- tests/dialects/test_oracle.py
+++ tests/dialects/test_oracle.py
@@ -236,6 +236,7 @@ class TestOracle(Validator):
             "SELECT TO_CHAR(TIMESTAMP '1999-12-01 10:00:00')",
             write={
                 "oracle": "SELECT TO_CHAR(CAST('1999-12-01 10:00:00' AS TIMESTAMP))",
+                "postgres": "SELECT CAST(CAST('1999-12-01 10:00:00' AS TIMESTAMP) AS TEXT)",
             },
         )
         self.validate_all(

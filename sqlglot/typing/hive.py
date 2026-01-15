af008bd51482c69b2c0c9ef01008ec0e657d6c9b
Abhishek Bhosale <108826846+AbhishekASLK@users.noreply.github.com>
2026-01-16 00:55:59 +0530

    feat(optimizer)!: Annotate SIN, COS, TAN for Hive and inherited dialects (#6759)

--- sqlglot/typing/hive.py
+++ sqlglot/typing/hive.py
@@ -9,8 +9,11 @@ EXPRESSION_METADATA = {
         expr_type: {"returns": exp.DataType.Type.DOUBLE}
         for expr_type in {
             exp.Acos,
+            exp.Cos,
             exp.Cosh,
+            exp.Sin,
             exp.Sinh,
+            exp.Tan,
             exp.Tanh,
         }
     },

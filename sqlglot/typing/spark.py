8c3e3fdb09f79901371c478104b899843491f24c
Abhishek Bhosale <108826846+AbhishekASLK@users.noreply.github.com>
2026-01-16 14:29:11 +0530

    feat(optimizer)!: Annotate `SEC` for Spark and DBX (#6768)
    * feat(optimizer)!: Annotate SEC for Spark and DBX
    
    * cleanup
    
    ---------
    
    Co-authored-by: Jo <46752250+georgesittas@users.noreply.github.com>

--- sqlglot/typing/spark.py
+++ sqlglot/typing/spark.py
@@ -5,8 +5,14 @@ from sqlglot.typing.spark2 import EXPRESSION_METADATA
 
 EXPRESSION_METADATA = {
     **EXPRESSION_METADATA,
+    **{
+        exp_type: {"returns": exp.DataType.Type.DOUBLE}
+        for exp_type in {
+            exp.Acosh,
+            exp.Sec,
+        }
+    },
     exp.CurrentTimezone: {"returns": exp.DataType.Type.VARCHAR},
     exp.Localtimestamp: {"returns": exp.DataType.Type.TIMESTAMPNTZ},
     exp.ToBinary: {"returns": exp.DataType.Type.BINARY},
-    exp.Acosh: {"returns": exp.DataType.Type.DOUBLE},
 }

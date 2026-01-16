9d8083bbf1411d698f9939c7a99010de20227d8c
Abhishek Bhosale <bhoslea925@gmail.com>
2026-01-16 09:00:28 +0530

    cleanup

--- sqlglot/typing/spark.py
+++ sqlglot/typing/spark.py
@@ -15,5 +15,4 @@ EXPRESSION_METADATA = {
     exp.CurrentTimezone: {"returns": exp.DataType.Type.VARCHAR},
     exp.Localtimestamp: {"returns": exp.DataType.Type.TIMESTAMPNTZ},
     exp.ToBinary: {"returns": exp.DataType.Type.BINARY},
-    exp.Acosh: {"returns": exp.DataType.Type.DOUBLE},
 }

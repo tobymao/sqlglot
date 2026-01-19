0a14b924a5ca15cc690a9974cd881f34ce001c8c
Abhishek Bhosale <bhoslea925@gmail.com>
2026-01-19 21:43:31 +0530

    feat(optimizer)!: Annotate UNHEX for Hive, Spark and DBX

--- sqlglot/typing/hive.py
+++ sqlglot/typing/hive.py
@@ -6,6 +6,13 @@ from sqlglot.typing import EXPRESSION_METADATA
 EXPRESSION_METADATA = {
     **EXPRESSION_METADATA,
     **{
+        expr_type: {"returns": exp.DataType.Type.BINARY}
+        for expr_type in {
+            exp.Encode,
+            exp.Unhex,
+        }
+    },
+    **{
         expr_type: {"returns": exp.DataType.Type.DOUBLE}
         for expr_type in {
             exp.Acos,
@@ -33,7 +40,6 @@ EXPRESSION_METADATA = {
     exp.Coalesce: {
         "annotator": lambda self, e: self._annotate_by_args(e, "this", "expressions", promote=True)
     },
-    exp.Encode: {"returns": exp.DataType.Type.BINARY},
     exp.If: {"annotator": lambda self, e: self._annotate_by_args(e, "true", "false", promote=True)},
     exp.StrToUnix: {"returns": exp.DataType.Type.BIGINT},
     exp.Month: {"returns": exp.DataType.Type.INT},

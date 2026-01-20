d5fcf1ffc845d39f21e70ba96671a9ee4239adf1
Jo <46752250+georgesittas@users.noreply.github.com>
2026-01-20 15:30:35 +0200

    Use `validate_identity` in tests

--- tests/dialects/test_starrocks.py
+++ tests/dialects/test_starrocks.py
@@ -181,21 +181,12 @@ class TestStarrocks(Validator):
                 "mysql": "SELECT * FROM t WHERE a BETWEEN 1 AND 5",
             },
         )
-        self.validate_all(
-            "SELECT a BETWEEN 1 AND 5 FROM t",
-            write={
-                "starrocks": "SELECT a BETWEEN 1 AND 5 FROM t",
-            },
-        )
-        self.validate_all(
+        self.validate_identity("SELECT a BETWEEN 1 AND 5 FROM t")
+        self.validate_identity(
             "DELETE FROM t WHERE a BETWEEN b AND c",
-            write={
-                "starrocks": "DELETE FROM t WHERE a >= b AND a <= c",
-            },
+            "DELETE FROM t WHERE a >= b AND a <= c",
         )
-        self.validate_all(
+        self.validate_identity(
             "DELETE FROM t WHERE a BETWEEN 1 AND 10 AND b BETWEEN 20 AND 30 OR c BETWEEN 'x' AND 'z'",
-            write={
-                "starrocks": "DELETE FROM t WHERE a >= 1 AND a <= 10 AND b >= 20 AND b <= 30 OR c >= 'x' AND c <= 'z'",
-            },
+            "DELETE FROM t WHERE a >= 1 AND a <= 10 AND b >= 20 AND b <= 30 OR c >= 'x' AND c <= 'z'",
         )

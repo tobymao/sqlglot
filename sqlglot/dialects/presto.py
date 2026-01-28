ddec2500e994dc4b7cc4a80b501dc9bc1f2fb4d1
Nick Crews <nicholas.b.crews@gmail.com>
2026-01-28 04:39:52 -0900

    fix(presto): don't overwrite_types when annotating types in struct_sql() (#6870)
    * fix(presto): don't overwrite_types when annotating types in struct_sql()
    
    * Refactor
    
    ---------
    
    Co-authored-by: Jo <46752250+georgesittas@users.noreply.github.com>

--- sqlglot/dialects/presto.py
+++ sqlglot/dialects/presto.py
@@ -711,7 +711,8 @@ class Presto(Dialect):
         def struct_sql(self, expression: exp.Struct) -> str:
             from sqlglot.optimizer.annotate_types import annotate_types
 
-            expression = annotate_types(expression, dialect=self.dialect)
+            if not expression.type:
+                expression = annotate_types(expression, dialect=self.dialect)
             values: t.List[str] = []
             schema: t.List[str] = []
             unknown_type = False

f39b514936e6188799bf1c392937050d8aef6ac8
Abhishek Bhosale <108826846+AbhishekASLK@users.noreply.github.com>
2026-01-30 21:19:09 +0530

    feat(optimmizer)!: Annotate ASIN(expr) for DuckDB (#6901)
    Co-authored-by: Jo <46752250+georgesittas@users.noreply.github.com>

--- sqlglot/typing/duckdb.py
+++ sqlglot/typing/duckdb.py
@@ -14,6 +14,7 @@ EXPRESSION_METADATA = {
     **{
         expr_type: {"returns": exp.DataType.Type.DOUBLE}
         for expr_type in {
+            exp.Asin,
             exp.Atan,
             exp.Cos,
             exp.Cot,

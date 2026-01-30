fc26fd957d4e1650e7605e8c687b5a1dc181901e
Abhishek Bhosale <bhoslea925@gmail.com>
2026-01-30 21:07:07 +0530

    feat(optimizer)!: Annotate ACOS(expr) for DuckDB

--- tests/fixtures/optimizer/annotate_functions.sql
+++ tests/fixtures/optimizer/annotate_functions.sql
@@ -5619,6 +5619,14 @@ COS(tbl.double_col);
 DOUBLE;
 
 # dialect: duckdb
+ACOS(tbl.int_col);
+DOUBLE;
+
+# dialect: duckdb
+ACOS(tbl.double_col);
+DOUBLE;
+
+# dialect: duckdb
 COT(tbl.int_col);
 DOUBLE;
 

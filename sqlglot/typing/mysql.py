cbfd8f2a766127784feb5c02f1cd405ca3f29402
Abhishek Bhosale <bhoslea925@gmail.com>
2026-01-31 21:05:09 +0530

    feat(optimizer)!: Annotate arc trignometric func for MySQL

--- sqlglot/typing/mysql.py
+++ sqlglot/typing/mysql.py
@@ -5,6 +5,15 @@ from sqlglot.typing import EXPRESSION_METADATA
 
 EXPRESSION_METADATA = {
     **EXPRESSION_METADATA,
+    **{
+        expr_type: {"returns": exp.DataType.Type.DOUBLE}
+        for expr_type in {
+            exp.Acos,
+            exp.Asin,
+            exp.Atan,
+            exp.Atan2,
+        }
+    },
     **{expr_type: {"returns": exp.DataType.Type.VARCHAR} for expr_type in (exp.Elt,)},
     exp.Localtime: {"returns": exp.DataType.Type.DATETIME},
     exp.DayOfWeek: {"returns": exp.DataType.Type.INT},

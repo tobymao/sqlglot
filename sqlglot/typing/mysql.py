e0a63d0851f9df9e3287d862fdf04d0c5cf45386
Abhishek Bhosale <bhoslea925@gmail.com>
2026-01-31 21:53:20 +0530

    feat(optimizer)!: Annotate DEGREES(expr) for MySQL

--- sqlglot/typing/mysql.py
+++ sqlglot/typing/mysql.py
@@ -5,6 +5,12 @@ from sqlglot.typing import EXPRESSION_METADATA
 
 EXPRESSION_METADATA = {
     **EXPRESSION_METADATA,
+    **{
+        expr_type: {"returns": exp.DataType.Type.DOUBLE}
+        for expr_type in {
+            exp.Degrees,
+        }
+    },
     **{expr_type: {"returns": exp.DataType.Type.VARCHAR} for expr_type in (exp.Elt,)},
     exp.Localtime: {"returns": exp.DataType.Type.DATETIME},
     exp.DayOfWeek: {"returns": exp.DataType.Type.INT},

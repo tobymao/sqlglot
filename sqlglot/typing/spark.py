d0bc4a503a1162119059f4debb58d2ab0f448e11
Abhishek Bhosale <bhoslea925@gmail.com>
2026-02-05 22:42:11 +0530

    feat(optimizer)!: Annotate OVERLAY(expr) for Spark/DBX

--- sqlglot/typing/spark.py
+++ sqlglot/typing/spark.py
@@ -3,6 +3,14 @@ from __future__ import annotations
 from sqlglot import exp
 from sqlglot.typing.spark2 import EXPRESSION_METADATA
 
+
+def _annotate_overlay(self, expression):
+    if expression.this.is_type(exp.DataType.Type.BINARY):
+        self._set_type(expression, exp.DataType.Type.BINARY)
+    else:
+        self._set_type(expression, exp.DataType.Type.VARCHAR)
+
+
 EXPRESSION_METADATA = {
     **EXPRESSION_METADATA,
     **{
@@ -25,4 +33,5 @@ EXPRESSION_METADATA = {
     exp.ToBinary: {"returns": exp.DataType.Type.BINARY},
     exp.DateFromUnixDate: {"returns": exp.DataType.Type.DATE},
     exp.ArraySize: {"returns": exp.DataType.Type.INT},
+    exp.Overlay: {"annotator": _annotate_overlay},
 }
--- tests/fixtures/optimizer/annotate_functions.sql
+++ tests/fixtures/optimizer/annotate_functions.sql
@@ -641,6 +641,14 @@ STRING;
 FORMAT_STRING(tbl.str_col, tbl.int_col, tbl.str_col);
 STRING;
 
+# dialect: spark, databricks
+OVERLAY(tbl.str_col PLACING tbl.str_col FROM tbl.int_col);
+STRING;
+
+# dialect: spark, databricks
+OVERLAY(tbl.bin_col PLACING tbl.bin_col FROM tbl.int_col FOR tbl.int_col);
+BINARY;
+
 --------------------------------------
 -- BigQuery
 --------------------------------------

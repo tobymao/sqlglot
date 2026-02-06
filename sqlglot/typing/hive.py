37db91c5cea14488654a2b69aceab13b6c6a98b7
Abhishek Bhosale <108826846+AbhishekASLK@users.noreply.github.com>
2026-02-06 18:27:51 +0530

    feat(optimizer)!: Annotate `REVERSE(expr)` for Hive, Spark and DBX (#6979)
    * feat(optimizer)!: Annotate REVERSE(expr) for Hive, Spark and DBX
    
    * removed promote=True

--- sqlglot/typing/hive.py
+++ sqlglot/typing/hive.py
@@ -49,4 +49,5 @@ EXPRESSION_METADATA = {
         "annotator": lambda self, e: self._annotate_by_args(e, "this", "expressions", promote=True)
     },
     exp.If: {"annotator": lambda self, e: self._annotate_by_args(e, "true", "false", promote=True)},
+    exp.Reverse: {"annotator": lambda self, e: self._annotate_by_args(e, "this")},
 }
--- tests/fixtures/optimizer/annotate_functions.sql
+++ tests/fixtures/optimizer/annotate_functions.sql
@@ -665,6 +665,14 @@ STRING;
 OVERLAY(tbl.bin_col PLACING tbl.bin_col FROM tbl.int_col FOR tbl.int_col);
 BINARY;
 
+# dialect: hive, spark2, spark, databricks
+REVERSE(tbl.str_col);
+STRING;
+
+# dialect: hive, spark2, spark, databricks
+REVERSE(tbl.array_col);
+ARRAY<STRING>;
+
 # dialect: spark2, spark, databricks
 RIGHT(tbl.str_col, tbl.int_col);
 STRING;

fec19e658bb911508a365e4cf3e4e5d30ecd928f
geooo109 <geomichas96@gmail.com>
2026-02-06 10:56:34 +0200

    add more tests

--- tests/dialects/test_redshift.py
+++ tests/dialects/test_redshift.py
@@ -486,7 +486,9 @@ ORDER BY
             },
         )
 
-        self.validate_identity("SELECT 1 AS EXCLUDE")
+        self.validate_identity("SELECT 1 AS exclude")
+        self.validate_identity("SELECT * FROM (SELECT 1 AS exclude) AS t")
+        self.validate_identity("SELECT 1 AS exclude, 2 AS foo");
 
     def test_values(self):
         # Test crazy-sized VALUES clause to UNION ALL conversion to ensure we don't get RecursionError

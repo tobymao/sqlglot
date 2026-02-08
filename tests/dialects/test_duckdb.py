b53fbac0ed32e1a499a049f447cac7b34b43dfe4
Abhishek Bhosale <bhoslea925@gmail.com>
2026-02-08 10:48:05 +0530

    feat(duckdb)!: support today() for duckdb

--- tests/dialects/test_duckdb.py
+++ tests/dialects/test_duckdb.py
@@ -1372,7 +1372,7 @@ class TestDuckDB(Validator):
         self.validate_identity("ROUND(2.256, 1)")
 
         # TODO: This is incorrect AST, DATE_PART creates a STRUCT of values but it's stored in 'year' arg
-        self.validate_identity("SELECT MAKE_DATE(DATE_PART(['year', 'month', 'day'], TODAY()))")
+        self.validate_identity("SELECT MAKE_DATE(DATE_PART(['year', 'month', 'day'], CURRENT_DATE))")
 
         self.validate_identity("SELECT * FROM t PIVOT(SUM(y) FOR foo IN y_enum)")
         self.validate_identity("SELECT 20_000 AS literal")
@@ -1412,6 +1412,7 @@ class TestDuckDB(Validator):
 
         self.validate_identity("SELECT [1, 2, 3][1 + 1:LENGTH([1, 2, 3]) + -1]")
         self.validate_identity("VERSION()")
+        self.validate_identity("SELECT TODAY()", "SELECT CURRENT_DATE")
 
     def test_array_index(self):
         with self.assertLogs(helper_logger) as cm:

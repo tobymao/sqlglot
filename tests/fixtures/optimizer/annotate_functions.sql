d0afa4ec2b3a9b74375a44a102aa2fad11ee377e
Abhishek Bhosale <bhoslea925@gmail.com>
2026-02-09 18:02:43 +0530

    removed testcase for version

--- tests/fixtures/optimizer/annotate_functions.sql
+++ tests/fixtures/optimizer/annotate_functions.sql
@@ -695,10 +695,6 @@ VARCHAR;
 TRANSLATE(tbl.str_col, tbl.str_col, tbl.str_col);
 STRING; 
 
-# dialect: spark, databricks
-VERSION();
-STRING;
-
 # dialect: hive, spark2, spark, databricks
 SPLIT(tbl.str_col, tbl.str_col, tbl.int_col);
 ARRAY<STRING>;

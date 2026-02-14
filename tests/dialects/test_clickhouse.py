5236cdd4a60aaf0955f2df2dfa22e276867f5ce4
Abhishek Bhosale <bhoslea925@gmail.com>
2026-02-14 10:19:45 +0530

    added more test

--- tests/dialects/test_clickhouse.py
+++ tests/dialects/test_clickhouse.py
@@ -698,6 +698,8 @@ class TestClickhouse(Validator):
         self.validate_identity("SELECT or(and(3, 0), 5)", "SELECT (3 AND 0) OR 5")
 
         self.validate_identity("SELECT UTCTimestamp()", "SELECT CURRENT_TIMESTAMP('UTC')")
+        query = parse_one("SELECT UTCTimestamp()", read="clickhouse")
+        self.assertIsInstance(query.expressions[0], exp.UtcTimestamp)
 
     def test_clickhouse_values(self):
         ast = self.parse_one("SELECT * FROM VALUES (1, 2, 3)")

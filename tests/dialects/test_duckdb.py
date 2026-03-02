f65ff36e3fce5a4a8ed85ce7c34dadfab18c78ab
geooo109 <geomichas96@gmail.com>
2026-03-02 15:29:41 +0200

    refactor

--- tests/dialects/test_duckdb.py
+++ tests/dialects/test_duckdb.py
@@ -1452,7 +1452,9 @@ class TestDuckDB(Validator):
             exp.Localtimestamp
         )
 
-        self.validate_identity("SELECT SUM(x) OVER (ORDER BY x GROUPS BETWEEN 1 PRECEDING AND CURRENT ROW) FROM t")
+        self.validate_identity(
+            "SELECT SUM(x) OVER (ORDER BY x GROUPS BETWEEN 1 PRECEDING AND CURRENT ROW) FROM t"
+        )
 
     def test_array_index(self):
         with self.assertLogs(helper_logger) as cm:

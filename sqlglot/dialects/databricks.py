e8d0dabf10d4afe22f2277d46cf931c39409063e
Vaggelis Danias <daniasevangelos@gmail.com>
2026-03-09 19:33:44 +0200

    refactor!: Extract Databricks parser for mypyc compilation (#7253)
    * refactor!: Extract Databricks parser for mypyc compilation
    
    * Remove unused import
    
    * Reset submodule pointer

--- sqlglot/dialects/databricks.py
+++ sqlglot/dialects/databricks.py
@@ -3,17 +3,14 @@ from __future__ import annotations
 from copy import deepcopy
 from collections import defaultdict
 
-from sqlglot import exp, transforms, jsonpath, parser
+from sqlglot import exp, transforms, jsonpath
 from sqlglot.dialects.dialect import (
     date_delta_sql,
-    build_date_delta,
     timestamptrunc_sql,
-    build_formatted_time,
     groupconcat_sql,
 )
 from sqlglot.dialects.spark import Spark
-from sqlglot.parsers.spark import SparkParser
-from sqlglot.helper import seq_get
+from sqlglot.parsers.databricks import DatabricksParser
 from sqlglot.tokens import TokenType
 from sqlglot.optimizer.annotate_types import TypeAnnotator
 
@@ -49,53 +46,7 @@ class Databricks(Spark):
             "VOID": TokenType.VOID,
         }
 
-    class Parser(SparkParser):
-        LOG_DEFAULTS_TO_LN = True
-        STRICT_CAST = True
-        COLON_IS_VARIANT_EXTRACT = True
-
-        FUNCTIONS = {
-            **SparkParser.FUNCTIONS,
-            "GETDATE": exp.CurrentTimestamp.from_arg_list,
-            "DATEADD": build_date_delta(exp.DateAdd),
-            "DATE_ADD": build_date_delta(exp.DateAdd),
-            "DATEDIFF": build_date_delta(exp.DateDiff),
-            "DATE_DIFF": build_date_delta(exp.DateDiff),
-            "NOW": exp.CurrentTimestamp.from_arg_list,
-            "TO_DATE": build_formatted_time(exp.TsOrDsToDate, "databricks"),
-            "UNIFORM": lambda args: exp.Uniform(
-                this=seq_get(args, 0), expression=seq_get(args, 1), seed=seq_get(args, 2)
-            ),
-        }
-
-        NO_PAREN_FUNCTION_PARSERS = {
-            **SparkParser.NO_PAREN_FUNCTION_PARSERS,
-            "CURDATE": lambda self: self._parse_curdate(),
-        }
-
-        FACTOR = {
-            **SparkParser.FACTOR,
-            TokenType.COLON: exp.JSONExtract,
-        }
-
-        COLUMN_OPERATORS = {
-            **parser.Parser.COLUMN_OPERATORS,
-            TokenType.QDCOLON: lambda self, this, to: self.build_cast(
-                False,
-                this=this,
-                to=to,
-            ),
-        }
-        CAST_COLUMN_OPERATORS = {
-            *SparkParser.CAST_COLUMN_OPERATORS,
-            TokenType.QDCOLON,
-        }
-
-        def _parse_curdate(self) -> exp.CurrentDate:
-            # CURDATE, an alias for CURRENT_DATE, has optional parentheses
-            if self._match(TokenType.L_PAREN):
-                self._match_r_paren()
-            return self.expression(exp.CurrentDate)
+    Parser = DatabricksParser
 
     class Generator(Spark.Generator):
         TABLESAMPLE_SEED_KEYWORD = "REPEATABLE"

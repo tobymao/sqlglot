ab6331fc920636e47c6bb03825086642d9425b77
Vaggelis Danias <daniasevangelos@gmail.com>
2026-03-09 17:29:42 +0200

    refactor!: Extract SQLite parser for mypyc compilation (#7240)
    * Extract sqlite parser for mypyc compilation
    
    * Enable mypyc compilation for sqlite parser

--- sqlglotc/setup.py
+++ sqlglotc/setup.py
@@ -56,6 +56,7 @@ SOURCE_FILES = [
             "trino.py",
             "spark2.py",
             "spark.py",
+            "sqlite.py",
             "tableau.py",
             "redshift.py",
             "solr.py",

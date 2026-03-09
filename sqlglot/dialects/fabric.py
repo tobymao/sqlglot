8c799adf70cc0136f1c6647c3212239c83f6cbe1
Vaggelis Danias <daniasevangelos@gmail.com>
2026-03-09 19:33:57 +0200

    refactor!: Extract Fabric parser for mypyc compilation (#7254)

--- sqlglot/dialects/fabric.py
+++ sqlglot/dialects/fabric.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from sqlglot import exp, transforms
 from sqlglot.dialects.dialect import NormalizationStrategy
 from sqlglot.dialects.tsql import TSQL
-from sqlglot.parsers.tsql import TSQLParser
+from sqlglot.parsers.fabric import FabricParser
 from sqlglot.tokens import TokenType
 
 
@@ -82,27 +82,7 @@ class Fabric(TSQL):
             "UTINYINT": TokenType.UTINYINT,
         }
 
-    class Parser(TSQLParser):
-        def _parse_create(self) -> exp.Create | exp.Command:
-            create = super()._parse_create()
-
-            if isinstance(create, exp.Create):
-                # Transform VARCHAR/CHAR without precision to VARCHAR(1)/CHAR(1)
-                if create.kind == "TABLE" and isinstance(create.this, exp.Schema):
-                    for column in create.this.expressions:
-                        if isinstance(column, exp.ColumnDef):
-                            column_type = column.kind
-                            if (
-                                isinstance(column_type, exp.DataType)
-                                and column_type.this in (exp.DType.VARCHAR, exp.DType.CHAR)
-                                and not column_type.expressions
-                            ):
-                                # Add default precision of 1 to VARCHAR/CHAR without precision
-                                # When n isn't specified in a data definition or variable declaration statement, the default length is 1.
-                                # https://learn.microsoft.com/en-us/sql/t-sql/data-types/char-and-varchar-transact-sql?view=sql-server-ver17#remarks
-                                column_type.set("expressions", [exp.Literal.number("1")])
-
-            return create
+    Parser = FabricParser
 
     class Generator(TSQL.Generator):
         # Fabric-specific type mappings - override T-SQL types that aren't supported
---
+++ sqlglot/parsers/fabric.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from sqlglot import exp
+from sqlglot.helper import mypyc_attr
+from sqlglot.parsers.tsql import TSQLParser
+
+
+@mypyc_attr(allow_interpreted_subclasses=True)
+class FabricParser(TSQLParser):
+    def _parse_create(self) -> exp.Create | exp.Command:
+        create = super()._parse_create()
+
+        if isinstance(create, exp.Create):
+            # Transform VARCHAR/CHAR without precision to VARCHAR(1)/CHAR(1)
+            if create.kind == "TABLE" and isinstance(create.this, exp.Schema):
+                for column in create.this.expressions:
+                    if isinstance(column, exp.ColumnDef):
+                        column_type = column.kind
+                        if (
+                            isinstance(column_type, exp.DataType)
+                            and column_type.this in (exp.DType.VARCHAR, exp.DType.CHAR)
+                            and not column_type.expressions
+                        ):
+                            # Add default precision of 1 to VARCHAR/CHAR without precision
+                            # When n isn't specified in a data definition or variable declaration statement, the default length is 1.
+                            # https://learn.microsoft.com/en-us/sql/t-sql/data-types/char-and-varchar-transact-sql?view=sql-server-ver17#remarks
+                            column_type.set("expressions", [exp.Literal.number("1")])
+
+        return create

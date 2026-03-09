364fca74025eab10f8ba34f2498f9545321e8a3f
Vaggelis Danias <daniasevangelos@gmail.com>
2026-03-09 17:28:59 +0200

    refactor!: Extract Tableau parser for mypyc compilation (#7243)
    * Extract tableau parser for mypyc compilation
    
    * Enable mypyc compilation for tableau parser

--- sqlglotc/setup.py
+++ sqlglotc/setup.py
@@ -56,6 +56,7 @@ SOURCE_FILES = [
             "trino.py",
             "spark2.py",
             "spark.py",
+            "tableau.py",
             "redshift.py",
             "solr.py",
             "risingwave.py",

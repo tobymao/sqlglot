fa4290956b922f173042170b7ff85fd29eb1f0d4
George Sittas <giwrgos.sittas@gmail.com>
2026-03-10 17:54:52 +0200

    Chore: do not run integration tests on make unit

--- tests/test_integration_loader.py
+++ tests/test_integration_loader.py
@@ -1,5 +1,7 @@
 import os
 
+from tests.helpers import SKIP_INTEGRATION
+
 INTEGRATION_TEST_DIR = os.path.join(
     os.path.dirname(__file__),
     "..",
@@ -10,6 +12,6 @@ INTEGRATION_TEST_DIR = os.path.join(
 
 
 def load_tests(loader, suite, pattern):
-    if os.path.isdir(INTEGRATION_TEST_DIR):
+    if not SKIP_INTEGRATION and os.path.isdir(INTEGRATION_TEST_DIR):
         suite.addTests(loader.discover(INTEGRATION_TEST_DIR, pattern="test*.py"))
     return suite

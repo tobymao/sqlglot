08a7ad89e4186f897c107e529e96f0201c7a760c
vaggelisd <daniasevangelos@gmail.com>
2026-03-24 13:05:34 +0200

    refactor(druid): Extract and compile generator

--- sqlglot/dialects/druid.py
+++ sqlglot/dialects/druid.py
@@ -1,24 +1,9 @@
-from sqlglot import exp, generator
-from sqlglot.dialects.dialect import rename_func, Dialect
+from sqlglot.dialects.dialect import Dialect
+from sqlglot.generators.druid import DruidGenerator
 from sqlglot.parsers.druid import DruidParser
 
 
 class Druid(Dialect):
     Parser = DruidParser
 
-    class Generator(generator.Generator):
-        # https://druid.apache.org/docs/latest/querying/sql-data-types/
-        TYPE_MAPPING = {
-            **generator.Generator.TYPE_MAPPING,
-            exp.DType.NCHAR: "STRING",
-            exp.DType.NVARCHAR: "STRING",
-            exp.DType.TEXT: "STRING",
-            exp.DType.UUID: "STRING",
-        }
-
-        TRANSFORMS = {
-            **generator.Generator.TRANSFORMS,
-            exp.CurrentTimestamp: lambda *_: "CURRENT_TIMESTAMP",
-            exp.Mod: rename_func("MOD"),
-            exp.Array: lambda self, e: f"ARRAY[{self.expressions(e)}]",
-        }
+    Generator = DruidGenerator

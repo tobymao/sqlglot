5eed701764bdc69e38ff714c527cc1ed15846660
vaggelisd <daniasevangelos@gmail.com>
2026-03-24 13:21:35 +0200

    refactor(bigquery): Extract and compile generator

--- tests/dialects/test_bigquery.py
+++ tests/dialects/test_bigquery.py
@@ -2445,7 +2445,7 @@ OPTIONS (
             },
         )
 
-    @mock.patch("sqlglot.dialects.bigquery.logger")
+    @mock.patch("sqlglot.generators.bigquery.logger")
     def test_pushdown_cte_column_names(self, logger):
         with self.assertRaises(UnsupportedError):
             transpile(

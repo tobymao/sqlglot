eeee4f33fd2c964451344c8873f36729af68936b
Shubham Kapoor <shubhamkapoor@mac.lkw-in.ibm.com>
2026-04-13 11:41:36 +0530

    feat: Add DB2 dialect foundation  - Add minimal DB2 dialect class with Tokenizer     - Register DB2 in dialects module     - Add DB2 to Dialects enum     - Add basic identity tests
        This is the foundation for DB2 support. Subsequent PRs will add:
        - Parser functions
        - Generator transforms
        - Custom SQL methods
        - Date/time functions

--- sqlglot/dialects/dialect.py
+++ sqlglot/dialects/dialect.py
@@ -86,6 +86,7 @@ class Dialects(str, Enum):
     BIGQUERY = "bigquery"
     CLICKHOUSE = "clickhouse"
     DATABRICKS = "databricks"
+    DB2 = "db2"
     DORIS = "doris"
     DREMIO = "dremio"
     DRILL = "drill"

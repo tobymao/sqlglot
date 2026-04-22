371e4a714d751fbb4ed1f4d9ca22a93adc6d7172
geooo109 <geomichas96@gmail.com>
2026-04-22 18:53:58 +0300

    add trino

--- sqlglot/dialects/trino.py
+++ sqlglot/dialects/trino.py
@@ -9,6 +9,7 @@ from sqlglot.tokens import TokenType
 class Trino(Presto):
     SUPPORTS_USER_DEFINED_TYPES = False
     LOG_BASE_FIRST = True
+    CONCAT_WS_COALESCE = True
 
     class Tokenizer(Presto.Tokenizer):
         KEYWORDS = {

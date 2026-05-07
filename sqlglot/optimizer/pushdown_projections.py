4683563aa7d0905df376d6ab7484ad997d5090c2
geooo109 <geomichas96@gmail.com>
2026-05-07 18:23:35 +0300

    revert line

--- sqlglot/optimizer/pushdown_projections.py
+++ sqlglot/optimizer/pushdown_projections.py
@@ -113,6 +113,7 @@ def pushdown_projections(
             for name, (node, source) in scope.selected_sources.items():
                 if isinstance(source, Scope) and isinstance(source.expression, exp.Selectable):
                     select = seq_get(source.expression.selects, 0)
+
                     if scope.pivots or isinstance(select, exp.QueryTransform):
                         columns: set[object] = {SELECT_ALL}
                     else:

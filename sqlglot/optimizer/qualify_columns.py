15b0b5bdf64daf070fa46aa592ff509d40b93462
fivetran-kwoodbeck <kris.woodbeck@fivetran.com>
2026-07-28 10:38:39 -0400

    bugfix: replaced find with find_in_scope

--- sqlglot/optimizer/qualify_columns.py
+++ sqlglot/optimizer/qualify_columns.py
@@ -10,7 +10,7 @@ from sqlglot.errors import OptimizeError, highlight_sql
 from sqlglot.helper import seq_get
 from sqlglot.optimizer.annotate_types import TypeAnnotator
 from sqlglot.optimizer.resolver import Resolver
-from sqlglot.optimizer.scope import Scope, build_scope, traverse_scope, walk_in_scope
+from sqlglot.optimizer.scope import Scope, build_scope, find_in_scope, traverse_scope, walk_in_scope
 from sqlglot.optimizer.simplify import simplify_parens
 from sqlglot.schema import Schema, ensure_schema
 
@@ -358,7 +358,7 @@ def _expand_alias_refs(
             if alias_expr:
                 # An aggregate alias must not be expanded into a GROUP BY or another (non-window) aggregate.
                 skip_replace = bool(
-                    alias_expr.find(exp.AggFunc)
+                    find_in_scope(alias_expr, exp.AggFunc)
                     and (
                         is_group_by
                         or (

b1e2a5bb44b2adaa02e014d59da60459bfd10098
George Sittas <giwrgos.sittas@gmail.com>
2026-07-31 17:38:14 +0300

    Silence command fallback warnings in WITH CHECK OPTION tests

--- tests/dialects/test_postgres.py
+++ tests/dialects/test_postgres.py
@@ -1176,7 +1176,9 @@ FROM json_data, field_ids""",
 
         self.validate_identity("CREATE TYPE mood AS ENUM ()").assert_is(exp.Create)
 
-        self.validate_identity("CREATE VIEW v AS SELECT * FROM start WITH CHECK OPTION")
+        self.validate_identity(
+            "CREATE VIEW v AS SELECT * FROM start WITH CHECK OPTION", check_command_warning=True
+        )
         self.validate_identity("CREATE VIEW start WITH (security_barrier=TRUE) AS SELECT 1")
 
         create_type = self.validate_identity(

0a176e429b3f59ba4a3fda3347d635cf66931012
George Sittas <giwrgos.sittas@gmail.com>
2026-07-31 16:15:10 +0300

    Restore consumed prefix tokens when _parse_property_before fails to match

--- sqlglot/parser.py
+++ sqlglot/parser.py
@@ -2794,6 +2794,8 @@ class Parser:
         return self.expression(exp.TriggerExecute(this=func_call))
 
     def _parse_property_before(self) -> exp.Expr | list[exp.Expr] | None:
+        index = self._index
+
         # only used for teradata currently
         self._match(TokenType.COMMA)
 
@@ -2816,6 +2818,8 @@ class Parser:
             except TypeError:
                 self.raise_error(f"Cannot parse property '{self._prev.text}'")
 
+        # Restore any consumed prefix keywords (e.g. DEFAULT) so `_parse_property` can parse them
+        self._retreat(index)
         return None
 
     def _parse_wrapped_properties(self) -> list[exp.Expr | list[exp.Expr]]:

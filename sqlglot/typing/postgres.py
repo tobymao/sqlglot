09f639583d1f4845cdcf202b834e3538d2dd2206
Jo <46752250+georgesittas@users.noreply.github.com>
2026-08-17 02:13:11 +0300

    Update sqlglot/typing/postgres.py

--- sqlglot/typing/postgres.py
+++ sqlglot/typing/postgres.py
@@ -5,8 +5,6 @@ from sqlglot.typing import EXPRESSION_METADATA
 
 EXPRESSION_METADATA = {
     **EXPRESSION_METADATA,
-    # https://www.postgresql.org/docs/current/functions-window.html
-    # NTILE returns integer; other ranking functions return bigint (base default).
     **{
         expr_type: {"returns": exp.DType.INT}
         for expr_type in {

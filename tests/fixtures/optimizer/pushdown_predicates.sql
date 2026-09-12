5cfb5997a99010940138670adf3d6b34ac5a0a08
Toby Mao <toby.mao@gmail.com>
2026-09-12 04:01:25 -0700

    fix(optimizer)!: preserve precedence when replacing aliases in predicate pushdown [GLM] (#8342)
    replace_aliases inlined projection expressions verbatim, so
    WHERE d = FALSE with a OR b AS d became a OR b = FALSE, which
    re-parses as a OR (b = FALSE) and changes results. Wrap the
    substituted expression in parens when it is an operator expression
    placed inside another operator context.

--- tests/fixtures/optimizer/pushdown_predicates.sql
+++ tests/fixtures/optimizer/pushdown_predicates.sql
@@ -14,10 +14,16 @@ SELECT x.a FROM (SELECT * FROM x) AS x JOIN y WHERE (x.a = y.a AND x.a = 1 AND x
 SELECT x.a FROM (SELECT * FROM x) AS x JOIN y ON (x.a = 1 AND x.a = y.a AND x.b = 1) OR x.a = y.b WHERE (x.a = 1 AND x.a = y.a AND x.b = 1) OR x.a = y.b;
 
 SELECT x.a FROM (SELECT x.a AS a, x.b * 1 AS c FROM x) AS x WHERE x.c = 1;
-SELECT x.a FROM (SELECT x.a AS a, x.b * 1 AS c FROM x WHERE x.b * 1 = 1) AS x WHERE TRUE;
+SELECT x.a FROM (SELECT x.a AS a, x.b * 1 AS c FROM x WHERE (x.b * 1) = 1) AS x WHERE TRUE;
 
 SELECT x.a FROM (SELECT x.a AS a, x.b * 1 AS c FROM x) AS x WHERE x.c = 1 or x.c = 2;
-SELECT x.a FROM (SELECT x.a AS a, x.b * 1 AS c FROM x WHERE x.b * 1 = 1 OR x.b * 1 = 2) AS x WHERE TRUE;
+SELECT x.a FROM (SELECT x.a AS a, x.b * 1 AS c FROM x WHERE (x.b * 1) = 1 OR (x.b * 1) = 2) AS x WHERE TRUE;
+
+SELECT x.a FROM (SELECT x.a AS a, x.b OR x.c AS d FROM x) AS x WHERE x.d = FALSE;
+SELECT x.a FROM (SELECT x.a AS a, x.b OR x.c AS d FROM x WHERE (x.b OR x.c) = FALSE) AS x WHERE TRUE;
+
+SELECT x.a FROM (SELECT x.a AS a, x.b AND x.c AS d FROM x) AS x WHERE NOT x.d;
+SELECT x.a FROM (SELECT x.a AS a, x.b AND x.c AS d FROM x WHERE NOT x.b OR NOT x.c) AS x WHERE TRUE;
 
 SELECT x.a AS a FROM (SELECT x.a FROM x AS x) AS x JOIN y WHERE x.a = 1 AND x.b = 1 AND (x.c = 1 OR y.c = 1);
 SELECT x.a AS a FROM (SELECT x.a FROM x AS x WHERE x.a = 1 AND x.b = 1) AS x JOIN y ON x.c = 1 OR y.c = 1 WHERE TRUE AND TRUE AND (TRUE);
